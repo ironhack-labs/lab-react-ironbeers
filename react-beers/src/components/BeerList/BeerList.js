beede3f45f500d7d92825ba3cde831d3181e2a48
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-27 17:39:58 -0400

    DONE

--- react-beers/src/components/BeerList/BeerList.js
+++ react-beers/src/components/BeerList/BeerList.js
@@ -6,7 +6,8 @@ import axios from 'axios';
 
 class BeerList extends Component {
     state={
-      beerList: []
+      beerList: [],
+      name: ""
     };
 
     componentDidMount() {
@@ -21,11 +22,32 @@ class BeerList extends Component {
             });
     }
 
+    handleChange = (event) => {
+        const { value } = event.target;
+        this.setState({
+            name: value
+        });
+
+        if(value !== "") {
+            axios.get(`https://ironbeer-api.herokuapp.com/beers/search?q=${value}`)
+                .then((res) => {
+                    this.setState({
+                        beerList: res.data
+                    });
+                })
+                .catch( error => console.log(error) )
+        }
+    };
+
     render() {
         return (
             <div>
                 <Header />
                 <div className="container beerCont">
+                    <div className="form-group search col-12 col-md-5">
+                        <label htmlFor="name">Search beer:</label>
+                        <input type="text" value={ this.state.name} className="form-control"  name="name" onChange={ this.handleChange } placeholder="Search..."/>
+                    </div>
                     {
                         this.state.beerList.map((beer, id) => {
                             const { image_url, name, tagline, contributed_by, _id } = beer;
