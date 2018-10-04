f3a2d54c75809b858897a03c9f9309c02f6c9709
Mapno <mariorvinas@gmail.com>
2018-10-04 18:00:12 +0200

    creates beer

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './views/HomePage';
 import { Route } from 'react-router-dom';
 import BeerList from './views/BeerList';
 import SingleBeer from './views/SingleBeer';
+import NewBeerForm from './views/NewBeerForm';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Route exact path="/beers" render={() => <BeerList></BeerList>}></Route>
         <Route path="/beers/:id" component={SingleBeer}></Route>
         <Route path="/random-beer" render={() => <SingleBeer random={true}></SingleBeer>}></Route>
+        <Route path="/new-beer" component={NewBeerForm}></Route>
       </div>
     );
   }
