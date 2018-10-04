74f1d2dc680d18f6851199145dbb1b245e377edd
LauraMiguelanez <laura.miguelanez@gmail.com>
2018-10-04 17:58:29 +0200

    Iteration 6 Done

--- ironbeers/src/components/RandomBeer.js
+++ ironbeers/src/components/RandomBeer.js
@@ -1,11 +1,53 @@
 import React from "react";
 import { Header } from "../components/Header";
+import axios from "axios";
 
 export class RandomBeer extends React.Component {
   constructor() {
     super();
+    this.state = {
+      beer: {}
+    };
+  }
+
+  getRandomBeer = () => {
+
+    axios
+      .get(`https://ironbeer-api.herokuapp.com/beers/random`)
+      .then(beer => {
+        this.setState({ beer: beer.data[0] });
+
+      })
+      .catch(error => console.log(error));
+  };
+
+  componentWillMount() {
+    this.getRandomBeer();
+    console.log(this.state.beer);
   }
   render() {
-    return <Header />;
+    let {
+        image_url,
+      name,
+      tagline,
+      first_brewed,
+      attenuation_level,
+      description,
+      contributed_by
+    } = this.state.beer;
+    return (
+      <div>
+        <Header />
+        <div>
+          <img height="300px" src={image_url} alt={name} />
+          <h1>{name}</h1>
+          <h2>{attenuation_level}</h2>
+          <p>{tagline}</p>
+          <p>{first_brewed}</p>
+          <p>{description}</p>
+          <p>{contributed_by}</p>
+        </div>
+      </div>
+    );
   }
 }
