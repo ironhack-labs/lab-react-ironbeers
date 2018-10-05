91ca22771237afe7ad70e1516ed6971c735d436a
cesararmada <cesar.armada@gmail.com>
2018-10-05 09:21:15 +0200

    todo menos bonus

--- reactive/src/index.js
+++ reactive/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import 'bulma/css/bulma.css'
 
 ReactDOM.render(
   <BrowserRouter>
