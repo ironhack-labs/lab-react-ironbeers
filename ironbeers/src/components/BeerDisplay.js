2c22d7b29f6c31f0e085b03bcad495d87e576da8
AmelRodr <amelrodr@gmail.com>
2018-10-05 07:19:20 -0500

    done7

--- ironbeers/src/components/BeerDisplay.js
+++ ironbeers/src/components/BeerDisplay.js
@@ -6,24 +6,19 @@ const BeerDisplay = (info) => {
   return(
     <Card
     hoverable
+    bordered
     style={{ width: 300 }}
     cover={<img src={beer.image_url} style={{width: '60px'}} alt={'photo of' + beer.name}/>}
-
     >
-   <Meta
-        
+   <Meta        
    title={beer.name}
    description={beer.description}
-    />
-  
+    />  
     <h4>{beer.tagline}</h4>
-    <h3> Level: {beer.attenuation_level}</h3>
-    
-   <h5>Description</h5>
-   
-   <h5>Brewer Tips</h5>
-   <p>{beer.brewers_tips}</p>
-   </Card>
+    <h3> Level: {beer.attenuation_level}</h3>    
+    <h5>Brewer Tips</h5>
+    <p>{beer.brewers_tips}</p>
+    </Card>
   )
 }
 
--- ironbeers/src/components/Beers.js
+++ ironbeers/src/components/Beers.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Axios from 'axios'
 import {NavLink} from 'react-router-dom'
+//import { List, Avatar } from 'antd';
 var url = 'https://ironbeer-api.herokuapp.com/beers/all'
 
 class Beers extends Component {
@@ -18,21 +19,26 @@ class Beers extends Component {
 
   showBeer = (beer,index) => {
     let ruta = "/beers/"+beer._id
-    return <div key={index} style={{margin:"30px", width: "230px"}}>
+    return (
       
+
+    <div key={index} style={{margin:"30px", width: "150px"}}>      
       <img src={beer.image_url} style={{width:'50px'}} alt={beer.name}/>
       <br/>
       <NavLink to={ruta}><h3>{beer.name}</h3></NavLink>  
-
+       <h4>{beer.tagline}</h4>
+       <p>Creted by:{beer.contributed_by} </p>
       </div>
-
+    )
   }
+
+
 
   render(){
     const {beers} = this.state
     return (
       <div >
-        {beers.map((b,i)=>this.showBeer(b,i))}
+        {beers.map((ber,index)=>this.showBeer(ber,index))}
       </div>
     )
   }
