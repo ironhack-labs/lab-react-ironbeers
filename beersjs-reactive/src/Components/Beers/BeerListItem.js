b755c3be5620f54abd577bc0a845aac6a777c1cd
Vermont <vermont.ironhack@gmail.com>
2018-11-26 19:47:39 -0600

    Iteration 4 done

---
+++ beersjs-reactive/src/Components/Beers/BeerListItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const BeerListItem = ({_id, image_url, name, tagline, contributed_by}) => (
+  <div className='beers-list'>
+    <Link className='beer-list-item' to={`/beers/'${_id}`}>
+      <div>
+        <img src={image_url} alt={name}/>
+      </div>
+      <div>
+        <h4>Name {name}</h4>
+        <p> Tag {tagline}</p>
+        <p><strong>Contributed {contributed_by}</strong></p>
+      </div>
+    </Link>
+  </div>
+)
+
+export default BeerListItem;
--- beersjs-reactive/src/Components/Beers/Beers.js
+++ beersjs-reactive/src/Components/Beers/Beers.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from '../Header/Header';
+import axios from 'axios';
+import BeerListItem from './BeerListItem';
 
-const Beers = () => (
-  <section>
-    <Header />
-    <h1>All</h1>
-  </section>
-)
+class Beers extends Component{
+  
+  constructor(){
+    super();
+    this.state = {
+      beers:[]
+    }
+
+  }
+  
+  componentWillMount(){
+    const base_url = 'https://ironbeer-api.herokuapp.com/beers'
+
+    axios.get(`${base_url}/all`)
+      .then(res =>{
+        this.setState({beers: res.data})
+        console.log('State =====>', this.state.beers)
+      })
+      .catch(err => {
+        console.log('Error =====>', err)
+      });
+  }
+
+  render(){
+    const {beers} = this.state;
+    return (
+      <section>
+        <Header />
+        {beers.map(beer => <BeerListItem key={beer._id} {...beer}/>)}
+      </section>
+    )
+  }
+}
 
 export default Beers;
