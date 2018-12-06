b9c566de022732bc386d18f789e19ad5be11125a
David <davidmunozvi@icloud.com>
2018-12-06 21:03:11 +0100

    Iteration 4 list of beers

--- ironbeers/src/components/Beers/Beers.js
+++ ironbeers/src/components/Beers/Beers.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react'
 import { Link, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 
 export default class Beers extends Component {
-  render() {
-    return (
-      <div>
-        <Link to='/'>
-                  <h3>Home</h3>
-         </Link>
-      </div>
-    )
-  }
+  constructor(){
+    super();
+    this.state = {listOfBeers: null};
+}
+
+getAllBeers = () =>{
+  axios.get(`https://ironbeer-api.herokuapp.com/beers/all`)
+  .then(responseFromApi => {
+    console.log(responseFromApi);
+    this.setState({
+      listOfBeers: responseFromApi.data
+    })
+  })
+}
+
+componentDidMount() {
+  this.getAllBeers();
+}
+
+render(){
+  
+  return this.state.listOfBeers ? 
+    (<div>
+      <Link to='/'>
+    <h3>Home</h3>
+  </Link>
+    <div>
+      { this.state.listOfBeers.map((beer, index) => {
+         return (
+          <div>
+            <img src={beer.image_url} alt=""/>
+            <h1>{beer.name}</h1>
+            <p>{beer.tagline}</p>
+            <p>contributed by: {beer.contributed_by}</p>
+          </div>
+        )})
+      }    
+    </div>
+  </div>)
+  : 
+    (
+    <p>Loading...</p>
+  )
+  
+ 
+
+}
 }
