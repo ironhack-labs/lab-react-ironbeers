47ff1c6e66ccdcb0b0f1edb843c9e6ee5ac0a642
David Diez <ddiezr@gmail.com>
2018-12-06 21:50:35 +0100

    Iteration 7 finished

--- react-ironbeers/src/components/NewBeer/NewBeer.js
+++ react-ironbeers/src/components/NewBeer/NewBeer.js
@@ -5,16 +5,13 @@ export default class NewBeer extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { name: "", tagline: "" , description: ""}
+    this.state = { name: "", tagline: "" , description: "",first_brewed:"",brewers_tips:"",attenuation_level:0,contributed_by:""}
   }
 
   handleFormSubmit =(event)=>{
      event.preventDefault();
-     const name= this.state.name;
-     const tagline= this.state.tagline;
-     const description= this.state.description;
-     console.log(name,tagline,description);
-     axios.post('https://ironbeer-api.herokuapp.com/beers/new',{name , tagline, description})
+     console.log(this.state,"DENTRO DEL SUBMIT");
+     axios.post('https://ironbeer-api.herokuapp.com/beers/new',this.state)
      .then((resApi)=>{
         console.log(resApi);
      })
@@ -28,7 +25,6 @@ export default class NewBeer extends Component {
     console.log(event.target.value);
     const {name, value} = event.target;
     this.setState({[name]: value});
-    console.log(this.state);
   }
 
 
@@ -42,8 +38,16 @@ export default class NewBeer extends Component {
             <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
             <label>Tagline:</label>
             <input type="text" name="tagline" value={this.state.tagline} onChange={this.handleChange}/>
-            <label>Description:</label>
-            <textarea name="description" value={this.state.description} onChange={this.handleChange} />
+            <label>description:</label>
+            <input type="text" name="description" value={this.state.description} onChange={this.handleChange}/>
+            <label>first_brewed:</label>
+            <input type="text" name="first_brewed" value={this.state.first_brewed} onChange={this.handleChange}/>
+            <label>brewers_tips:</label>
+            <input type="text" name="brewers_tips" value={this.state.brewers_tips} onChange={this.handleChange}/>
+            <label>attenuation_level:</label>
+            <input type="number" name="attenuation_level" value={this.state.attenuation_level} onChange={this.handleChange}/>
+            <label>contributed_by:</label>
+            <input type="text" name="contributed_by" value={this.state.contributed_by} onChange={this.handleChange}/>
         <input type="submit" value="Submit" />
         </form>
       </div>
