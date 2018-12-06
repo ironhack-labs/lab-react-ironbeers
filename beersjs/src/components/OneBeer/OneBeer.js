c15326c0e71edc7a11f84d1f966991951bc14e1e
N R <mnpuzzle@gmail.com>
2018-12-06 23:55:29 +0100

    almost all Iteration 5: Single Beer

--- beersjs/src/components/OneBeer/OneBeer.js
+++ beersjs/src/components/OneBeer/OneBeer.js
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
- 
+import axios from "axios";
 
 export default class OneBeer extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      beer: null
+    }
+  }
+
+  getOneBeer = () => {
+    
+  }
+  
   render() {
     
-    return (
-      <div>
+
+    if(this.state.beers !== null){
+      return ( 
         <div>
-          
-          
+          <img src={this.state.beer.image_url}></img>
+        <div>{this.state.beer.name}</div>
+        <div>{this.state.beer.tagline}</div>
+        <div>{this.state.beer.first_brewed}</div>
+        <div>{this.state.beer.attenuation_level}</div>
+        <div>{this.state.beer.description}</div>
+        <div>{this.state.beer.contributed_by}</div>
         </div>
-      </div>
-    );
+        )
+      
+    }
+    else {
+      return <h1>Loading...</h1>
+    }
+
   }
 }
