68ad413b9f76ee6d924b7c8d02b72536786f5dc3
Jorge Devesa <jrgdevesa@gmail.com>
2018-12-06 22:37:37 +0100

    Iteration 4

--- starter-code/src/Components/Beers/Beers.js
+++ starter-code/src/Components/Beers/Beers.js
@@ -1,12 +1,61 @@
 import React, { Component } from 'react'
 import NavBar from '../NavBar/NavBar'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class Beers extends Component {
+  constructor(){
+    super();
+    this.state = { lisOfbeers: null };
+}
+getAllBeers = () =>{
+  axios.get(`https://ironbeer-api.herokuapp.com/beers/all`)
+  .then(resApi => {
+    this.setState({
+      lisOfbeers: resApi.data
+    });
+  });
+};
+
+componentDidMount() {
+  this.getAllBeers();
+  console.log(this.getAllBeers())
+}
+
   render() {
+
     return (
-      <div>
+    
+        this.state.lisOfbeers ? 
+          (<div>
+              <div>
         <NavBar></NavBar>
-      </div>
+    </div>
+          <div>
+          <br></br>
+            { this.state.lisOfbeers.map((beer, index) => {
+               return (
+                <div key={beer._id}>
+                  <Link to={`/beers/${beer._id}`}>
+                    <h3>{beer.name}</h3>
+                  </Link>
+                  <p>{beer.tagline} </p>
+                  <img src={`${beer.image_url}`} style={{width:'10%'}}></img>
+                  <h5>{beer.contributed_by}</h5>
+                  <br></br>
+                </div>
+
+              )})
+            }    
+          </div>
+          <div style={{width: '40%', float:"right"}}>
+              {/* <AddBeer getData={() => this.getAllProjects()}/> */}
+          </div>
+        </div>)
+        : 
+          (
+          <p>Espera, hermano. Estamos recopilando la data.</p>
+        )
     )
   }
 }
--- starter-code/src/Components/Home/Home.js
+++ starter-code/src/Components/Home/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import NavBar from '../NavBar/NavBar'
+
+
 export default class Home extends Component {
   render() {
     return (
