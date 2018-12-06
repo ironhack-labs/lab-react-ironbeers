b62c260544bfe1a6891698dc93715e2194be973b
cocobilbao <cocobilbao@gmail.com>
2018-12-06 19:55:24 +0100

    iteration 2

---
+++ ironbeers/src/components/NewBeer/NewBeer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class NewBeer extends Component {
+  render() {
+    return (
+        <div>
+        <img src="https://www.comprar-bebidas.com/media/catalog/product/cache/5/image/1800x/040ec09b1e35df139433887a97daa66f/2/4/2448.jpg" alt=""/>
+      <h2>NewBeers</h2>
+      <p>hgjhgkhghgjhgjhgjhgkhghkjghjgg</p>
+    </div>
+    )
+  }
+}
