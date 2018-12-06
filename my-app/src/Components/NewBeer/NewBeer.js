421054c345e72bada4708ca8404500b1e3a91c6d
Patricia LG <patri.lopg5@gmail.com>
2018-12-06 19:04:01 +0100

    router done

---
+++ my-app/src/Components/NewBeer/NewBeer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class NewBeer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>New Beer</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adip</p>
+      </div>
+    )
+  }
+}
---
+++ my-app/src/Components/RandomBeer/RandomBeer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class RandomeBeer extends Component {
+  render() {
+    return (
+      <div>
+         <h1>RandomBeer</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adip</p>
+      </div>
+    )
+  }
+}
