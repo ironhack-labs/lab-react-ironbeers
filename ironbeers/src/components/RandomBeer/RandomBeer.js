fdf2a4d8e8b11ac65121c80a6909346368d3f4ab
David <davidmunozvi@icloud.com>
2018-12-06 21:34:26 +0100

    Iteration 6 Random Beer

--- ironbeers/src/components/RandomBeer/RandomBeer.js
+++ ironbeers/src/components/RandomBeer/RandomBeer.js
@@ -1,14 +1,55 @@
 import React, { Component } from 'react'
 import { Link, Switch, Route } from 'react-router-dom';
-
+import axios from 'axios';
 export default class RandomBeer extends Component {
-  render() {
-    return (
-      <div>
-        <Link to='/'>
-                  <h3>Home</h3>
-         </Link>
-      </div>
-    )
-  }
+  constructor(){
+    super();
+    this.state = {randomBeer:null};
+}
+
+getAllBeers = () =>{
+  axios.get(`https://ironbeer-api.herokuapp.com/beers/random`)
+  .then(responseFromApi => {
+    console.log(responseFromApi);
+    this.setState({
+      randomBeer: responseFromApi.data
+    })
+  })
+}
+
+componentDidMount() {
+  this.getAllBeers();
+}
+
+render(){
+  
+  return this.state.randomBeer ? 
+    (<div>
+      <Link to='/'>
+    <h3>Home</h3>
+  </Link>
+    <div>
+      { this.state.randomBeer.map((randomBeer, index) => {
+         return (
+          <div>
+            <img src={randomBeer.image_url} alt=""/>
+            <h1>{randomBeer.name}</h1>
+            <p>{randomBeer.tagline}</p>
+            <p>{randomBeer.first_brewed}</p>
+            <p>{randomBeer.attenuation_level}</p>
+            <p>{randomBeer.description}</p>
+            <p>contributed by: {randomBeer.contributed_by}</p>
+          </div>
+        )})
+      }    
+    </div>
+  </div>)
+  : 
+    (
+    <p>Loading...</p>
+  )
+  
+ 
+
+}
 }
