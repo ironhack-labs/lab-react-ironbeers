a2c0c39449d92c39ebbab90ac5486434c808cfd5
aemartos <aemartos@gmail.com>
2019-01-21 11:43:36 +0100

    Minor changes

--- src/components/ItemMenu.jsx
+++ src/components/ItemMenu.jsx
@@ -8,13 +8,23 @@ const StyledItem = styled.div`
   img {
     width: 100%;
     height: 65%;
+    object-fit: cover;
   }
   .infoLink {
     padding: 0 1em 1em;
     a {
       text-decoration: none;
       color: black;
+      height: 1.5em;
       font-size: 1.3em;
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: flex-start;
+      align-items: center;
+      i {
+        font-size: .6em;
+        margin: .4em 0 0 .5em;
+      }
     }
     .description {
       font-size: .7em;
@@ -29,7 +39,7 @@ const ItemMenu = (props) => {
     <StyledItem>
       <img src={`/img/${props.to}.png`} alt={props.to}/>
       <div className="infoLink">
-        <Link className="linkMeu" to={`/${props.to}`}>{props.linkName}</Link>
+        <Link className="linkMeu" to={`/${props.to}`}><span>{props.linkName}</span> <i className="fas fa-arrow-right"></i></Link>
         <div className="description">{props.children}</div>
       </div>
     </StyledItem>
