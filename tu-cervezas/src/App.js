9a87fac534271d5d86985c04229c50cd383ca6d7
Diego Rodríguez <diegorodriguezgzz@gmail.com>
2019-03-05 19:09:24 -0600

    Scaffolding done

---
+++ tu-cervezas/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default App;
