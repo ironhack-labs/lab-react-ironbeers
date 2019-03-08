be75708a8450653c254d3fccc3e28a8a7b3885f0
Victoria Hickam <vhickam@gmail.com>
2019-03-08 12:13:10 -0500

    all beers show

--- starter-code/src/components/AllBeers.js
+++ starter-code/src/components/AllBeers.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 //import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class AllBeers extends Component {
+
+  state = {
+    beers: []
+  }
+
+  componentDidMount() {
+    axios.get("https://ironbeer-api.herokuapp.com/beers/all")
+    .then(response => {
+      this.setState({beers: response.data})
+    })
+  }
+
+  showBeers = () => {
+    let list = this.state.beers.map((beer) => {
+      return ( 
+        <h1>{beer.name}</h1>
+        )
+    })
+    return list;
+  }
+
+
   render() {
     return (
       <div className="AllBeers">
-        <h1>This is all beers</h1>
+        <h1>{this.showBeers()}</h1>
       </div>
     );
   }
