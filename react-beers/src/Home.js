b85f1bc537da49859468e1b2903673a3636e48f6
Daniela Olano <danielaolano96@gmail.com>
2019-05-22 09:39:02 -0400

    first commit

---
+++ react-beers/src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='Home'>
+        Add a Beer
+      </div>
+    );
+  }
+}
+
+export default Home;
