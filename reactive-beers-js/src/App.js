57dc68f4a21e48cae0afe733ff3fc7ad8b550f83
Guille LA <guillermolucena90@gmail.com>
2019-05-31 17:35:23 +0200

    third iteration done

--- reactive-beers-js/src/App.js
+++ reactive-beers-js/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './scenes/Home'
+import Beers from './scenes/Beers'
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/beers" component={Home} />
+        <Route exact path="/beers" component={Beers} />
         <Route exact path="/random-beer" component={Home} />
         <Route exact path="/new-beer" component={Home} />
       </Switch>
---
+++ reactive-beers-js/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+
+return <div className="header"><Link to="/"><i className="fas fa-home header-icon"></i></Link></div>
+} 
+
+export default Header
