254a19a30844b1f17cda8e8abc1af4bde0a50d22
Josue Valencia <josue.valencia@domo.ec>
2019-05-31 17:21:06 +0200

    single page, reusing cards

--- starter-code/src/components/Cards.js
+++ starter-code/src/components/Cards.js
@@ -7,8 +7,15 @@ const Cards = (props) => {
 			<img className="card-img-top" src={process.env.PUBLIC_URL + props.img} alt="..." />
 			<div className="card-body">
 				<h5 className="card-title">{props.name}</h5>
-				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				<Link to={props.link} className="btn btn-primary">Check the Page</Link>
+				<p className="card-text">{
+					props.description 
+						? props.description 
+						: 'Vivamus consectetuer hendrerit lacus. Cras non dolor. Etiam sollicitudin, ipsum eu pulvinar rutrum, tellus ipsum laoreet sapien, quis venenatis ante odio sit amet eros. Phasellus magna.'
+				}</p>
+				
+				{props.link 
+					? <Link to={props.link} className="btn btn-primary">Check the Page</Link>
+					: null}
 			</div>
 		</div>
 	)
--- starter-code/src/scenes/beer.js
+++ starter-code/src/scenes/beer.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, {Component, Fragment} from 'react'
 import Navbar from '../components/navbar'
+import BeersService from '../services/BeersService'
+import Cards from '../components/Cards'
 
-const Beer = () => (
-  <Navbar />
-)
+class Beer extends Component {
+  state = {
+    beer : {}
+  }
 
+  componentDidMount () {
+    const {id} = this.props.match.params
+    BeersService.getBeerbyId(id)
+      .then(
+        beer => this.setState({beer}), 
+        err => console.error(err)
+      )
+  }
+
+  render (){
+    console.log(this.state.beer)
+    return (
+      <Fragment>
+        <Navbar />
+        <Cards 
+          name={this.state.beer.name} 
+          img={this.state.beer.image_url && this.state.beer.image_url } 
+          description = {this.state.beer.description && this.state.beer.description}
+        />
+      </Fragment>
+    )
+  }
+}
 export default Beer
