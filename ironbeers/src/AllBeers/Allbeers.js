e62e6f9244aada4258389b403bdb6e8281b67dba
Francho Garcia <franchog79@gmail.com>
2019-07-18 22:09:21 +0200

    search box finished

--- ironbeers/src/AllBeers/Allbeers.js
+++ ironbeers/src/AllBeers/Allbeers.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import Axios from "axios";
 import { Link } from "react-router-dom";
 import AddNewBeer from "../AddNewBeer/AddNewBeer";
+import HomeBtn from "../HomeBtn/HomeBtn";
 
 class Allbeers extends Component {
   constructor() {
     super();
-    this.state = { listOfBeers: [] };
+    this.state = { listOfBeers: [], filterQuery: "" };
   }
 
   getAllBeers = () => {
@@ -17,6 +18,21 @@ class Allbeers extends Component {
     });
   };
 
+  searchOneBeer(e) {
+    const filter = e.target.value;
+
+    this.setState({
+      filterQuery: filter
+    });
+    Axios.get(`http://localhost:5000/search?q=${filter}`).then(
+      responseFromApi => {
+        this.setState({
+          listOfBeers: responseFromApi.data
+        });
+      }
+    );
+  }
+
   componentDidMount() {
     this.getAllBeers();
   }
@@ -24,12 +40,22 @@ class Allbeers extends Component {
   render() {
     return (
       <div>
-        <Link to="/">Home</Link>
+        <div className="fix">
+          <HomeBtn />
+          <input
+            className="search"
+            type="search"
+            name="searchBox"
+            placeholder="Search Beer"
+            onChange={e => this.searchOneBeer(e)}
+            value={this.state.filterQuery}
+          />
+        </div>
         {/* <AddNewBeer getData={() => this.getAllBeers()} /> */}
         <div className="allBeers">
           {this.state.listOfBeers.map(beer => {
             return (
-              <div key={beer._id}>
+              <div className="each" key={beer._id}>
                 <Link to={`/detail/${beer._id}`}>
                   <h3 className="link">{beer.name}</h3>
                 </Link>
