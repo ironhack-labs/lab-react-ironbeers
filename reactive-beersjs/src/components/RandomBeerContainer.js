6ec84347c95533a27dd36df306a8063a4f95e384
Edgar Herera <contactoedgarherrera@gmail.com>
2019-07-19 00:20:14 -0500

    Iteration 6 | Done.

--- reactive-beersjs/src/components/RandomBeerContainer.js
+++ reactive-beersjs/src/components/RandomBeerContainer.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Header from './Header';
+import axios from 'axios';
+export default class RandomBeerContainer extends Component {
 
-const RandomBeerContainer = () => {
-  return (
-    <div>
+  state = {
+    randomBeer: {}
+  }
+
+  componentDidMount() {
+    axios.get('https://ih-beer-api.herokuapp.com/beers/random')
+      .then(({ data }) => {
+        this.setState({ randomBeer: data })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    const {randomBeer} = this.state
+    return (
+      <div>
       <Header />
-      <h1>Random Beer</h1>
+      <div className="beer-detail">
+        <img src={randomBeer.image_url} alt={randomBeer.name} />
+        <div className="randomBeer-detail-name">
+          <div>
+            <h3>{randomBeer.name}</h3>
+            <p>{randomBeer.tagline}</p>
+          </div>
+          <div>
+            <small> <b>{randomBeer.first_brewed}</b> </small>
+            <small> {randomBeer.attenuation_level} </small>
+          </div>
+        </div>
+        <p> {randomBeer.description} </p>
+        <small> {randomBeer.contributed_by} </small>
+      </div>
     </div>
-  )
+    )
+  }
 }
 
-export default RandomBeerContainer
