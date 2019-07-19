c49338baa4390a11c23374ec8adce9aa2e6f8797
Edgar Herera <contactoedgarherrera@gmail.com>
2019-07-19 01:43:26 -0500

    Iteration 7, Bonus | Done.

--- reactive-beersjs/src/components/BeersContainer.js
+++ reactive-beersjs/src/components/BeersContainer.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react'
 import Header from './Header';
 import axios from 'axios'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 class BeersContainer extends Component {
 
   state = {
-    beers: []
+    beers: [],
+    moreBeers: [],
   }
 
   componentDidMount = () => {
     axios.get('https://ih-beer-api.herokuapp.com/beers')
       .then(({ data }) => {
-        this.setState({ beers: [data[0], data[1], data[2]] })
+        this.setState({ beers: [data[0], data[1], data[2]], moreBeers: [data[0], data[1], data[2]] })
       })
       .catch(err => console.log(err))
   }
+
+  searchBeer = (beer) => {
+
+    let newArray = [...this.state.beers]
+
+    let search = newArray.filter((e) => {
+      return e.name.toLowerCase().indexOf(beer.toLowerCase()) !== -1;
+    })
+
+    this.setState({ beers: beer ? search : this.state.moreBeers })
+  }
+
+  handleChange = (e) => {
+    let value = e.target.value
+    this.searchBeer(value)    
+  }
+
   render() {
     return (
       <div>
         <Header />
         <h1>Beers Container</h1>
+        <input type="text" placeholder="Search" onChange={this.handleChange} />
         {this.state.beers.map((beer, i) => {
           return <div className="beer-item" key={i}>
             <Link to={`/beer/${beer._id}`}>
--- reactive-beersjs/src/components/NewBeerContainer.js
+++ reactive-beersjs/src/components/NewBeerContainer.js
@@ -1,12 +1,87 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Header from './Header';
-const NewBeerContainer = () => {
-  return (
-    <div>
-      <Header />
-      <h1>New beer</h1>
-    </div>
-  )
-}
+import axios from 'axios'
 
-export default NewBeerContainer
+export default class NewBeerContainer extends Component {
+
+  state = {
+    name: '',
+    tagline: '',
+    description: '',
+    first_brewed: '',
+    brewers_tips: '',
+    attenuation_level: 0,
+    contributed_by: ''
+  }
+
+  addNewBeer = (e) => {
+    e.preventDefault()
+    axios.post('https://ih-beer-api.herokuapp.com/beers/new', this.state)
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
+
+    this.setState({
+      name: '',
+      tagline: '',
+      description: '',
+      first_brewed: '',
+      brewers_tips: '',
+      attenuation_level: 0,
+      contributed_by: ''
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="form-container">
+          <form className="new-beer" onSubmit={this.addNewBeer}>
+
+            <div>
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" onChange={this.handleChange} value={this.state.name} />
+            </div>
+
+            <div>
+              <label htmlFor="tagline">Tagline</label>
+              <input type="text" name="tagline" onChange={this.handleChange} value={this.state.tagline} />
+            </div>
+
+            <div>
+              <label htmlFor="description">Description</label>
+              <input type="text" name="description" onChange={this.handleChange} value={this.state.description} />
+            </div>
+
+            <div>
+              <label htmlFor="first_brewed">First brewed</label>
+              <input type="text" name="first_brewed" onChange={this.handleChange} value={this.state.first_brewed} />
+            </div>
+
+            <div>
+              <label htmlFor="brewers_tips">Brewers tips</label>
+              <input type="text" name="brewers_tips" onChange={this.handleChange} value={this.state.brewers_tips} />
+            </div>
+
+            <div>
+              <label htmlFor="attenuation_level">Attenuation level</label>
+              <input type="number" name="attenuation_level" onChange={this.handleChange} value={this.state.attenuation_level} />
+            </div>
+
+            <div>
+              <label htmlFor="contributed_by">Contributed by</label>
+              <input type="text" name="contributed_by" onChange={this.handleChange} value={this.state.contributed_by} />
+            </div>
+
+            <button type="submit">Submit</button>
+
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
