1521f52ba4c26512c85341f211c13b3371bd26c6
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-10-03 21:15:38 +0200

    Fix the Home page. Now the beers are requested in the Beers page

--- starter-code/src/pages/Beers/Beers.jsx
+++ starter-code/src/pages/Beers/Beers.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Beers extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      allBeers: []
+    }
+  }
+  
+  componentDidMount() {
+    axios.get("http://localhost:5000/all").then(allBeers => {
+      this.setState({
+        ...this.state,
+        allBeers: allBeers.data
+      })
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, I am the beers site</h1>
+        <ul>
+          {this.state.allBeers.map((beer, idx) => {
+            return <li key={idx}>{beer.name}</li>
+          })}
+        </ul>
       </div>
     )
   }
--- starter-code/src/pages/Home/Home.jsx
+++ starter-code/src/pages/Home/Home.jsx
@@ -2,31 +2,10 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class Home extends Component {
-  constructor(props) {
-    super()
-    this.state = {
-      allBeers: []
-    }
-  }
-  
-  componentDidMount() {
-    axios.get("http://localhost:5000/all").then(allBeers => {
-      this.setState({
-        ...this.state,
-        allBeers: allBeers.data
-      })
-    })
-  }
-
   render() {
     return (
       <div>
         <h1>Hello, I am the Home site</h1>
-        <ul>
-          {this.state.allBeers.map((beer, idx) => {
-            return <li key={idx}>{beer.name}</li>
-          })}
-        </ul>
       </div>
     )
   }
