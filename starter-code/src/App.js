b1af11ff8912d716860efb0983fa6763f787b5ca
LsM Lucas Martins <lucas.smar@gmail.com>
2019-10-23 09:17:15 -0300

    iteration 3

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
+import Beers from "./Beers";
 
 class App extends Component {
   render() {
@@ -9,8 +10,8 @@ class App extends Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={Home} />
-          {/* <Route path="/beers" component={Beers} /> 
-          <Route path="/random-beer" component={Random} /> 
+          <Route path="/beers" component={Beers} />
+          {/* <Route path="/random-beer" component={Random} /> 
           <Route path="/new-beer" component={NewBeer} /> */}
         </Switch>
       </div>
---
+++ starter-code/src/Beers.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
+
+export default class Beers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: []
+    };
+    this.getBeers = this.getBeers.bind(this);
+  }
+  getBeers() {
+    Axios.get("https://ih-beers-api.herokuapp.com/beers")
+      .then(result => {
+        this.setState({ beers: result.data });
+      })
+      .catch(err => console.log(err));
+  }
+  componentDidMount() {
+    this.getBeers();
+  }
+  //  image_url
+  // name
+  // tagline
+  // contributed_by
+  render() {
+    return (
+      <div>
+        <Link to="/">Home</Link>
+        <br />
+        {this.state.beers.length !== 0
+          ? this.state.beers.map(beer => (
+              <div>
+                <img src={beer.image_url} />
+                <h2>{beer.name}</h2>
+                <h3>{beer.tagline}</h3>
+                <p>{beer.contributed_by}</p>
+                <hr />
+              </div>
+            ))
+          : null}
+      </div>
+    );
+  }
+}
