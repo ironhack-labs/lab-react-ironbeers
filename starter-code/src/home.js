6c47a2b077530197f3adc6edf5a289b60cfedf1b
gdel21 <56038834+gdel21@users.noreply.github.com>
2019-11-18 09:31:46 -0500

    done

--- starter-code/src/home.js
+++ starter-code/src/home.js
@@ -18,17 +18,17 @@ export default class Home extends React.Component {
                 <div className="all-beers whole-containers">
                     <img src={require('./images/beers.png')} className="beer-imgs" alt="all"/>
                     <Link to={'/all/'} className="allbeer-link">All Beers</Link>
-                    <p>Look at all the beers</p>
+                    <p className="homepage-p">Look at all the beers</p>
                 </div>
                 <div className="random-beer whole-containers">
                     <img src={require('./images/random-beer.png')} className="beer-imgs" alt="random"/>
                     <Link to={'/beer/' + this.randomBeer()} className="allbeer-link">Random Beer</Link>
-                    <p>Find a random beer</p>
+                    <p className="homepage-p">Find a random beer</p>
                 </div>
                 <div className="new-beer whole-containers">
                     <img src={require('./images/new-beer.png')} className="beer-imgs" alt="new"/>
                     <Link to={'/newbeer'} className="allbeer-link">New Beers</Link>
-                    <p>Input a new beer</p>
+                    <p className="homepage-p">Input a new beer</p>
                 </div>
             </div>
         )
