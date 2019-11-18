e51b198a0533b2bb0d287a937cbd3d95ac218b23
CodesGM <54691076+CodesGM@users.noreply.github.com>
2019-11-18 00:12:39 -0500

    Completed Iteration 3.

--- starter-code/src/Beers.jsx
+++ starter-code/src/Beers.jsx
@@ -5,18 +5,40 @@ import axios from "axios";
 const data = "https://ih-beers-api2.herokuapp.com/beers";
 
 export default class Beers extends React.Component {
-  componentDidMount = () => {
-    axios.get(data).then(allb => {
-      allb.data.map(each => console.log(each.name));
-    });
+  state = {
+    beers: [],
   };
+  componentDidMount = () => {
+    axios.get(data).then(allb => this.setState({ beers: allb.data }));
+  };
+  showEachBeer = () =>
+    this.state.beers.map(each => {
+      return (
+        <div>
+          <h3>
+            <Link to={`/beers/${each._id}`}>
+              {each.name}
+            </Link>
+          </h3>
+          {each.tagline} <br />
+          <img src={each.image_url} title={each.name} />
+          <br />
+          <br />
+        </div>
+      );
+    });
   render = () => {
     return (
-      <div>
-        <Link to="/">
-          <button className="btn btn-warning">Home</button>
-        </Link>
-      </div>
+      <React.Fragment>
+        <div>
+          {this.showEachBeer()}
+        </div>
+        <div>
+          <Link to="/">
+            <button className="btn btn-warning">Home</button>
+          </Link>
+        </div>
+      </React.Fragment>
     );
   };
 }
