56def56a8e2467142b7123afacb5cd0f01b18dd6
gabzafra <gab.zafra@gmail.com>
2019-12-05 19:20:43 +0100

    iteration 2

--- starter-code/src/Header/Header.js
+++ starter-code/src/Header/Header.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                SOY HEADER
-            </div>
+<Link to={"/"}>
+          <header>
+            <img src="./images/home.svg" alt="beers" />
+            
+          </header>
+        </Link>
         )
     }
 }
