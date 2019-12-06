1d7a8cf23286b04c06903a0ab9be7703d4d703ed
Sandra Tejedor delso <sandratejedordelso@MacBook-Pro-de-Sandra.local>
2019-12-06 08:53:48 +0100

    done

--- starter-code/src/components/AllBeers.js
+++ starter-code/src/components/AllBeers.js
@@ -10,7 +10,8 @@ class AllBeers extends Component {
     super();
     this._service = new Service();
     this.state = {
-      beers: []
+      beers: [],
+      search: ""
     };
   }
 
@@ -25,23 +26,43 @@ class AllBeers extends Component {
       })
       .catch(err => console.log("Error", err));
   };
+  handleSearchChange = e => this.setState({ search: e.target.value });
   render() {
+    const filterItems = () => {
+      let buscar = this.state.search;
+      return this.state.beers.filter(
+        el => el.name.toLowerCase().indexOf(buscar.toLowerCase()) > -1
+      );
+    };
     return (
       <div>
         <section>
           <Container>
             <h1>Índice de Cervezas</h1>
-            <Link className="btn btn-sm btn-dark" to={'/new'}>
+            <Link className="btn btn-sm btn-dark" to={"/new"}>
               Añadir nueva cerveza
             </Link>
+            <br></br>
+            <br></br>
+            <div className="form-group">
+              <label htmlFor="search">Search</label>
+              <input
+                name="search"
+                type="text"
+                className="form-control"
+                id="search"
+                value={this.state.search}
+                onChange={this.handleSearchChange}
+              />
+            </div>
+            <br></br>
             {console.log(this.state.beers)}
             <Row>
-              {this.state.beers.map(beers => (
+              {filterItems().map(beers => (
                 <BeerCard key={beers._id} {...beers} />
               ))}
             </Row>
           </Container>
-
         </section>
       </div>
     );
