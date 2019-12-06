4c9d49e7c84f4a44ba5210daf0919edc74415485
pabloPeralta <paps21.gol@gmail.com>
2019-12-06 00:45:42 -0600

    remove search bar

--- starter-code/src/pages/AllBeers/index.js
+++ starter-code/src/pages/AllBeers/index.js
@@ -1,3 +1,5 @@
+///origininal si barra de busqueda
+
 import React, {Component} from 'react';
 import axios from 'axios';
 import CardComponent from './CardComponent'
@@ -6,54 +8,22 @@ const ENDPOINT = 'https://ih-beers-api2.herokuapp.com/beers';
 class AllBeers extends Component {
 
   state = {
-      beers: [],
-      filterBeer:[],
-      search:""
+      beers: []
     }
 
     async componentDidMount(){
       const {data} = await axios.get (ENDPOINT)
       this.setState({ beers: [...data] })
     }
-
-
-    filterBeer =  ({ target: { name, value } }) => {
-
-      const SEARCHPOINT = "https://ih-beers-api2.herokuapp.com/beers/search?q={query}"
-      const expr = new RegExp(value, 'ig')
-      this.setState({
-        ...this.state,
-        [name]: value,
-        filterBeer: this.state.beer.SEARCHPOINT(beer => beer.name.match(expr))
-      })
-    }
-
-
-
-
     render(){
-      const { beers, filterBeer } = this.state;
+      const { beers } = this.state;
       return(
         <>
             <h1 style={{
                 marginTop: "100px"}}>
                 Lista de cervezas
             </h1>
-
-            <input
-          type="text"
-          name="search"
-          id="search"
-          onChange={this.filterBeer}
-          placeholder="🔍 search"
-        />  
-
-            
-        {
-
-          this.state.search.length === 0 ? 
-
-          (beers.map((beer, i) => (
+        {beers.map((beer, i) => (
           <CardComponent
             key = {i}
             route={beer._id}
@@ -62,27 +32,7 @@ class AllBeers extends Component {
             description = {beer.description}
             contributor = {beer.contributed_by}
           />
-        )))
-
-        :
-            (filterBeer.map((beer, i) => 
-            <CardComponent
-            key = {i}
-            route={beer._id}
-            image={beer.image_url}
-            name = {beer.name}
-            description = {beer.description}
-            contributor = {beer.contributed_by}
-          />
-            ))
-            
-
-
-
-        }    
-
-
-        
+        ))}
         </>
       )
     }
