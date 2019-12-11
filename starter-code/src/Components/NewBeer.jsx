921ad123c9d17d853f3bc19475f39b73db60efc9
Lissy Gundersen <user@Users-MacBook-Pro-2.local>
2019-12-11 14:57:30 +0000

    finished everything

--- starter-code/src/Components/NewBeer.jsx
+++ starter-code/src/Components/NewBeer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import { addBeer } from './../services/beer';
 
 import {
 	BrowserRouter,
@@ -15,36 +16,28 @@ class NewBeer extends Component {
 		this.state = {
 			name: '',
 			tagline: '',
-			descrption: '',
+			description: '',
 			first_brewed: '',
 			attenuation_level: '',
 			contributed_by: ''
 		};
+		this.myChangeHandler = this.myChangeHandler.bind(this);
+		this.addBeerToList = this.addBeerToList.bind(this);
 	}
 
-	myChangeHandler = event =>
+	myChangeHandler = event => {
+		// when you use an arrow function, you also bind, but you shouldn't do it like this, keep binding in your constructor
+		const name = event.target.name; // target ALL the names from the form it is connected to
+		const value = event.target.value; // same thing as above, but for the values
 		this.setState({
-			name: event.target.value1,
-			tagline: event.target.value2,
-			descrption: event.target.value3,
-			first_brewed: event.target.value4,
-			attenuation_level: event.target.value5,
-			contributed_by: event.target.value6
+			[name]: value // [name] with square braces takes into account every possible name input
 		});
+	};
 
 	addBeerToList(event) {
 		event.preventDefault();
-		const beerName = this.state.name;
-		console.log('Here are values' + beerName);
-		const beers = [...this.state.beers, beerName];
-
-		this.setState({
-			beers: [],
-			name: '',
-			tagline: '',
-			name: '',
-			name: ''
-		});
+		console.log(this.state);
+		addBeer(this.state);
 	}
 
 	render() {
@@ -57,7 +50,7 @@ class NewBeer extends Component {
 					<input
 						type="text"
 						name="name"
-						value1={this.state.name}
+						value={this.state.name}
 						onChange={this.myChangeHandler}
 					/>
 
@@ -65,7 +58,7 @@ class NewBeer extends Component {
 					<input
 						type="text"
 						name="tagline"
-						value2={this.state.tagline}
+						value={this.state.tagline}
 						onChange={this.myChangeHandler}
 					/>
 
@@ -73,7 +66,7 @@ class NewBeer extends Component {
 					<input
 						type="text"
 						name="description"
-						value3={this.state.description}
+						value={this.state.description}
 						onChange={this.myChangeHandler}
 					/>
 
@@ -89,7 +82,7 @@ class NewBeer extends Component {
 					<input
 						type="number"
 						name="attenuation_level"
-						value5={this.state.attenuation_level}
+						value={this.state.attenuation_level}
 						onChange={this.myChangeHandler}
 					/>
 
@@ -97,7 +90,7 @@ class NewBeer extends Component {
 					<input
 						type="text"
 						name="contributed_by"
-						value6={this.state.contributed_by}
+						value={this.state.contributed_by}
 						onChange={this.myChangeHandler}
 					/>
 
--- starter-code/src/services/beer.js
+++ starter-code/src/services/beer.js
@@ -42,3 +42,20 @@ export const load = data =>
         reject(error);
       });
   });
+
+// TODO ====> NEW SERVICE TO DO AN AXIOS.POST() THAT RECEIVES THE DATA AS AN ARGUMENT AND ADD IT TO THE API
+    export const addBeer = data =>
+  new Promise((resolve, reject) => {
+    axios
+      .post(`https://ih-beers-api2.herokuapp.com/beers/new`, data)
+      .then(response => {
+        console.log("CREATED BEER", response.data);
+        resolve(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+        reject(error);
+      });
+  });
+
+  
