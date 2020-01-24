a7f32c74c44367bc37eebfdb9674edcf937d7b5a
Juliane Arruda <juliane_ch.arruda@live.com>
2020-01-24 18:27:48 -0300

    implementing iteration 1

---
+++ starter-code/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import React from 'react';
+
+const NavBar = () => (
+<div className="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+  <ul className="navbar-nav">
+    <li className="nav-item active">
+      <a className="nav-link" href="#">Home <span className="sr-only">(atual)</span></a>
+    </li>
+  </ul>
+</div>
+)
+
+export default NavBar;
