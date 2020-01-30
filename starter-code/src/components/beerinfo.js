e536d901739ec1a57d3ad8dffa529e1b68c09ed7
Strapak, Sergej <strapak@mediamarktsaturn.com>
2020-01-30 15:01:14 +0100

    Iteration 3 ?

---
+++ starter-code/src/components/beerinfo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class beerinfo extends Component {
+    render() {
+        return (
+            <div>
+                some Beer Info here
+            </div>
+        )
+    }
+}
--- starter-code/src/components/beers.js
+++ starter-code/src/components/beers.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom'
 
 export default class beers extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            beers: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get("https://ih-beers-api2.herokuapp.com/beers")
+            .then(response => {
+                this.setState({beers: response.data})
+            })
+    }
+
     render() {
         return (
-            <div>
-                <h1>All Beers</h1>
-                <Link to='/'>Back to Main Page</Link>
-            </div>
+            <div ClassName="ImportantDivForReturnOtherwiseItWontWorkForWhateverReason">
+            {
+                this.state.beers.map((beer, index) => 
+                    <div key={index} ClassName="Beers">
+                        <div ClassName="Infos">
+                            <Link to={`/beers/:beerId`} ClassName="BeerDetails"> {beer.name} </Link><br/>
+                            {beer.tagline}<br/>
+                            Created by: <b>{beer.contributed_by.split(" ").slice(0, 2).join(' ')}</b><br/>
+                        </div>
+
+                        <div ClassName="Image">
+                            <img src={beer.image_url} alt="NoPic" />
+                        </div>
+                    </div>
+                    
+                    
+                )
+            }
+            </div>        
         )
     }
 }
