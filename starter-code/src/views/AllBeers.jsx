6bd2426ba52156b46f6f0fccc263162868b6451c
Alex Meyer <alexandre.meyer78@gmail.com>
2020-02-21 10:35:03 +0100

    Add search bar

--- starter-code/src/views/AllBeers.jsx
+++ starter-code/src/views/AllBeers.jsx
@@ -8,20 +8,37 @@ class AllBeers extends Component {
     beers : []
   }
 
-  componentDidMount() {
-    axios.get("https://ih-beers-api2.herokuapp.com/beers")
+  onSearch(e) {
+    this.apiCall(e.target.value.toLowerCase())
+  }
+
+  apiCall(query) {
+    let baseUrl = "https://ih-beers-api2.herokuapp.com/beers";
+    let endPoint = !!query ? `/search?q=${query}` : '';
+    axios.get(baseUrl + endPoint)
     .then((results) => {
       this.setState({beers:results.data})
     }).catch(err => console.log("Error : ", err))
   }
 
+  componentDidMount() {
+    this.apiCall()
+  }
+
   render() {
     return (
-      <div className="page">
+      <div className="page allBeers">
+        <div className="marginContainer searchBar">
+          <input type="text" placeholder="Filter beers by name..." onChange={(e) => this.onSearch(e)} />
+        </div>
+
         <div className="marginContainer">
-          {this.state.beers.map((b, i) => (
+          { this.state.beers.length > 0 ?
+            this.state.beers.map((b, i) => (
             <BeerCard beer={b} key={i}/>
-          ))}
+          ))
+          : <p>Loading beers ...</p>
+          }
         </div>
       </div>
     )
