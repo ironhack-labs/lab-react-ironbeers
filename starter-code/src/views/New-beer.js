5fc9f402f9bb7de7f003c8edcc79c10be421892e
franck-treboit <ftreboit@nexity.fr>
2020-02-21 09:32:59 +0100

    v1.0

--- starter-code/src/views/New-beer.js
+++ starter-code/src/views/New-beer.js
@@ -1,10 +1,85 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from "axios"; //  npm i axios
 
-export default function NewBeer() {
-    return (
-        <div className="page">
-            <h1 className="title">New-beer</h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quidem! Itaque, eius nostrum. Odio voluptatem dolore a cum. Quae in ipsa rem magni harum sapiente, porro at accusantium corporis id!
-        </div>
-    )
+export default class NewBeer extends Component {
+    state = {
+        beers: [],
+        APIEndpoint:
+            "https://ih-beers-api2.herokuapp.com/beers/new",
+        name:'',
+        tagline: '',
+        description: '',
+        first_brewed: '',
+    };
+
+    handleChangeName = event => {
+        this.setState({ name: event.target.value });
+    }
+    handleChangeDesc  = event => {
+        this.setState({ description: event.target.value });
+    }
+    handleChangeTag = event => {
+        this.setState({ tagline: event.target.value });
+    }
+    handleChangeFirst = event => {
+        this.setState({ first_brewed: event.target.value });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const newBeer = {
+            "image_url": "https://images.punkapi.com/v2/keg.png",
+            "name": this.state.name,
+            "description": this.state.description,
+            "tagline": this.state.tagline,
+            "first_brewed": this.state.first_brewed
+        }
+
+        axios
+            .post(this.state.APIEndpoint, newBeer)
+            .then(apiRes =>  {
+                console.log("________________________");
+                console.log(apiRes);
+                console.log(apiRes.data);
+                console.log("________________________");
+            })
+
+            .catch(apiErr => console.error(apiErr));
+    }
+
+    render() {
+        return  (
+            <div className="page">
+                <h1 className="title">New Beer</h1>
+
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                        Beer Name:
+                        <input type="text" name="name" onChange={this.handleChangeName} />
+                        </label>
+                        <label>
+                            Beer first brewed:
+                            <input type="text" name="first_brewed" onChange={this.handleChangeFirst} />
+                        </label>
+                        <label>
+                            Beer tag line:
+                            <input type="text" name="tagline" onChange={this.handleChangeTag} />
+                        </label>
+                        <label>
+                            Beer description:
+                            <input type="text" name="description" onChange={this.handleChangeDesc} />
+                        </label>
+                        <button type="submit">Add</button>
+                    </form>
+                </div>
+
+            </div>
+        )
+
+
+    }
 }
+
+
+
--- starter-code/src/views/Random-beer.js
+++ starter-code/src/views/Random-beer.js
@@ -1,10 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from "axios"; //  npm i axios
 
-export default function RandomBeer() {
-    return (
-        <div className="page">
-            <h1 className="title">Random-beer</h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quidem! Itaque, eius nostrum. Odio voluptatem dolore a cum. Quae in ipsa rem magni harum sapiente, porro at accusantium corporis id!
-        </div>
-    )
+export default class RandomBeer extends Component {
+    state = {
+        beers: [],
+        APIEndpoint:
+            "https://ih-beers-api2.herokuapp.com/beers",
+        oneBeer: []
+    };
+
+    componentDidMount() {
+
+        axios
+            .get(this.state.APIEndpoint)
+            .then(apiRes =>  {
+                let min= 0;
+                let max =  apiRes.data.length;
+                let randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
+                this.setState({ oneBeer: apiRes.data[randomValue] });
+                console.log(apiRes.data[randomValue]);
+            })
+
+            .catch(apiErr => console.error(apiErr));
+
+    }
+
+    render() {
+
+        return  (
+
+            <div className="page">
+                <h1 className="title">Beer</h1>
+                <div className="beer1">
+                    <div  className="beer11"> <img src={this.state.oneBeer.image_url} className="img-beer" /> </div>
+                    <div  className="beer12">
+                        <ul>
+                            <li>
+                                {this.state.oneBeer.name}
+                            </li>
+                            <li>
+                                {this.state.oneBeer.tagline}
+                            </li>
+                            <li>
+                                {this.state.oneBeer.contributed_by}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+
+
+    }
 }
+
+
+
