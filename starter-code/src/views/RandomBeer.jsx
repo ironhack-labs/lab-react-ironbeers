81961fc671f0a20b93e03591521362f019d2b7b9
ina <mgmuniza@gmail.com>
2020-02-21 10:37:19 +0100

    Iteration 5 | OK

--- starter-code/src/views/RandomBeer.jsx
+++ starter-code/src/views/RandomBeer.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import APIEndpoint from "../api/Handler"
+import "./../styles/SingleBeer.css"
 
-export default function RandomBeer() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default class SingleBeer extends Component {
+
+    state = {
+        beer:{}
+    }
+
+    api = new APIEndpoint()
+
+    componentDidMount() {
+        this.api
+          .get(`/beers/random`)
+          .then(apiRes => this.setState({beer: apiRes.data}))
+          .catch(apiErr => console.error(apiErr));
+      }
+
+    render() {
+        return this.state.beer? (
+            <div className="oneBeer-cont">
+                <img src={this.state.beer.image_url} alt='beer'/>
+                <tbody className="table">
+                    <tr>
+                        <td><h4>{this.state.beer.name}</h4></td>
+                        <td><h4 style={{textAlign:"right"}}>{this.state.beer.attenuation_level}</h4></td>
+                    </tr>
+                    <tr>
+                        <td>{this.state.beer.tagline}</td>
+                        <td>{this.state.beer.first_brewed}</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="2" >{this.state.beer.description}</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="2" >{this.state.beer.contributed_by}</td>
+                    </tr>
+                </tbody>
+            </div>
+        ):
+        <p>Lodding...</p>
+    }
 }
