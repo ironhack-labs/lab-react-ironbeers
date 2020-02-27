5636f7eeaabc811db3a0e36a3beb45277f2f34b7
almonasterio <a.monasterio.a@gmail.com>
2020-02-27 23:29:15 +0100

    routes set

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home.js";
+import Beers from "./components/Beers.js";
+import RandomBeer from "./components/RandomBeer.js";
+import NewBeer from "./components/NewBeer.js";
+import OneBeer from "./components/OneBeer.js";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/beers" component={Beers} />
+          <Route path="/random-beer" component={RandomBeer} />
+          <Route path="/new-beer" component={NewBeer} />
+          <Route path="/beers/:id" component={OneBeer} />
+        </Switch>
       </div>
     );
   }
 }
-
 export default App;
---
+++ starter-code/src/components/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Link to="/">
+          <div className="header">
+            <img
+              className="headerlogo"
+              src="https://png.pngtree.com/svg/20161111/home_white_1319461.png"
+              alt="home"
+            />
+          </div>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default Header;
