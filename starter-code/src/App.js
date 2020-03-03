d8fd41ffe00a49f4edd1d28c8689c11d1c6f1f71
Ardash <ardache@hotmail.com>
2020-03-03 12:29:22 -0600

    iteration :one: Done!!

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import beersall from './images/beers.png';
+import randombeer from './images/random-beer.png';
+import newbeer from './images/new-beer.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <ul>
+          <img src={beersall} alt="Beers" height="140" width="400"/>
+          <li>All Beers</li>
+          <p>Ldasjd djsd skd dieieur  dhjfd eiurei s dsadu aisduasd iadu aisdaid iaud asiduasdy f asgag datasdga d tafdt adta tfa sdsa</p>
+          <img src={randombeer} alt="Beers" height="140" width="400"/>
+          <li>Random Beers</li>
+          <p>Ldasjd djsd skd dieieur  dhjfd eiurei s dsadu aisduasd iadu aisdaid iaud asiduasdy f asgag datasdga d tafdt adta tfa sdsa</p>
+          <img src={newbeer} alt="Beers" height="140" width="400"/>
+          <li>New Beers</li>
+          <p>Ldasjd djsd skd dieieur  dhjfd eiurei s dsadu aisduasd iadu aisdaid iaud asiduasdy f asgag datasdga d tafdt adta tfa sdsa</p>
+        </ul>
       </div>
     );
   }
