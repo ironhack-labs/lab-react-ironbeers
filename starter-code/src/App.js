2460c8dabbffb405e707e0d8145761241ff5cd6b
mario010203 <mariojose1490@hotmail.com>
2020-03-17 17:21:49 +0100

    parcel working

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import "./App.css";
 
+const App = () => (
+  <div>
+    <p>holas</p>
+  </div>
+);
 export default App;
