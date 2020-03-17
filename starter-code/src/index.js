9eee5ff9fa1667e23514b17b7930f72acd5bce61
Ivan Esteban <ivan.estebanisabel@gmail.com>
2020-03-17 20:14:14 +0100

    start with parcel

--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { App } from "./App";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+});
