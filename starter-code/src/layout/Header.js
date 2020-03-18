93a41da1b3e272d7269c5db1113b4858c7295516
mario010203 <mariojose1490@hotmail.com>
2020-03-18 23:38:20 +0100

    image logo added

--- starter-code/src/layout/Header.js
+++ starter-code/src/layout/Header.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
+import logo from "/public/images/house.svg";
 
 export const Header = () => {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="/">
-          <img
-            alt=""
-            src="/public/images/house.png"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />
+          <div class="nav-img">
+            <img
+              alt="home"
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+          </div>
         </Navbar.Brand>
       </Navbar>
     </>
