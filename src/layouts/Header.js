cff9d66918d5fec826e705f6601706cf807cf277
vctorChamizo <victor.chamizo96@gmail.com>
2020-03-18 19:43:25 +0100

    Add header component

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,3 +1,13 @@
 import React from "react";
 
-export const Header = () => <header></header>;
+import { Link } from "react-router-dom";
+
+import home from "../../public/icon/home.svg";
+
+export const Header = () => (
+  <header className="card-header bg-primary">
+    <Link to={"/"} className="link-header">
+      <img src={home} alt="Home Icon" className="home-icon"></img>
+    </Link>
+  </header>
+);
