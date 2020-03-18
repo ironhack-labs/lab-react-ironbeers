0deae5817d610337dfd148fbdbc93e2ac6d34de8
vctorChamizo <victor.chamizo96@gmail.com>
2020-03-18 20:46:04 +0100

    Add list beers - incomplete

--- src/pages/List.page.js
+++ src/pages/List.page.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { getBeers } from "../../lib/beer.api";
+
 export const ListPage = () => {
+  const list = getBeers();
+  console.log(list);
   return <p>LIST</p>;
 };
