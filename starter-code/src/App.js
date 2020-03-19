8345b4de5cbcdcb7079d28a42c502b442d5126af
Borja Domínguez <borja.d.atienza@gmail.com>
2020-03-19 20:15:04 +0100

    Router done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { Home } from "./pages/Home.page";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export const App = () => {
-  return <h1>Hola</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+      </Switch>
+    </Router>
+  );
 };
