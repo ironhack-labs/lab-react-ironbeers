6b9cd253bec115a1ce7178bd259725301e8546bf
David Garcia <dgarciabravo13@gmail.com>
2020-03-19 23:20:35 +0100

    iteration 2 done

--- starter-code/src/components/Navbar.js
+++ starter-code/src/components/Navbar.js
@@ -1,14 +1,26 @@
 import React from 'react';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
+import logo from './../images/Home-icon.png'
+
+const Nav = styled.nav`
+width:100vw;
+height:45px;
+position:absolute;
+display:flex;
+justify-content:center;
+align-items:center;
+background-color:#415DEB;
+color:white;
+`;
+
 
 const Navbar = () => {
 
   return(
-    <nav>
-      <div>
-        <p>Hola Mundo</p>
-      </div>
-    </nav>
+    <Nav>
+      <Link to="/"><span><img width="40px" src={logo} alt="logo"/></span></Link> 
+    </Nav>
   );
 }
 
