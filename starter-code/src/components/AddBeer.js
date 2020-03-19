b52c778ceb5352f832db0b1b7c967a41d2962829
Ana Segarra <anasegarraber@gmail.com>
2020-03-19 23:25:44 +0100

    Style form page

--- starter-code/src/components/AddBeer.js
+++ starter-code/src/components/AddBeer.js
@@ -2,6 +2,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+// styled components
+import { Form } from '../styles/Form';
+
 export const AddBeer = () => {
   const [beer, setNewBeer] = useState({
     name: '',
@@ -27,59 +30,77 @@ export const AddBeer = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name: </label>
-      <input
-        type="text"
-        id="name"
-        value={beer.name}
-        onChange={e => setNewBeer({ ...beer, name: e.target.value })}
-      />
-      <label htmlFor="Tagline">Tagline: </label>
-      <input
-        type="text"
-        id="Tagline"
-        value={beer.tagline}
-        onChange={e => setNewBeer({ ...beer, tagline: e.target.value })}
-      />
-      <label htmlFor="Description">Description: </label>
-      <input
-        type="text"
-        id="Description"
-        value={beer.description}
-        onChange={e => setNewBeer({ ...beer, description: e.target.value })}
-      />
-      <label htmlFor="first_brewed">Date of first brew: </label>
-      <input
-        type="text"
-        id="first_brewed"
-        value={beer.first_brewed}
-        onChange={e => setNewBeer({ ...beer, first_brewed: e.target.value })}
-      />
-      <label htmlFor="brewers_tips">Some tips for brewers: </label>
-      <input
-        type="text"
-        id="brewers_tips"
-        value={beer.brewers_tips}
-        onChange={e => setNewBeer({ ...beer, brewers_tips: e.target.value })}
-      />
-      <label htmlFor="attenuation_level">Level of attenuation: </label>
-      <input
-        type="number"
-        id="attenuation_level"
-        value={beer.attenuation_level}
-        onChange={e =>
-          setNewBeer({ ...beer, attenuation_level: e.target.value })
-        }
-      />
-      <label htmlFor="contributed_by">Created by: </label>
-      <input
-        type="text"
-        id="contributed_by"
-        value={beer.contributed_by}
-        onChange={e => setNewBeer({ ...beer, contributed_by: e.target.value })}
-      />
-      <button type="submit">Add beer!</button>
-    </form>
+    <Form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          id="name"
+          value={beer.name}
+          onChange={e => setNewBeer({ ...beer, name: e.target.value })}
+        />
+      </div>
+      <div>
+        <label htmlFor="Tagline">Tagline: </label>
+        <input
+          type="text"
+          id="Tagline"
+          value={beer.tagline}
+          onChange={e => setNewBeer({ ...beer, tagline: e.target.value })}
+        />
+      </div>
+      <div>
+        <label htmlFor="Description">Description: </label>
+        <input
+          type="text"
+          id="Description"
+          value={beer.description}
+          onChange={e => setNewBeer({ ...beer, description: e.target.value })}
+        />
+      </div>
+      <div>
+        <label htmlFor="first_brewed">Date of first brew: </label>
+        <input
+          type="text"
+          id="first_brewed"
+          value={beer.first_brewed}
+          onChange={e => setNewBeer({ ...beer, first_brewed: e.target.value })}
+        />
+      </div>
+      <div>
+        <label htmlFor="brewers_tips">Some tips for brewers: </label>
+        <input
+          type="text"
+          id="brewers_tips"
+          value={beer.brewers_tips}
+          onChange={e => setNewBeer({ ...beer, brewers_tips: e.target.value })}
+        />
+      </div>
+      <div>
+        <label htmlFor="attenuation_level">Level of attenuation: </label>
+        <input
+          type="number"
+          id="attenuation_level"
+          value={beer.attenuation_level}
+          onChange={e =>
+            setNewBeer({ ...beer, attenuation_level: e.target.value })
+          }
+        />
+      </div>
+      <div>
+        <label htmlFor="contributed_by">Created by: </label>
+        <input
+          type="text"
+          id="contributed_by"
+          value={beer.contributed_by}
+          onChange={e =>
+            setNewBeer({ ...beer, contributed_by: e.target.value })
+          }
+        />
+      </div>
+      <div className="btn-container">
+        <button type="submit">Add beer!</button>
+      </div>
+    </Form>
   );
 };
---
+++ starter-code/src/styles/Form.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding: 3.5em;
+  color: #172c66;
+  div {
+    display: flex;
+    margin: 1em;
+    width: 30%;
+  }
+  label {
+    font-weight: 700;
+    margin: 1em;
+    min-width: fit-content;
+  }
+  input {
+    color: #172c66;
+    font-size: 1rem;
+    padding: 0.5em;
+    border-radius: 2em;
+    border: 0.1em solid #8bd3dd;
+    outline: none;
+    width: 100%;
+    &:focus {
+      box-shadow: 0 0 0.3em 0.2em #8bd3dd;
+    }
+  }
+  div.btn-container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    button {
+      border: 0.1em solid #f582ae;
+      color: #172c66;
+      padding: 0.5em 1em;
+      font-size: 1.2rem;
+      cursor: pointer;
+      border-radius: 1em;
+      outline: none;
+      &:hover {
+        background-color: #f582ae;
+        color: #fff;
+      }
+      &:focus {
+        box-shadow: 0 0 0.3em 0.2em #f582ae;
+      }
+    }
+  }
+
+  @media (max-width: 1024px) {
+    div {
+      flex-direction: column;
+      width: 45%;
+    }
+    label {
+      margin-bottom: 0.4em;
+      margin-left: 1.5em;
+    }
+    input {
+      padding: 1.2em;
+      width: auto;
+    }
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    div {
+      width: 100%;
+    }
+  }
+`;
