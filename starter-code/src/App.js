09b2466539e6a83f55258199d8ccdc5f90b16af6
Fran <fmjerez@icloud.com>
2020-03-19 12:59:41 +0100

    Iteration 3 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,16 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "../public/styles/App.css";
 import Home from "./Home";
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 3em;
-  align-items: center;
-  flex-direction: column-reverse;
-`;
 
 export const App = () => {
   return <Home />;
