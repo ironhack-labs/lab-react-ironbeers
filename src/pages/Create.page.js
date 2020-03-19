06a1dad49aceac13e75282495860eeb4f1e44336
vctorChamizo <victor.chamizo96@gmail.com>
2020-03-19 20:29:32 +0100

    Add create new beer

--- src/pages/Create.page.js
+++ src/pages/Create.page.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export const CreatePage = () => {
-  return <p>CREATE</p>;
-};
+import { FromCreate } from "../components/FormCreate";
+import { withRouter } from "react-router-dom";
+
+import { createBeer } from "../../lib/beer.api";
+
+export const CreatePage = withRouter(({ history }) => {
+  const handleSumbit = async data => {
+    const res = await createBeer(data);
+    if (res.status === 200) history.push("/");
+    else console.log(res.data.message);
+  };
+
+  return <FromCreate setSubmit={data => handleSumbit(data)} />;
+});
