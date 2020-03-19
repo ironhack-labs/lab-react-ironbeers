3740934ff26d84bc0a20e5641528d11f252ba487
vctorChamizo <victor.chamizo96@gmail.com>
2020-03-19 10:54:16 +0100

    Add bug Run Time exception

--- src/pages/List.page.js
+++ src/pages/List.page.js
@@ -3,7 +3,9 @@ import React from "react";
 import { getBeers } from "../../lib/beer.api";
 
 export const ListPage = () => {
-  const list = getBeers();
-  console.log(list);
+  getBeers().then(e => {
+    console.log(e);
+    return null;
+  });
   return <p>LIST</p>;
 };
