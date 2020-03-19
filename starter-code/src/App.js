144130027d854af519a47d2ffa3791f43b45624f
Borja Domínguez <borja.d.atienza@gmail.com>
2020-03-19 19:49:17 +0100

    Error to start

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export const App = () => {
   return <h1>Hola</h1>;
---
+++ starter-code/src/pages/Home.page.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export const Home = () => {
+  return <div>Esta es la HOME</div>;
+};
