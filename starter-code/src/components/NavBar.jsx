c2766b2d995d4e10d2eec66c9152c7a73d5e1b25
Román Méndez <mendezbeck@gmail.com>
2020-03-19 15:10:46 +0100

    iteration 3 done

--- starter-code/src/components/NavBar.jsx
+++ starter-code/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import navBarImage from "../../public/images/beers-home.png";
 const NavBar = () => {
   return (
     <Link to="/">
-      <img src={navBarImage} alt="header" />
+      <img className="navbar-img" src={navBarImage} alt="header" />
     </Link>
   );
 };
