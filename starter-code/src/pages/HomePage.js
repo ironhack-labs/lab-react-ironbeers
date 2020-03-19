be7d76672b6d77bb12ab41f1dd1c41b106b72ee6
J. A. Hernández <joseanher81@gmail.com>
2020-03-19 19:23:30 +0100

    Home Page estructure

---
+++ starter-code/src/pages/HomePage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const HomePage = () => {
+
+  return(
+    <div>
+      <img src={'./images/beers.png'} alt='Beers'></img>
+      <h1>All Beers</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis velit ut risus pulvinar hendrerit eget et eros. Sed quis arcu nulla. Suspendisse ullamcorper eu velit et malesuada.</p>
+      <img src={'./images/random-beer.png'} alt='Beers'></img>
+      <h1>Random Beer</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis velit ut risus pulvinar hendrerit eget et eros. Sed quis arcu nulla. Suspendisse ullamcorper eu velit et malesuada.</p>
+      <img src={'./images/new-beer.png'} alt='Beers'></img>
+      <h1>New Beer</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis velit ut risus pulvinar hendrerit eget et eros. Sed quis arcu nulla. Suspendisse ullamcorper eu velit et malesuada.</p>
+    </div>
+
+  );
+}
+
+export default HomePage;
