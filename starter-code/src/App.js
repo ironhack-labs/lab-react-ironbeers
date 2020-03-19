28b642c73dae2c697b04f7aec0c51b4093b60e8b
Miguel <alonsogalalrdomiguel@gmail.com>
2020-03-19 12:10:30 +0100

    iteration 4 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Beer } from "./pages/Beers";
 import { RandomBeer } from "./pages/Random-beer";
+import { BeerDetail } from "./pages/BeerDetail";
 import { NewBeer } from "./pages/New-beer";
 
 export const App = () => {
@@ -11,6 +12,7 @@ export const App = () => {
       <Switch>
         <Route exact path="/" component={Home}></Route>
         <Route exact path="/beers" component={Beer}></Route>
+        <Route exact path="/beer/:id" component={BeerDetail}></Route>
         <Route exact path="/random-beer" component={RandomBeer}></Route>
         <Route exact path="/new-beer" component={NewBeer}></Route>
       </Switch>
