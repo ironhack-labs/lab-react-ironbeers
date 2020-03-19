e62d2ed0b71e925d85be1ce19725835b29c733d3
Miguel <alonsogalalrdomiguel@gmail.com>
2020-03-19 12:33:00 +0100

    iteration 5 done

--- starter-code/src/pages/Random-beer.js
+++ starter-code/src/pages/Random-beer.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Header } from "../components/Header";
 
-export const RandomBeer = () => {
+export const RandomBeer = props => {
+  const { id } = props.match.params;
+  const [beer, setBeer] = useState([]);
+
+  const listAllBeers = async () => {
+    const res = await axios.get(
+      `https://ih-beers-api2.herokuapp.com/beers/random`
+    );
+    return res.data;
+  };
+
+  useEffect(() => {
+    listAllBeers().then(beer => setBeer(beer));
+  }, []);
+
   return (
     <div>
       <Header></Header>
-      <h1>RANDOMBEER</h1>;
+      <div>
+        <p>{beer.name}</p>
+        <img src={beer.image_url} alt={beer.name}></img>
+        <p>{beer.tagline}</p>
+        <p>{beer.first_brewed}</p>
+        <p>{beer.attenuation_level}</p>
+        <p>{beer.description}</p>
+        <p>{beer.contributed_by}</p>
+      </div>
     </div>
   );
 };
+
+// export const RandomBeer = () => {
+//   return (
+//     <div>
+//       <Header></Header>
+//       <h1>RANDOMBEER</h1>;
+//     </div>
+//   );
+// };
