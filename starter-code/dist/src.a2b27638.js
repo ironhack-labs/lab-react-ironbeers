6199849f31b661d2a2f3934d44720ca7bd5c0699
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2020-03-19 17:48:08 +0100

    changes

--- starter-code/dist/src.a2b27638.js
+++ starter-code/dist/src.a2b27638.js
@@ -35582,7 +35582,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60259" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62197" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
