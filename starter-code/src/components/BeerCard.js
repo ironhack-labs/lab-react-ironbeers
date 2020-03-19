d42f23f9eb13fe950f7562464bf828240d1508b5
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-03-19 12:38:38 +0100

    Sticky navbar and layout of the beer list

---
+++ starter-code/src/components/BeerCard.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  align-self: center;
+  flex-direction: row;
+  align-items: center;
+  height: 200px;
+  width: 100%;
+  padding: 20px 0 40px;
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 0 20px;
+  h4 {
+    color: #000;
+    padding: 0;
+    margin: 1rem 20px 0;
+    font-size: 2rem;
+    font-weight: 400;
+  }
+  p {
+    color: #ccc;
+    padding: 0;
+    margin: 0px 20px;
+    font-size: 1.5rem;
+  }
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+`;
+
+const ImageContainer = styled.div`
+  width: 20%;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  height: 100%;
+`;
+
+export const BeerCard = props => {
+  console.log(props);
+  return (
+    <Container>
+      <ImageContainer>
+        <Image src={props.image_url} />
+      </ImageContainer>
+      <Wrapper>
+        <h4>{props.name}</h4>
+        <p>{props.tagline}</p>
+      </Wrapper>
+    </Container>
+  );
+};
--- starter-code/src/pages/Beer.page.js
+++ starter-code/src/pages/Beer.page.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Layout } from "../layouts/Layout";
-// import styled from "styled-components";
+import axios from "axios";
+import { BeerCard } from "../components/BeerCard";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 60%;
+  margin: 100px auto 0;
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+`;
+
+const api = axios.create({
+  baseURL: "https://ih-beers-api2.herokuapp.com"
+});
 
 export const BeerPage = () => {
+  const [beers, setBeers] = useState();
+
+  useEffect(() => {
+    api.get("/beers").then(res => {
+      const data = res.data;
+      setBeers(data);
+    });
+  }, []);
+  console.log(beers);
   return (
     <Layout>
-      <div>Hello</div>
+      <Container>
+        {beers &&
+          beers.map((e, i) => {
+            return <BeerCard key={i} {...e}></BeerCard>;
+          })}
+      </Container>
     </Layout>
   );
 };
+
+/*{
+  beers.map((e, i) => {
+    return <BeerCard {...e}></BeerCard>;
+  });
+}*/
