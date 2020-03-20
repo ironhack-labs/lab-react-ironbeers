e6896b288c5b69de810b65a7963d3a63236550c2
Teresa Pelinski <teresapelinski@gmail.com>
2020-03-20 17:13:54 +0100

    deleted header from layout since it must not be rendered at /

--- starter-code/src/layouts/Layout.js
+++ starter-code/src/layouts/Layout.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from "./Header";
 import Footer from "./Footer"
 
 const Layout = ({ children }) => (
     <>
-        <Header />
         <section className="container">{children}</section>
         <Footer />
     </>
