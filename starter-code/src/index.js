acf0a65fd156420ee68c351fea4a4d9a5c5fa9b1
Miguel <alonsogalalrdomiguel@gmail.com>
2020-03-20 13:57:41 +0100

    iteration 4 and 5 complete

--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./App.css";
 import { App } from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
--- starter-code/src/services/GetBeer.js
+++ starter-code/src/services/GetBeer.js
@@ -1,54 +1,97 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import "../App.css";
 
-//CONSULTA AXIOS EN COMPONENTE FUNCIONAL
+export const AllBeer = () => {
+  const [beer, setBeer] = useState([]);
+  const [search, setSearch] = useState("");
 
-// const AllBeer = () => {
-//   const [beer, setBeer] = useState({beer:[esto esta asi para que funcionase con lo que he puesto en las clases]})
+  const beers = async () => {
+    const res = await axios.get(`https://ih-beers-api2.herokuapp.com/beers/`);
+    return res.data;
+  };
+  useEffect(() => {
+    beers().then(beer => setBeer(beer));
+    console.log(beer);
+  }, []);
 
-//   useEffect(() => {
-//       fetch("https://ih-beers-api2.herokuapp.com/beers")
-//         .then(response => response.json())
-//         .then(beersArray => setBeer({ beer: beersArray }));
-//   },[])
-//   return (blabla)
+  const searchBeers = async query => {
+    const response = await axios.get(
+      `https://ih-beers-api2.herokuapp.com/beers/search?q=${query}`
+    );
+    return response.data;
+  };
+
+  const handleSearch = e => {
+    const query = e.target.value;
+    searchBeers(query).then(beer => setBeer(beer));
+    setSearch(query);
+  };
+
+  if (!beer[0] && !search) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <form onSubmit={e => e.preventDefault()}>
+        <input
+          name="search"
+          placeholder="Search a beer"
+          value={search}
+          onChange={handleSearch}
+        />
+      </form>
+      {beer.map(beer => (
+        <div className="prueba">
+          <Link className="link" to={`/beer/${beer._id}`} key={beer.i}>
+            <img src={beer.image_url} alt={beer.name}></img>
+            <h3>{`Name: ${beer.name}`}</h3>
+            <p>{`Tagline: ${beer.tagline}`}</p>
+            <p>{`Contributed By: ${beer.contributed_by}`}</p>
+            <hr></hr>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+//-------------------------------------------------------------------------------------------------------------------------------
+// Asi seria en una clase
+// class AllBeer extends Component {
+//   constructor() {
+//     super();
+//     this.state = { beer: undefined };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://ih-beers-api2.herokuapp.com/beers")
+//       .then(response => response.json())
+//       .then(beersArray => this.setState({ beer: beersArray }));
+//   }
+//   render() {
+//     return (
+//       <div>
+//         {this.state.beer ? (
+//           <div>
+//             {this.state.beer.map(beer => (
+//               <div className="prueba">
+//                 <Link className="link" to={`/beer/${beer._id}`} key={beer.i}>
+//                   <img src={beer.image_url} alt={beer.name}></img>
+//                   <h3>{`Name: ${beer.name}`}</h3>
+//                   <p>{`Tagline: ${beer.tagline}`}</p>
+//                   <p>{`Contributed By: ${beer.contributed_by}`}</p>
+//                   <hr></hr>
+//                 </Link>
+//               </div>
+//             ))}
+//           </div>
+//         ) : (
+//           <p>Cargando...</p>
+//         )}
+//       </div>
+//     );
+//   }
 // }
 
-class AllBeer extends Component {
-  constructor() {
-    super();
-    this.state = { beer: undefined };
-  }
-
-  componentDidMount() {
-    fetch("https://ih-beers-api2.herokuapp.com/beers")
-      .then(response => response.json())
-      .then(beersArray => this.setState({ beer: beersArray }));
-  }
-  render() {
-    return (
-      <div>
-        {this.state.beer ? (
-          <div>
-            {this.state.beer.map(beer => (
-              <div className="prueba">
-                <Link className="link" to={`/beer/${beer._id}`} key={beer.i}>
-                  <img src={beer.image_url} alt={beer.name}></img>
-                  <h3>{`Name: ${beer.name}`}</h3>
-                  <p>{`Tagline: ${beer.tagline}`}</p>
-                  <p>{`Contributed By: ${beer.contributed_by}`}</p>
-                  <hr></hr>
-                </Link>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>Cargando...</p>
-        )}
-      </div>
-    );
-  }
-}
-
-export default AllBeer;
+// export default AllBeer;
