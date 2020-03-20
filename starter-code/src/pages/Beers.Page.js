58de3f949c78e2b0cf1fbf26c45f76efc46fa085
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-03-20 21:45:29 +0100

    iteration3 done

--- starter-code/src/pages/Beers.Page.js
+++ starter-code/src/pages/Beers.Page.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { beersList } from "../../lib/BeersApi";
 import { Header } from "../components/Header";
+import styled from "styled-components";
+
+const ContainerImg = styled.img`
+  width: 100px;
+`;
 
 export const Beers = () => {
-  return <Header></Header>;
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    beersList().then(list => setList(list));
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <ul>
+        {list.map(beer => (
+          <Link to={`/beers/${beer._id}`}>
+            <img src={beer.image_url} alt={beer.name} />
+            <h2>{beer.name}</h2>
+            <h3>{beer.tagline}</h3>
+            <p>{beer.contributed_by}</p>
+          </Link>
+        ))}
+      </ul>
+    </>
+  );
 };
