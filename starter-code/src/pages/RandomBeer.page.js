e9f288518640ebee7a82541f96d720a35949f4f2
Teresa Pelinski <teresapelinski@gmail.com>
2020-03-20 21:28:01 +0100

    finished RandomBeer page

--- starter-code/src/pages/RandomBeer.page.js
+++ starter-code/src/pages/RandomBeer.page.js
@@ -1,8 +1,19 @@
-import React from "react";
-import Header from "../layouts/Header";
+import React, { useState, useEffect } from "react";
+import BeerInfo from "../components/BeerInfo"
+import Loading from "../components/Loading"
+import { getRandomBeer } from "../lib/beers.api"
 
-const RandomBeerPage = () => (
-    <Header />
-);
+const RandomBeerPage = () => {
+    const [beer, setBeer] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => { getRandomBeer().then(beer => setBeer(beer)).finally(() => setLoading(false)) }, [])
+    return (
+        <>
+            {loading && <Loading />}
+            < BeerInfo beer={beer} />
+        </>
+    )
+
+};
 
 export default RandomBeerPage;
