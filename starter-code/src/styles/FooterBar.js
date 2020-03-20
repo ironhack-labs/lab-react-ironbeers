c43357317047974a408665fdb5daa3008168b805
Ana Segarra <anasegarraber@gmail.com>
2020-03-20 11:48:30 +0100

    Add footer

---
+++ starter-code/src/styles/FooterBar.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const FooterBar = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1em;
+  background: #f7e0d4;
+  font-size: 0.8rem;
+  color: #172c66;
+  a:visited {
+    color: #172c66;
+  }
+`;
