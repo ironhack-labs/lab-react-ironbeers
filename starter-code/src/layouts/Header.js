b69b32f2a48cb06c0098a763e846f1d8d2ac7de5
Teresa Pelinski <teresapelinski@gmail.com>
2020-03-20 17:13:19 +0100

    created header

--- starter-code/src/layouts/Header.js
+++ starter-code/src/layouts/Header.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-const Header = () => (<></>);
+const HeaderStyle = styled.div`
+position:fixed;
+width:100%;
+top:0;
+background-color:deepskyblue;
+height:10vh;
+display:flex;
+justify-content:center;
+img {
+    align-self:center;
+    height:8vh;
+    padding-top:1vh;
+}
+`
+
+const Header = () => (
+    <HeaderStyle>
+        <Link to="/"><img src="home.svg" alt="Home icon" /></Link>
+    </HeaderStyle>);
 
 export default Header;
