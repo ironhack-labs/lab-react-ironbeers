4525f35582f2d905da4259afaa376a57d766ac4e
Ana Segarra <anasegarraber@gmail.com>
2020-03-20 02:14:58 +0100

    Implement provisional search functionality

--- starter-code/src/components/BeersList.js
+++ starter-code/src/components/BeersList.js
@@ -14,39 +14,47 @@ export const BeersList = () => {
       const response = await axios.get(url);
       setBeers(response.data);
     };
-
     fetchData();
   }, []);
 
-  console.log('el state', beers);
+  const handleSearch = async e => {
+    const { data } = await axios.get(
+      `https://ih-beers-api2.herokuapp.com/beers/search?q=${e.target.value}`
+    );
+    console.log('search result ', data);
+    setBeers(data);
+  };
 
   const formatName = str => str.replace(/<.*>/, '');
 
   return (
-    <BeersContainer>
-      {beers.length === 0 ? (
-        <p>Loading...</p>
-      ) : (
-        beers.map((beer, i) => {
-          return (
-            <BeerCard key={i}>
-              <ImgContainer>
-                <img src={beer.image_url} />
-              </ImgContainer>
+    <>
+      <input type="text" onChange={handleSearch} />
+      <BeersContainer>
+        {beers.length === 0 ? (
+          <p>Loading...</p>
+        ) : (
+          beers.map((beer, i) => {
+            return (
+              <BeerCard key={i}>
+                <ImgContainer>
+                  <img src={beer.image_url} />
+                </ImgContainer>
 
-              <div>
-                <Link to={`/beers/${beer._id}`}>{beer.name}</Link>
-                <p className="light">{beer.tagline}</p>
-                <p>
-                  <span> Created by: </span>
-                  {(beer.contributed_by && formatName(beer.contributed_by)) ||
-                    'unknown'}
-                </p>
-              </div>
-            </BeerCard>
-          );
-        })
-      )}
-    </BeersContainer>
+                <div>
+                  <Link to={`/beers/${beer._id}`}>{beer.name}</Link>
+                  <p className="light">{beer.tagline}</p>
+                  <p>
+                    <span> Created by: </span>
+                    {(beer.contributed_by && formatName(beer.contributed_by)) ||
+                      'unknown'}
+                  </p>
+                </div>
+              </BeerCard>
+            );
+          })
+        )}
+      </BeersContainer>
+    </>
   );
 };
