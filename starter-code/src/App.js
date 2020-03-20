566cfcb1adbad6c744d7ced74e192c9909d0c9fd
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-03-20 20:56:06 +0100

    Pending Logo To Not Appear at Home

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Home from "./components/home"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+//import Beers from "./components/beers"
+import HomeBtn from "./components/ui/homeButton"
 
 const App = () => {
 
   return (
     <>
-      <BrowserRouter>
+      <Router>
+        <HomeBtn path="/">Home</HomeBtn>
+
         <Switch>
           <Route path="/" exact component={Home} />
-
-
+          {/* <Route path="/beers" component={Beers} /> */}
         </Switch>
 
-      </BrowserRouter>
+      </Router>
 
     </>
-
-
   )
 
 }
