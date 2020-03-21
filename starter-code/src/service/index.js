d7e521b8ccf1303f4239577bb71275274d257f1f
Carlos Mateo <cmc2k5@gmail.com>
2020-03-21 19:43:32 +0100

    Iteration 6 completed

--- starter-code/src/service/index.js
+++ starter-code/src/service/index.js
@@ -17,4 +17,9 @@ export const getBeer = async (id) => {
 export const getRandomBeer = async () => {
     const res = await request.get('/random');
     return res.data;
-}
+};
+
+export const newBeer = async (formData) => {
+    const res = await request.post('/new', formData);
+    return res.data;
+};
