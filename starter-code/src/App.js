41cd0dbae8335aecf4b2867cec972bfd482d8cfc
Carlos Mateo <cmc2k5@gmail.com>
2020-03-21 07:34:24 +0100

    Iteration 1 completed

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
+// CSS
+import './styles.scss';
+
+// Pages
+import { Layout } from './layouts/Layout';
+import { Home } from './pages/Home';
+
+export const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Layout>
+            <Router>
+                <Route path="/" exact component={() => <Home />}></Route>
+            </Router>
+        </Layout>
     );
-  }
 }
 
 export default App;
