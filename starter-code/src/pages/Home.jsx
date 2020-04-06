26899637b359a2503fd65ebfc5abcdcce0fe705c
Francis Bukenya <frankie.bukenya@gmail.com>
2020-04-06 23:47:50 +0200

    Add Styling to home text

--- starter-code/src/pages/Home.jsx
+++ starter-code/src/pages/Home.jsx
@@ -8,15 +8,39 @@ class Home extends Component {
       <div className="Home">
         <Link className="Home-link" to="/ListBeers">
           <img src="../images/beers.png" alt="" className="Home-img" />
-          <h1 className="Home-heading">All Beers</h1>
+          <div className="Home-content">
+            <h1 className="Home-heading">All Beers</h1>
+            <small className="Home-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </small>
+          </div>
         </Link>
         <Link className="Home-link" to="/RandomBeer">
           <img src="../images/random-beer.png" alt="" className="Home-img" />
-          <h1>Random Beer</h1>
+          <div className="Home-content">
+            <h1 className="Home-heading">Random Beer</h1>
+            <small className="Home-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </small>
+          </div>
         </Link>
         <Link className="Home-link" to="/AddNewBeer">
           <img src="../images/new-beer.png" alt="" className="Home-img" />
-          <h1> Add a new beer</h1>
+          <div className="Home-content">
+            <h1 className="Home-heading"> Add a new beer</h1>
+            <small className="Home-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </small>
+          </div>
         </Link>
       </div>
     );
