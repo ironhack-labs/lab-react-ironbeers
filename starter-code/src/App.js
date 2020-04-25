d952d754dddb1426b08031a8003525b5b10f1422
Pablolo <pablo_olondriz@hotmail.com>
2020-04-25 20:36:00 +0200

    iteration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -9,9 +9,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <Link to='/beers'> Beers Page </Link>
-       <Link to='/random-beer'> RandomBeer Page </Link>
-       <Link to='/new-beer'> NewBeer Page </Link>
+        <Link to='/beers'>
+          <img src="/images/beers.png" alt="allbeers"/>
+          <h2>All Beers</h2>  
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab amet neque alias. Magni id mollitia architecto laborum.</p>
+        </Link>
+        <Link to='/random-beer'>
+          <img src="/images/random-beer.png" alt="randombeer"/> 
+          <h2>Random Beer</h2> 
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab amet neque alias. Magni id mollitia architecto laborum.</p>
+        </Link>
+        <Link to='/new-beer'>
+          <img src="/images/new-beer.png" alt="newbeer"/> 
+          <h2>New Beer</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab amet neque alias. Magni id mollitia architecto laborum.</p>
+        </Link>
 
        <Switch>
           <Route path='/beers' component={Beers}/>
