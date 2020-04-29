1f04de47ee4274361c13877db53e27eb59f111a3
Pablolo <pablo_olondriz@hotmail.com>
2020-04-29 16:39:12 +0200

    bonus done

--- starter-code/src/pages/Beers.js
+++ starter-code/src/pages/Beers.js
@@ -4,10 +4,35 @@ import { Link } from 'react-router-dom';
 
 class Beers extends Component {
 
-  printBeers = () => {
+  state = {
+    search: '',
+  }
+
+  searchBeer = (e) => {
+    this.setState({
+      search: e.target.value,
+    })
+  }
+
+  printFilteredBeers = () => {
     const {beers} = this.props;
-    return beers.map((item, index) => {
-      return <Link key={index} className='beer-link' to={`/beers/${item._id}`}>
+    const { search } = this.state;
+                           
+    if (search === '') {
+      return beers.map((item, index) => {
+        return this.individualBeer(item, index);
+      })
+    } else {
+      return beers.map((item, index) => {
+        if (item.name.toLowerCase().includes(search.toLowerCase())) {
+          return this.individualBeer(item, index);
+        } 
+      })
+    }
+  }
+
+  individualBeer = (item, index) => {
+    return <Link key={index} className='beer-link' to={`/beers/${item._id}`}>
               <div className='beer-box' >
                 <img className='beer-img' src={item.image_url} alt=""/>
                 <div className='beer-description-box'>
@@ -17,15 +42,15 @@ class Beers extends Component {
                 </div>
               </div>
               <hr/>
-            </Link> 
-    })
+            </Link>
   }
 
   render() {
    return (
      <div>
       <Header />
-      {this.printBeers()}
+      <input className="search-input" onChange={this.searchBeer} placeholder="Search your Beer here..." type="text"/>
+      {this.printFilteredBeers()}
      </div>
     )
   }
