a241b0275e81fd9cf9856244c9296ec89530bb2d
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-07 21:58:10 +0200

    commit stray file

--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 import { BrowserRouter as Router } from 'react-router-dom'
 
