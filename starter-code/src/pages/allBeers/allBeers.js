f5bcddcd76188d5a590e7e68d4a72f71f3e87a02
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-07 21:03:07 +0200

    allBeers view now can fetch beers from the API through Axios

--- starter-code/src/pages/allBeers/allBeers.js
+++ starter-code/src/pages/allBeers/allBeers.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
 import Navbar from './../../components/navbar/navbar'
 
 class AllBeers extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = { listOfBeers: [] }
+  }
+
+  getAllBeers() {
+    axios
+      .get('https://ih-beers-api2.herokuapp.com/beers')
+      .then((response) => {
+        this.setState({ listOfBeers: response.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  componentDidMount() {
+    this.getAllBeers()
   }
 
   render() {
     return (
       <main>
         <Navbar />
-        <h1>lol</h1>
+        {this.state.listOfBeers.map((elm, idx) => (<p>{elm.name}</p>))}
       </main>
     )
   }
