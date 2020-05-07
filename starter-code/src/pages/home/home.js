ac719fb33c39379e408f0db5995c053ce060ef0b
Elvira Ramírez <elvirarp92@gmail.com>
2020-05-07 20:05:20 +0200

    home page

---
+++ starter-code/src/pages/home/home.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+import './home.css'
+
+class Home extends Component {
+  render() {
+    return (
+      <section>
+        <h1>IronBeers</h1>
+        <article>
+          <img src="./../../../images/beers.png" alt="Beers" />
+          <h2>All Beers</h2>
+          <p>
+            Lorem fistrum no te digo trigo por no llamarte Rodrigor amatomaa
+            tiene musho peligro no te digo trigo por no llamarte Rodrigor.
+            Fistro pupita a wan te voy a borrar el cerito no puedor qué dise
+            usteer ese hombree ese hombree quietooor.
+          </p>
+        </article>
+        <article>
+          <img src="./../../../images/new-beer.png" alt="New Beer" />
+          <h2>New Beer</h2>
+          <p>
+            Lorem fistrum no te digo trigo por no llamarte Rodrigor amatomaa
+            tiene musho peligro no te digo trigo por no llamarte Rodrigor.
+            Fistro pupita a wan te voy a borrar el cerito no puedor qué dise
+            usteer ese hombree ese hombree quietooor.
+          </p>
+        </article>
+        <article>
+          <img src="./../../../images/random-beer.png" alt="Random Beer" />
+          <h2>Random Beer</h2>
+          <p>
+            Lorem fistrum no te digo trigo por no llamarte Rodrigor amatomaa
+            tiene musho peligro no te digo trigo por no llamarte Rodrigor.
+            Fistro pupita a wan te voy a borrar el cerito no puedor qué dise
+            usteer ese hombree ese hombree quietooor.
+          </p>
+        </article>
+      </section>
+    )
+  }
+}
+
+export default Home
