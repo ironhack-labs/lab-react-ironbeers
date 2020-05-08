d071cc8fa5865bc2a96c18959aded3abc5dd9b1f
TheShua <g.deglisezinski@gmail.com>
2020-05-08 08:43:45 +0200

    Add ListBeer error display

--- starter-code/src/pages/ListBeers.jsx
+++ starter-code/src/pages/ListBeers.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import BeerBlocList from '../components/BeerBlocList';
 import InputForm from '../components/InputForm';
 import axios from 'axios';
+import '../stylesheets/ListBeers.css';
 
 class ListBeers extends Component {
 	state = { allBeers: [], search: '' };
@@ -27,15 +28,32 @@ class ListBeers extends Component {
 	};
 
 	render() {
+		let errorMessage = '';
+		if (!this.state.allBeers.length && this.state.search !== '') {
+			errorMessage = 'There is no beer matching your search';
+		} else if (!this.state.allBeers.length) {
+			errorMessage = 'There is no beer in the database...';
+		}
+
 		return (
 			<React.Fragment>
 				<Header />
 				<form>
-					<InputForm type="text" name="search" change={this.handleSearch} value={this.state.search} />
+					<InputForm
+						type="text"
+						name="search"
+						change={this.handleSearch}
+						value={this.state.search}
+						placeholder="Search..."
+					/>
 				</form>
-				{this.state.allBeers.map((beer, index) => {
-					return <BeerBlocList key={index} data={beer} />;
-				})}
+				{!this.state.allBeers.length ? (
+					<p className="empty">{errorMessage}</p>
+				) : (
+					this.state.allBeers.map((beer, index) => {
+						return <BeerBlocList key={index} data={beer} />;
+					})
+				)}
 			</React.Fragment>
 		);
 	}
