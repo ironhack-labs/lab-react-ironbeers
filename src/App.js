641f3d3eaa04d4b5e1a65910205f08b7c67a639c
Teddy ROGERS <teddy.rogers.pro@outlook.com>
2020-05-12 02:03:13 +0200

    PAR WDFT 0320 Teddy Rogers
    in progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './components/home.jsx';
+import Beers from './components/beers.jsx';
+import RandomBeer from './components/randombeer.jsx';
+import NewBeer from './components/newbeer.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Home} />;
+        <Route exact path="/beers" component={Beers} />;
+        <Route exact path="/randombeer" component={RandomBeer} />;
+        <Route exact path="./newbeer" component={NewBeer} />;
+      </Switch>
     </div>
   );
 }
