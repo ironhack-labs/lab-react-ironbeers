b74fcfa7a2af3019ab77e97239e84d3177fe58e5
Tzikas <niko.tzikas@gmail.com>
2020-07-02 17:43:07 -0400

    beers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    beers: []
+  }
+
+  async componentDidMount(){
+
+    let res = await axios.get('https://ih-beers-api2.herokuapp.com/beers')
+    console.log(res)
+
+    // await axios.post(`https://ironrest.herokuapp.com/ironhackbeersstolen`, {beers: res.data})
+  }
+
+  showTheBeers = () => {
+    return this.state.beers.map(beer => {
+      return <li>{beer.name}</li>
+      })
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        {this.showTheBeers()}
+      </div>
+    );
+  }
 }
 
 export default App;
