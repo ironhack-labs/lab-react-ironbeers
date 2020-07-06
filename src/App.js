965b5a2cfd09a5792b8b5ebc4edcb0d8a72bf07c
Kai Quander <kq1003@gmail.com>
2020-07-06 17:12:57 -0400

    did some

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    beers: [],
+    onebeer: []
+  }
+
+  async componentDidMount(){
+      let res = await axios.get('https://ih-beers-api2.herokuapp.com/beers')
+      console.log(res)
+
+      this.setState({
+        beers: res.data
+      })
+
+      let one = await axios.get('https://ih-beers-api2.herokuapp.com/beers/random')
+      console.log(one)
+
+      this.setState({
+        onebeer: one.data
+      })
+  }
+
+  showTheBeers = () => {
+    return this.state.beers.map(beer => {
+      return <li> {beer.name} </li>
+    })
+  }
+
+  showOneBeer = () => {
+    return this.state.onebeer.name
+  }
+
+  render() {
+    return (
+      <div>
+        one:{this.showOneBeer()}
+        {this.state.onebeer.name}
+        {this.showTheBeers()}
+      </div>
+    );
+  }
 }
 
 export default App;
