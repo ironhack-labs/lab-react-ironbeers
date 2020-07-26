08db8c173815e0f304b5a6001b1ff0a96e322ed3
Alba Gustems <albagustems@gmail.com>
2020-07-26 15:27:07 +0200

    Iteration 3:  List all the beers

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Home from './components/Home';
+import AllBeers from './components/AllBeers';
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/beers" render={(props) => <AllBeers {...props} />} />
+    </Switch>
   );
 }
 
---
+++ src/components/AllBeers.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import Header from './Header';
+
+export class AllBeers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://ih-beers-api2.herokuapp.com/beers')
+      .then((response) => {
+        this.setState({ beers: response.data });
+      })
+      .catch((err) => console.log('Error trying to get the beer list', err));
+  }
+
+  render() {
+    const beerList = this.state.beers.map((beer) => (
+      <Link className="link" to={'/beers/' + beer._id}>
+        <div key={beer._id} className="container">
+          <div className="beerImage">
+            <img src={beer.image_url} alt={beer.name} />
+          </div>
+          <div className="beerData">
+            <h4>{beer.name}</h4>
+            <h6>{beer.tagline}</h6>
+            <p>
+              <b>Created by:</b>
+              {beer.contributed_by}
+            </p>
+          </div>
+        </div>
+      </Link>
+    ));
+
+    return (
+      <div>
+        <Header /> {beerList}
+      </div>
+    );
+  }
+}
+
+export default AllBeers;
