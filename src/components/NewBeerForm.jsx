3c010ae4cf2c38ece0b3847481695762b317f81e
Toni <sancheztoni.garcia@gmail.com>
2020-07-28 18:20:12 +0200

    form

--- src/components/NewBeerForm.jsx
+++ src/components/NewBeerForm.jsx
@@ -6,7 +6,25 @@ function NewBeerForm() {
     return (
         <div>
             <HeaderNav/>
-            <h1>NewBeerForm</h1>
+            <div>
+                <form action="">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" placeholder="Name" name="name"/>
+                    <label htmlFor="Tagline">Tagline</label>
+                    <input type="text" placeholder="Tagline" name="tagline"/>
+                    <label htmlFor="Description">Description</label>
+                    <textarea name="description" placeholder="Description..." cols="30" rows="10"></textarea>
+                    <label htmlFor="brewed">First Brewed</label>
+                    <input type="text" placeholder="first_brewed" name="first_brewed"/>
+                    <label htmlFor="brewer_tips">Brewer Tips</label>
+                    <input type="text" name="brewer_tips"/>
+                    <label htmlFor="name">Attenuation Level</label>
+                    <input type="text" name="attenuation_level"/>
+                    <label htmlFor="name">Contributed By</label>
+                    <input type="text" name="contributed_by"/>
+                    <input type="submit" value="ADD NEW"/>
+                </form>
+            </div>
         </div>
     )
 }
