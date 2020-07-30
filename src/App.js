7fd92d333aa6452b26e9af79638eeeab1ea817d0
David de Lamo <d.lamo.valverde@gmail.com>
2020-07-30 17:23:43 +0200

    iteration 6

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AllBeers from './components/AllBeers';
 import SingleBeer from './components/SingleBeer';
+import NewBeerForm from './components/NewBeerForm';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path='/beers' component={AllBeers} />
           <Route path='/beers/:id' component={SingleBeer} />
           <Route path='/random-beer' component={SingleBeer} />
+          <Route path='/new-beer' component={NewBeerForm} />
         </Switch>
       </div>
     </BrowserRouter>
