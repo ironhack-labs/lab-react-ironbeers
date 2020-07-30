f997cc11078b865a1205d0e611859d953db4e822
David de Lamo <d.lamo.valverde@gmail.com>
2020-07-30 15:52:17 +0200

    iteration 4

--- src/components/SingleBeer.jsx
+++ src/components/SingleBeer.jsx
@@ -9,10 +9,20 @@ export class SingleBeer extends Component {
             <div>
                 <Header />
                 <div className='d-flex flex-column'>
-                    <div>
-                        <img src={beer.image_url} alt={beer.name} />
+                    <div className='align-self-center py-5'>
+                        <img style={{height: '250px'}} src={beer.image_url} alt={beer.name} />
                     </div>    
+                </div>
+                <div className='d-flex justify-content-between px-4'>
+                    <h2>{beer.name}</h2>
+                    <h3 className='text-secondary'>{beer.attenuation_level}</h3>    
                 </div> 
+                <div className='d-flex justify-content-between px-4'>
+                    <h5 className='text-secondary'>{beer.tagline}</h5>
+                    <h6><b>{beer.first_brewed}</b></h6>    
+                </div>
+                <p className='px-4 description-beer'><b>{beer.description}</b></p>
+                <p className='px-4 text-secondary'><b>{beer.contributed_by}</b></p>
             </div>
         )
     }
