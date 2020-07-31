4df13a50e70eb2d6c5e72b91d2b8e27199878083
irensvm <irens.vm@gmail.com>
2020-07-31 21:10:43 +0200

    1/2form

--- src/components/NewBeer.js
+++ src/components/NewBeer.js
@@ -16,47 +16,64 @@ export default class NewBeer extends Component {
             contributed_by: '',
         };
     }
+
+    handleChange = (element) => {
+        this.setState({
+            ...this.state,
+            [element.target.name]: element.target.value
+        })
+    }
+    handleSubmit = (element) => {
+        element.preventDefault();
+        axios.post('https://ih-beers-api2.herokuapp.com/beers/new', this.state)
+            .then((response) => {
+                console.log(response.data)
+            })
+    }
     render() {
         return (
             <div>
                 <header>
                     <Link to={"/"}><img alt="header" src='/header.png' ></img></Link>
                 </header>
-                <Form>
-                    <Form.Group controlId="name">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter beer name" />
-                    </Form.Group>
+                <div>
+                    <Form className="new-beer">
+                        <Form.Group controlId="name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter beer name" onChange={this.handleChange} value="" />
+                        </Form.Group>
 
-                    <Form.Group controlId="tagline">
-                        <Form.Label>tagline</Form.Label>
-                        <Form.Control type="text" placeholder="tagline" />
-                    </Form.Group>
-                    <Form.Group controlId="description">
-                        <Form.Label>description</Form.Label>
-                        <Form.Control type="text" placeholder="Be creative with your description" />
-                    </Form.Group>
-                    <Form.Group controlId="first_brewed">
-                        <Form.Label>first_brewed</Form.Label>
-                        <Form.Control type="text" placeholder="first_brewed" />
-                    </Form.Group>
-                    <Form.Group controlId="brewers_tips">
-                        <Form.Label>brewers_tips</Form.Label>
-                        <Form.Control type="text" placeholder="Do you have any brewers tips?" />
-                    </Form.Group>
-                    <Form.Group controlId="attenuation_level ">
-                        <Form.Label>attenuation_level </Form.Label>
-                        <Form.Control type="number" placeholder="Enter attenuation_level " />
-                    </Form.Group>
-                    <Form.Group controlId="contributed_by">
-                        <Form.Label>contributed_by</Form.Label>
-                        <Form.Control type="text" placeholder="Enter contributor name" />
-                    </Form.Group>
+                        <Form.Group controlId="tagline">
+                            <Form.Label>tagline</Form.Label>
+                            <Form.Control type="text" placeholder="tagline" onChange={this.handleChange} value="" />
+                        </Form.Group>
+                        <Form.Group controlId="description">
+                            <Form.Label>description</Form.Label>
+                            <Form.Control type="text" placeholder="Be creative with your description" onChange={this.handleChange} value="" />
+                        </Form.Group>
+                        <Form.Group controlId="first_brewed">
+                            <Form.Label>first_brewed</Form.Label>
+                            <Form.Control type="text" placeholder="first_brewed" onChange={this.handleChange} value="" />
+                        </Form.Group>
+                        <Form.Group controlId="brewers_tips">
+                            <Form.Label>brewers_tips</Form.Label>
+                            <Form.Control type="text" placeholder="Do you have any brewers tips?" onChange={this.handleChange} value="" />
+                        </Form.Group>
+                        <Form.Group controlId="attenuation_level ">
+                            <Form.Label>attenuation_level </Form.Label>
+                            <Form.Control type="number" placeholder="Enter attenuation_level" onChange={this.handleChange} value="" />
+                        </Form.Group>
+                        <Form.Group controlId="contributed_by">
+                            <Form.Label>contributed_by</Form.Label>
+                            <Form.Control type="text" placeholder="Enter contributor name" onChange={this.handleChange} value="" />
+                        </Form.Group>
 
-                    <Button variant="primary" type="submit">
-                        Add new
+                        <Button variant="primary" type="submit">
+                            Add new
                     </Button>
-                </Form>
+                    </Form>
+                </div>
+
             </div>
         )
     }
