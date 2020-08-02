6019ae6b5208c5d3708fd804c65b1bf0ada87ea9
Alberto Correia <alberto_cpv@hotmail.com>
2020-08-02 18:49:01 +0200

    iteration 1

---
+++ src/components/Home/Home.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+
+function Home() {
+    return (
+        <div>
+            <div>
+            <img src="/Images/beers.jpeg" alt="beers"></img> 
+            <h4>All beers </h4>
+            <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+             nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+             aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+             nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+             officia deserunt mollit anim id est laborum."
+             </p>
+            </div>
+            <hr></hr>
+
+            <div>
+            <img src="/Images/randombeer.jpeg" alt="random beer"></img>
+            <h4>Random Beer</h4>
+            <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+             dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+             mollit anim id est laborum."
+             </p>
+            </div>
+            <hr></hr>
+
+
+            <div>
+            <img src="/Images/newbeer.jpeg" alt="new beer"></img>
+            <h4>New Beer</h4>
+            <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum."
+              </p>
+            </div>
+            <hr></hr>
+
+            
+        </div>
+    )
+}
+
+export default Home
