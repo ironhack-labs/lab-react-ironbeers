8cf19c83e839872f06ecd15333945e55ae83cf20
JesusSantana <jesus@esmarketingdigital.com>
2020-08-07 13:26:59 +0200

    Iteration 2 | Header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Homepage from './components/Homepage';
+import Home from './components/Home';
+import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Homepage} />{' '}
-        <Route path="/random-beer" />
-        <Route path="/new-beer" />
-      </Switch>{' '}
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path='/' component={Header} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="card">
+          <img
+            className="card-img-top"
+            src={'/allbeers.jpg'}
+            alt="All beers"
+          />
+          <div className="card-body">
+            <Link to="/beers">
+              <h3 className="card-title">All Beers</h3>
+            </Link>
+            <p className="card-text text-secondary">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam
+              voluptates quis placeat totam dolore perferendis cumque
+              repellendus molestiae! Consequatur officia sunt in quisquam
+              adipisci?
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            className="card-img-top"
+            src={'/randombeer.jpeg'}
+            alt="Random beer"
+          />
+          <div className="card-body">
+            <Link to="/randombeer">
+              <h3 className="card-title">Random Beers</h3>
+            </Link>
+            <p className="card-text text-secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ex
+              doloremque sapiente culpa ducimus eius itaque! Sit eum possimus
+              fugit! Inventore, dignissimos ducimus. Ex.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card-img-top" src={'/newbeer.jpg'} alt="New beer" />
+          <div className="card-body">
+            <Link to="/newbeer">
+              <h3 className="card-title">New Beer</h3>
+            </Link>
+            <p className="card-text text-secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+              eligendi harum atque alias eos dolore. Totam architecto qui
+              dolorum quae similique in, corrupti labore?
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
