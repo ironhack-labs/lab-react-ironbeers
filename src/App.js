b8972c8a1902ce0ec49737c5b0a14d786345e0b3
JesusSantana <jesus@esmarketingdigital.com>
2020-08-07 12:51:44 +0200

    Iteration 1 | Home Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Homepage} />{' '}
+        <Route path="/random-beer" />
+        <Route path="/new-beer" />
+      </Switch>{' '}
+    </BrowserRouter>
   );
 }
 
