8690e56046980fc34de8d55fdc6bf992af68d93c
Adriano Ortiz Garda <adriano.ortizgarda@gmail.com>
2020-09-18 09:25:38 -0500

    ultima iteracion terminada

--- src/router.js
+++ src/router.js
@@ -4,11 +4,11 @@ import Home from "./pages/Home"
 import Beers from "./pages/Beers"
 import BeerDetail from "./pages/BeerDetail"
 import RandomBeer from './pages/Random'
-// import CreateBeer from './pages/CreateBeer'
+import CreateBeer from './pages/CreateBeer'
 import { createEvent } from "@testing-library/react"
 //const BeerDetail = () => <h1>Hola</h1>
 
-const CreateBeer = () => <h1>Create</h1>
+//const CreateBeer = () => <h1>Create</h1>
 
 
 const router = () => {
@@ -17,9 +17,9 @@ const router = () => {
         <Switch>
 					<Route component={Home} path="/" exact />
 					<Route component={Beers} path="/beers" exact/>
-        	        <Route component={BeerDetail} path='/:beerId' exact/>
                     <Route component={RandomBeer} path='/random' exact/>
                     <Route component={CreateBeer} path='/createBeer' exact/>
+                    <Route component={BeerDetail} path='/:beerId' exact/>
         </Switch>
 			</Router>
     )
