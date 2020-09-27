a88542cc668f93617f5fdfcc0e97c54845c09c9a
Daniel Papastratidis <idanpap@gmail.com>
2020-09-27 21:57:33 +0200

    add new beer

--- src/components/NewBeer.js
+++ src/components/NewBeer.js
@@ -4,41 +4,63 @@ import axios from 'axios';
 export default class NewBeer extends Component {
   handleSubmit = (event) => {
     console.log('this is the event:', event);
+    event.preventDefault();
 
-    axios.post(`https://ih-beers-api2.herokuapp.com/beers/new`).then((res) => {
-      console.log(res);
-    });
+    const form = event.target;
+    const name = form.querySelector('input#name').value;
+    const tagline = form.querySelector('input#tagline').value;
+    const description = form.querySelector('input#description').value;
+    const firstBrewed = form.querySelector('input#first_brewed').value;
+    const brewersTips = form.querySelector('input#brewers_tips').value;
+    const attenuationLevel = form.querySelector('input#attenuation_level')
+      .value;
+    const contributedBy = form.querySelector('input#contributed_by').value;
+
+    axios
+      .post('https://ih-beers-api2.herokuapp.com/beers/new', {
+        name: name,
+        tagline: tagline,
+        description: description,
+        first_brewed: firstBrewed,
+        brewers_tips: brewersTips,
+        attenuation_level: attenuationLevel,
+        contributed_by: contributedBy,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
   };
   render() {
     return (
       <div>
         <form action="/beers/new" method="POST" onSubmit={this.handleSubmit}>
-          <label htmlFor="">Name:</label>
+          <label htmlFor="name">Name:</label>
           <input type="text" name="name" id="name"></input>
 
-          <label htmlFor="">Tagline</label>
+          <label htmlFor="tagline">Tagline</label>
           <input type="text" name="tagline" id="tagline"></input>
 
-          <label htmlFor="">Description:</label>
+          <label htmlFor="description">Description:</label>
           <input type="text" name="description" id="description"></input>
 
-          <label htmlFor="">First Brewed:</label>
+          <label htmlFor="first_brewed">First Brewed:</label>
           <input type="text" name="first_brewed" id="first_brewed"></input>
 
-          <label htmlFor="">Brewers tips:</label>
+          <label htmlFor="brewers_tips">Brewers tips:</label>
           <input type="text" name="brewers_tips" id="brewers_tips"></input>
 
-          <label htmlFor="">Attenuation level:</label>
+          <label htmlFor="attenuation_level">Attenuation level:</label>
           <input
             type="number"
             name="attenuation_level"
             id="attenuation_level"
           ></input>
 
-          <label htmlFor="">Created by: </label>
+          <label htmlFor="contributed_by">Created by: </label>
           <input type="text" name="contributed_by" id="contributed_by"></input>
 
-          <button type="submit">Add a new beer</button>
+          <button>Add a new beer</button>
         </form>
       </div>
     );
