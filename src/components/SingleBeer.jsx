22284a0da55c9242ec8bd0880214a3061d7de28f
Rodrigo Poblacion <rodrigopoblacion@gmail.com>
2020-10-06 23:09:22 +0200

    Iteration 3 casi

--- src/components/SingleBeer.jsx
+++ src/components/SingleBeer.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
+import axios from 'axios'
 
 class SingleBeer extends React.Component {
 
   state = {
-    singlebeer: "",
+    oneBeer: "",
   }
 
   getSingleBeer() {
-    const oneBeer = this.props.match.params.id
-    console.log(oneBeer);
+    axios.get(`https://ih-beers-api2.herokuapp.com/beers/${this.props.match.params.id}`)
+    .then(response => {
+        this.setState({ oneBeer: response.data})
+    })
   }
 
-  render() {
+  componentDidMount() {
+    this.getSingleBeer()
+  }
+  
 
+  render() {
+    const beer = this.state.oneBeer
     return (
-      <div className="card" style={{ width: '18rem' }}>
-        <img src={ {width: '50px'}} className="card-img-top" alt="hola" />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          
+      <div className="container">
+        <div className="card text-center" style={{width: '18rem'}}>
+        <img className="card-img-top" src={beer.image_url} alt={beer.name} style={{  width: '200px' }}></img>
+        <div class="card-body">
+          <h5 className="card-title">{beer.name}</h5>
+          <h6 className="card-text"><small className="text-muted">{beer.tagline}</small></h6>
+          <p className="card-text">{beer.description}</p>
+          <p className="card-text">{beer.attenuation_level}</p>
+          <p className="card-text">{beer.first_brewed}</p>
+          <p className="card-text"><b>Created by: </b>{beer.contributed_by}</p>
         </div>
+
+      </div>
       </div>
     )
   }
