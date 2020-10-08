334c7e767f26a6016b217202bef5451864d86f29
Ailie McCorkindale <ailiem134@hotmail.com>
2020-10-08 23:48:03 +0200

    Iteration 3 done

---
+++ src/api/beerApi.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const service = axios.create({
+  baseURL: "https://ih-beers-api2.herokuapp.com/beers",
+});
+
+export default {
+  service,
+
+  getBeers() {
+    return service.get("/");
+  },
+ /* getOneBeer(id) {
+    return service.get("/" + id);
+  },
+
+  createOne(data) {
+    return service.post("/laroute-du-post", data);
+  },*/
+};
--- src/pages/AllBeers.jsx
+++ src/pages/AllBeers.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from 'react';
+import BeerAPI from '../api/beerApi';
+import { Link } from 'react-router-dom';
 
-const AllBeers = () => {
+class AllBeers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: [],
+      selectedBeer: null,
+    };
+  }
+
+  async componentDidMount() {
+    BeerAPI.getBeers()
+      .then((apiResponse) => {
+        console.log(apiResponse);
+        this.setState({ beers: apiResponse.data });
+      })
+      .catch((apiError) => {
+        console.log(apiError);
+      });
+  }
+
+  componentDidUpdate() {
+    console.log('I have updated !');
+  }
+
+  componentWillUnmount() {
+    console.log('I have unmounted');
+  }
+
+  render() {
     return (
-        <div>
-            <p>All Beers Page</p>
-        </div>
-    )
+      <div>
+        <p>All Beers Page</p>
+        <h1>All the Beers!</h1>
+        {this.state.beers.map((beer) => (
+          <div>
+            <Link key={beer._id} to={`/beer/${beer._id}`}>
+              <img src={beer.image_url} alt="" />
+              <h3>{beer.name}</h3>
+            </Link>
+            <p>{beer.tagline}</p>
+            <p>Created by: {beer.contributed_by}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default AllBeers
+export default AllBeers;
