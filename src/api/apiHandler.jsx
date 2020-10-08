27feb795bfbc9787bcd3a65765e9afc34680e0ec
Bouziane <bey.bouziane@live.fr>
2020-10-08 18:32:41 +0200

    Iteration 6 done

--- src/api/apiHandler.jsx
+++ src/api/apiHandler.jsx
@@ -4,6 +4,28 @@ const service = axios.create({
   baseURL: 'https://ih-beers-api2.herokuapp.com/beers',
 });
 
+export const createNewBeer = async (
+  name,
+  tagline,
+  description,
+  first_brewed,
+  brewers_tips,
+  attenuation_level,
+  contributed_by
+) => {
+  const newBeer = {
+    name,
+    tagline,
+    description,
+    first_brewed,
+    brewers_tips,
+    attenuation_level,
+    contributed_by,
+  };
+  const response = await service.post('/new', newBeer);
+  console.log('checking if new created: ', response);
+};
+
 export default {
   service,
 
