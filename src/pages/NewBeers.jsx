6275582dfcc64c6473d0103745785f3f043ea0d8
MaxJGruber <maxjgruber@gmail.com>
2020-10-08 15:05:24 +0200

    First commit and npm install

---
+++ src/pages/NewBeers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewBeers = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NewBeers
---
+++ src/pages/RandomBeer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RandomBeer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RandomBeer
+
