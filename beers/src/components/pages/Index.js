15a24eb5f5ccc3adbe6050b0a2e31d399330c6a3
Juan Pestana <juan.pestana82@gmail.com>
2020-10-30 12:06:01 +0100

    a little styling

--- beers/src/components/pages/Index.js
+++ beers/src/components/pages/Index.js
@@ -7,10 +7,14 @@ export default () => {
 
     return (
        <>
-       <div className='card-columns'>
+       <div className="container">
+           <div className="logo-wrapper">
+               <img src="https://www.glasgowliving.today/wp-content/uploads/2017/09/Brewdog-Logo.png" alt="brewdog-logo" />
+           </div>
+           <div className='card-columns'>
            <Link to = '/allBeers'>
            <div className="card">
-                <img className="card-img-top img-fluid" src="https://www.hosteleria.site/wp-content/uploads/2019/07/bar-731x411.jpg" alt=""/>
+                <img className="card-img-top img-fluid " src="https://www.hosteleria.site/wp-content/uploads/2019/07/bar-731x411.jpg" alt="beers img1"/>
                 <div className="card-body">
                     <h4 className="card-title">All Beers</h4>
                     <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content
@@ -20,7 +24,7 @@ export default () => {
            </Link>
            <Link to = '/randomBeer'>
            <div className="card">
-                <img className="card-img-top img-fluid" src="https://i.insider.com/5476423e69beddbb65619d0d?width=600&format=jpeg&auto=webp" alt=""/>
+                <img className="card-img-top img-fluid" src="https://i.insider.com/5476423e69beddbb65619d0d?width=600&format=jpeg&auto=webp" alt="beers img2"/>
                 <div className="card-body">
                     <h4 className="card-title">Random Beer</h4>
                     <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content
@@ -30,7 +34,7 @@ export default () => {
            </Link>
            <Link to = '/newBeer'>
            <div className="card">
-                <img className="card-img-top img-fluid" src="https://www.marketingtochina.com/wp-content/uploads/2019/11/26-052442-man_goes_on_beer_only_diet_loses_25_pounds.jpg" alt=""/>
+                <img className="card-img-top img-fluid" src="https://www.marketingtochina.com/wp-content/uploads/2019/11/26-052442-man_goes_on_beer_only_diet_loses_25_pounds.jpg" alt="beers img3"/>
                 <div className="card-body">
                     <h4 className="card-title">New Beer</h4>
                     <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content
@@ -39,6 +43,8 @@ export default () => {
             </div>
            </Link>
        </div>
+       </div>
+       
        
         
     </>
