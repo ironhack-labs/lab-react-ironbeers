c359309468795466fbe28568ada7fb1c6851c801
Aleksandra Tanasijenko <alekstana@Aleksandras-MBP.fritz.box>
2020-11-01 19:37:17 +0100

    got rid of console.logs

--- src/App.js
+++ src/App.js
@@ -10,32 +10,30 @@ import NewBeer from './components/NewBeer';
 import HomePage from './components/HomePage';
 import SingleBeer from './components/SingleBeer';
 
-
-
 class App extends Component {
-
-
   state = {
     beers: [],
   };
 
-
   componentDidMount() {
-    axios.get("https://ih-beers-api2.herokuapp.com/beers")
-    .then((response) => {
-      // console.log(response.data)
+    axios.get('https://ih-beers-api2.herokuapp.com/beers').then((response) => {
       this.setState({
         beers: response.data,
       });
     });
   }
 
-
-
   handleAdd = (event) => {
     event.preventDefault();
-    console.log(event.target)
-    const { name, tagline, first_brewed, brewers_tips, attenuation_level ,contributed_by, description} = event.target
+    const {
+      name,
+      tagline,
+      first_brewed,
+      brewers_tips,
+      attenuation_level,
+      contributed_by,
+      description,
+    } = event.target;
 
     let newBeer = {
       name: name.value,
@@ -45,58 +43,75 @@ class App extends Component {
       attenuation_level: attenuation_level.value,
       contributed_by: contributed_by.value,
       description: description.value,
-    }
+    };
 
-    
-
-    axios.post('https://ih-beers-api2.herokuapp.com/beers/new', newBeer)
-    .then((response) =>{
-        this.setState({
-          beers: [response.data , ...this.state.beers]
-        }, () => {
-          this.props.history.push('/')
-        });    
-    });
-  }
-
-
-
+    axios
+      .post('https://ih-beers-api2.herokuapp.com/beers/new', newBeer)
+      .then((response) => {
+        this.setState(
+          {
+            beers: [response.data, ...this.state.beers],
+          },
+          () => {
+            this.props.history.push('/');
+          }
+        );
+      });
+  };
 
   handleSearch = (event) => {
-    console.log("search called!")
-    console.log(event)
     event.preventDefault();
     let search = event.target.value;
-    const { beers } = this.state;
 
-  axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${search}`)
-    .then((response) =>{
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${search}`)
+      .then((response) => {
         this.setState({
-          beers: response.data
-        });    
-    });
-  
-  }
-
-
-
-
+          beers: response.data,
+        });
+      });
+  };
 
   render() {
     return (
       <div className="container">
-      <Switch>
-        <Route exact path="/" render={() => { return <HomePage/>}} />
-        <Route exact path="/beers" render={() => { return <BeerList beers={this.state.beers} onSearch={this.handleSearch}/> ; }} />
-        <Route path="/random-beer" render={() => { return <RandomBeer/>}} />
-        <Route path="/new-beer" render={() => { return <NewBeer onAdd={this.handleAdd}/> ; }} />
-        <Route path="/beers/:beerId" component={SingleBeer} />
-      </Switch>
-
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return <HomePage />;
+            }}
+          />
+          <Route
+            exact
+            path="/beers"
+            render={() => {
+              return (
+                <BeerList
+                  beers={this.state.beers}
+                  onSearch={this.handleSearch}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/random-beer"
+            render={() => {
+              return <RandomBeer />;
+            }}
+          />
+          <Route
+            path="/new-beer"
+            render={() => {
+              return <NewBeer onAdd={this.handleAdd} />;
+            }}
+          />
+          <Route path="/beers/:beerId" component={SingleBeer} />
+        </Switch>
       </div>
     );
   }
 }
 
 export default withRouter(App);
-
