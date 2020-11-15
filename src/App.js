5128c65b6a0e990f6d7fc42ed6f2aa0532717ad6
roicid <roi_cid89@hotmail.com>
2020-11-15 22:49:09 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
+import { Switch, Route } from 'react-router-dom';
+import axios from 'axios'
 
-function App() {
+class App extends Component {
+  state =
+  { beers }
+  
+  componentDidMount = async () => {
+    const beers = await axios.get("https://api.punkapi.com/v2/")
+    this.setState({ beers: beers.data})
+    console.log(beers.data)
+  }
+  render() {
+    // L18 definimos ruta para cuando se acceda a una ruta determinada se reenderice un countrydetails de esa informacion.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar />
+     <div className="container">
+          <div className="row">
+    
+     </div>
+     <Switch>
+     
+     </Switch>
+    </div> 
     </div>
+    
   );
-}
 
+}
+}
 export default App;
