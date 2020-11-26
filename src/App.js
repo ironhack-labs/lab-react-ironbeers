77a8f916aca02c17215ba047d104cac13063fb4f
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-26 15:54:31 -0600

    antD layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import './App.css';
+
+const { Header, Content, Footer } = Layout;
+
 function Home() {
   return <h2>Home</h2>;
 }
@@ -52,6 +57,27 @@ function App() {
             <Home />
           </Route>
         </Switch>
+        <Layout className="layout">
+          <Header>
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: '0 50px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-content">Content</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
       </div>
     </Router>
   );
