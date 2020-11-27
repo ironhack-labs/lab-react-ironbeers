a6f84cf11693e1f2d2e663ad20d2a6895cdb047b
Emma Northridge <emmanorthridge1@gmail.com>
2020-11-27 16:57:41 +0100

    Initial commit with homepage routes set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Beers from './Components/Beers';
+import RandomBeer from './Components/RandomBeer';
+import NewBeer from './Components/NewBeer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/beers" exact component={Beers} />
+        <Route path="/random-beer" exact component={RandomBeer} />
+        <Route path="/new-beer" exact component={NewBeer} />
+      </Switch>
+    </Router>
   );
 }
 
