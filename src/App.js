8960e9bc20a696420b37f04a870cb5cae91f816f
Emma Northridge <emmanorthridge1@gmail.com>
2020-11-29 14:46:04 +0100

    1st Iteration

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Home from './Components/Home.js'
 import Beers from './Components/Beers';
 import RandomBeer from './Components/RandomBeer';
 import NewBeer from './Components/NewBeer';
@@ -11,6 +12,7 @@ function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={Home} />
         <Route path="/beers" exact component={Beers} />
         <Route path="/random-beer" exact component={RandomBeer} />
         <Route path="/new-beer" exact component={NewBeer} />
---
+++ src/Components/Beers.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+function Home() {
+    return (
+        <div>
+            <h1>All the beers!</h1>
+        </div>
+    )
+}
+
+export default Home
