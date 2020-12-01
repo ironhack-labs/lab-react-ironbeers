69376b8d62c8ae15291d65871c6c1172d44b5dff
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2020-12-01 17:37:21 +0300

    deleted my comment

--- src/components/BeerDetail.js
+++ src/components/BeerDetail.js
@@ -1,5 +1,4 @@
 // Iteration 4 | Display a single beer
-//not completed, will be continued
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Nav from '../components/Nav'
