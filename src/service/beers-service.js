01d7b28f950969fdfa618c6c7bf457b466328e6e
Lara_Morgana <laraanjiru@gmail.com>
2020-12-04 12:59:20 +0100

    Service folder added

---
+++ src/service/beers-service.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export default class beersService {
+    constructor() {
+        this.apiHandler = axios.create({ baseURL: 'https://ih-beers-api2.herokuapp.com/beers' })
+    }
+
+    getAllBeers = () => this.apiHandler.get('/')
+    getOneBeer = id => this.apiHandler.get(`/${id}`)
+    getRandomBeer = () => this.apiHandler.get('/random')
+    createBeer = beer => this.apiHandler.post('/new', beer)
+}
