384c21830e5bb9610b005aace3b280a370444adf
Juliana Caruso <juliana.caruso@gmail.com>
2020-12-06 18:27:07 +0100

    missing last iteration

--- src/components/NewBeer.js
+++ src/components/NewBeer.js
@@ -4,32 +4,46 @@ import './NewBeer.css';
 
 export default class NewBeer extends Component {
 
-	addNewBeer = () => {
-		axios.post(`https://ih-beers-api2.herokuapp.com/beers/new`)
-		.then(response => {
-				console.log(response.data)
-		})
-	}
+	state = {
+		name:'',
+		tagline:'',
+		description:'',
+		first_brewed:'',
+		brewers_tips: '',
+		attenuation_Level: '',
+		contributed_by: ''
+}
+
+changeHandler = (event) => {
+	const {name, value} = event.target
+	this.setState ({[name]: value})
+}
+
+addNewBeer = (event) => {
+	event.preventDefault()
+	axios.post('https://ih-beers-api2.herokuapp.com/beers/new',this.state )
+	.then(response => console.log(response))
+}
 
 
 	render() {
 		return (
 			<div className="newBeerForm">
-				<form action="https://ih-beers-api2.herokuapp.com/beers/new" method="POST">
+				<form onSubmit= {this.addNewBeer}>
 					<label htmlFor="name">Name</label><br/>
-					<input name="name" type="text" /><br/>
+					<input onChange = {this.changeHandler} name="name" type="text" /><br/>
 					<label htmlFor="tagline">Tagline</label><br/>
-					<input name="tagline" type="text" /><br/>
+					<input onChange = {this.changeHandler} name="tagline" type="text" /><br/>
 					<label htmlFor="description">Description</label><br/>
-					<textarea name="description" type="text"></textarea><br/><br/>
+					<textarea onChange = {this.changeHandler} name="description" type="text"></textarea><br/><br/>
 					<label htmlFor="first_brewed">First Brewed</label><br/>
-					<input name="first_brewed" type="date" /><br/>
+					<input onChange = {this.changeHandler} name="first_brewed" type="date" /><br/>
 					<label htmlFor="brewers_tips">Brewer's Tips</label><br/>
-					<input name="brewers_tips" type="text" /><br/>
+					<input onChange = {this.changeHandler} name="brewers_tips" type="text" /><br/>
 					<label htmlFor="attenuation_Level">Attenuation Level</label><br/>
-					<input name="attenuation_Level" type="number" /><br/>
+					<input onChange = {this.changeHandler} name="attenuation_Level" type="number" /><br/>
 					<label htmlFor="contributed_by">Contributed by</label><br/>
-					<input name="contributed_by" type="text" /><br/>
+					<input onChange = {this.changeHandler} name="contributed_by" type="text" /><br/>
 					<button type="submit">Add New</button>
 				</form>
 			</div>
