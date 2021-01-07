12db3685428c6ac5306feb2d62fe990beb2b5c72
Marine Sanjuan <marine@sanjuan.eu>
2021-01-07 21:58:30 +0100

    iteration 5 done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Home from './components/Home'
 import Beers from './components/Beers';
 import NewBeer from './components/NewBeer';
-import RandomBeer from './components/NewBeer';
+import RandomBeer from './components/RandomBeer';
 import axios from 'axios';
 import OneBeer from './components/OneBeer'
 
@@ -16,7 +16,7 @@ function App() {
           <Route exact path='/' component={Home}/>
           <Route exact path='/beers' component={Beers}/>
           <Route exact path='/beers' component={NewBeer}/>
-          <Route exact path='/beers' component={RandomBeer}/>
+          <Route exact path='/random-beer' component={RandomBeer}/>
           <Route path="/beers/:id" component={OneBeer} />
         </Switch>
     </div>
--- src/components/RandomBeer.jsx
+++ src/components/RandomBeer.jsx
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import { NavLink } from "react-router-dom";
 import NavMain from './NavMain';
+import axios from 'axios';
 
 export default class RandomBeer extends Component {
+
+    state = {
+    beer: null,
+  };
+
+  componentDidMount() {
+    // console.log(this.props.match.params.id);
+
+    axios
+    .get("https://ih-beers-api2.herokuapp.com/beers/random")
+    .then((apiResponse) => {
+      console.log("the data", apiResponse.data);
+      setTimeout(() => {
+        this.setState({
+          beer: apiResponse.data,
+        });
+      }, 1000);
+    });
+  }
+
     render() {
+        if (!this.state.beer) {
+      return <div>Loading.....</div>;
+    }
+
         return (
             <div>
                 <NavMain />
+            <h2>Detail of a random beer</h2>
+                <div>
+                    <h2>Name of the mighty beer: {this.state.beer.name}</h2>
+                    <img src={this.state.beer.image_url} />
+                    <h3>Tagline: {this.state.beer.tagline}</h3>
+                    <h3>First brewed: {this.state.beer.first_brewed}</h3>
+                    <h3>Attenuation level: {this.state.beer.attenuation_level}</h3>
+                    <h3>Description: {this.state.beer.description}</h3>
+                    <h3>Contributed by the awesome: {this.state.beer.contributed_by}</h3>
+                </div>
             </div>
         )
     }
