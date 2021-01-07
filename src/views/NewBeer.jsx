551236104f2e787d30240e16475b845460b7049a
Charlotte Oury <charlotte.ouryb@gmail.com>
2021-01-07 22:28:17 +0100

    lab done without bonuses

--- src/views/NewBeer.jsx
+++ src/views/NewBeer.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import FormBeer from '../components/FormBeer';
 
 export class NewBeer extends Component {
   render() {
     return (
       <div>
         <Header />
+        <FormBeer />
       </div>
     );
   }
