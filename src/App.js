f2abf9c5ef67319ee0fb2c9bca222ef69e404f32
Menhaj safi <minhaj.sahil01@gmail.com>
2021-01-10 11:24:27 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Beers from './views/Beers';
+import RandomBeer from './views/RandomBeer';
+import NewBeer from './views/NewBeer';
+import Home from './views/Home';
+import OneBeer from './views/OneBeer';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -19,8 +25,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/beers" component={Beers}></Route>
+        <Route exact path="/random-beer" component={RandomBeer}></Route>
+        <Route exact path="/new-beer" component={NewBeer}></Route>
+        <Route exact path="/beers/:id" component={OneBeer} />
+      </Switch>
     </div>
   );
 }
-
 export default App;
