12f6cf53686b58eadccef8194ed0e2308ba1f3e2
Alicja Slupska <ala.slupska@gmail.com>
2021-01-13 11:49:12 +0100

    basic file and routes structure, nav bar and home components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Home from './components/Home';
+/* import ListBeers from './components/ListBeers';
+import SingleBeer from './components/SingleBeer';
+import RandomBeer from './components/RandomBeer';
+import NewBeer from './components/NewBeer'; */
+
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        <Route exact path='/' component={Home} />
+{/*         <Route exact path='/beers' component={ListBeers} />
+        <Route path='/beers/:id' component={SingleBeer} />
+        <Route path='/random-beer' component={RandomBeer} />
+        <Route path='/new-beer' component={NewBeer} /> */}
+      </Switch>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+    return (
+        <div>
+            <nav>
+                <Link to='/'><img src={process.env.PUBLIC_URL + 'images/house.png'} alt="small house icon" className='house-icon' /></Link>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
