a2c76d15d2d0198fadd72ce9de7f86efd1d50cb8
Dirk Kelderman <dirkkelderman@gmail.com>
2021-02-01 16:10:52 +0100

    iteration 3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import Beers from './components/Beers';
 import RandomBeer from './components/RandomBeer';
 import NewBeer from './components/NewBeer';
+import BeerDetails from './components/BeerDetails'
 import {Switch, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 
@@ -15,6 +16,7 @@ function App() {
         <Switch>      
           <Route exact path='/' component={Home} />
           <Route path='/beers' component={Beers} />
+          <Route exact path='/beers/:beerId' component={BeerDetails} /> 
           <Route exact path='/random-beer' component={RandomBeer} />
           <Route exact path='/new-beer' component={NewBeer} />
         </Switch>
