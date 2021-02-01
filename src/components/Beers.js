b5418ec9194efc75ec39d428b938b985b3a9ba90
Dirk Kelderman <dirkkelderman@gmail.com>
2021-02-01 15:12:37 +0100

    iteration 1

---
+++ src/components/Beers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Beers() {
+    return (
+        <div>
+            <h1>All Beers</h1>
+        </div>
+    )
+}
+
+export default Beers
---
+++ src/components/Home.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Beers from './Beers';
+import RandomBeer from './RandomBeer';
+import NewBeer from './NewBeer';
+import { Link } from 'react-router-dom';
+import allBeerImg from './img/Allbeers.png'
+import randomBeerImg from './img/Randombeer.png'
+import newBeerImg from './img/Newbeer.png'
+
+function Home() {
+  return (
+    <div className='homePage'>
+      <h1>IronBeers</h1>
+      <div className='beerSection'>
+        <Link to="/beers">
+          <img src={allBeerImg} alt="all-beers" />
+          <h2>All Beers</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            aliquet fermentum felis eget facilisis. Pellentesque habitant morbi
+            tristique senectus et netus et malesuada fames ac turpis egestas.
+            Proin et mattis justo. Donec iaculis sem eu felis posuere,
+            scelerisque posuere urna consectetur. Sed ac dapibus quam. Vivamus
+            nec posuere sapien, eu pulvinar tellus. Sed eu eleifend nisi. Nam
+            ornare felis vitae elit bibendum, ac tempus augue pulvinar. Mauris
+            mollis est id neque eleifend commodo.
+          </p>
+        </Link>
+      </div>
+      <div className='beerSection'>
+        <Link to="/random-beer">
+          <img src={randomBeerImg} alt="random-beer" />
+          <h2>Random Beers</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            aliquet fermentum felis eget facilisis. Pellentesque habitant morbi
+            tristique senectus et netus et malesuada fames ac turpis egestas.
+            Proin et mattis justo. Donec iaculis sem eu felis posuere,
+            scelerisque posuere urna consectetur. Sed ac dapibus quam. Vivamus
+            nec posuere sapien, eu pulvinar tellus. Sed eu eleifend nisi. Nam
+            ornare felis vitae elit bibendum, ac tempus augue pulvinar. Mauris
+            mollis est id neque eleifend commodo.
+          </p>
+        </Link>
+      </div>
+      <div className='beerSection'>
+        <Link to="/new-beer">
+          {' '}
+          <img src={newBeerImg} alt="new-beer" />
+          <h2>New Beer</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            aliquet fermentum felis eget facilisis. Pellentesque habitant morbi
+            tristique senectus et netus et malesuada fames ac turpis egestas.
+            Proin et mattis justo. Donec iaculis sem eu felis posuere,
+            scelerisque posuere urna consectetur. Sed ac dapibus quam. Vivamus
+            nec posuere sapien, eu pulvinar tellus. Sed eu eleifend nisi. Nam
+            ornare felis vitae elit bibendum, ac tempus augue pulvinar. Mauris
+            mollis est id neque eleifend commodo.
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
