d7396bcc9d3d1f66ce4e99ba39cbc37718259729
MCiappina <mciappops@gmail.com>
2021-02-11 19:52:41 -0300

    iteration 3 done

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from './Home';
+import BeersList from './BeersList';
+import SingleBeer from './SingleBeer';
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact path='/' component={Home} />
+      <Route path="/beers" component={BeersList} />
+      <Route path="/beers/:beerId" component={SingleBeer} />
     </BrowserRouter>
   );
 }
