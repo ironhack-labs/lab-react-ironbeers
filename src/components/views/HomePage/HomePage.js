a20a925fe49bf5bd3c21e38d61617a96c80029e6
Pablo Fernandez <pablo_fz@hotmail.com>
2021-02-21 13:42:04 +0100

    Iteration 1 HomePage

---
+++ src/components/views/HomePage/HomePage.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+export default function HomePage() {
+    const style ={
+        color: "black",
+         textDecoration: "none",
+    }
+    
+  return (
+    <main>
+    <Link to="/beers" style ={style}>
+    <section id="All Beers">
+        <img src="./images/allBeers.PNG" alt="" />
+        <article>
+          <h2>allBeers</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            fuga reiciendis odio nulla asperiores aperiam ipsum et explicabo,
+            provident tempora aspernatur, pariatur id consequuntur eum ducimus,
+            accusantium nostrum consequatur nihil.
+          </p>
+        </article>
+      </section>
+    </Link>
+    <Link to="/random-beer" style ={style}>
+      <section id="randomBeer">
+        <img src="./images/randomBeer.PNG" alt="" />
+        <article>
+        <h2>Random Beer</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            fuga reiciendis odio nulla asperiores aperiam ipsum et explicabo,
+            provident tempora aspernatur, pariatur id consequuntur eum ducimus,
+            accusantium nostrum consequatur nihil.
+          </p>
+        </article>
+      </section>
+      </Link>
+      <Link to="/new-beer" style ={style}>
+      <section id="newBeer">
+        <img src="./images/newBeer.PNG" alt="" />
+        <article>
+        <h2>New Beer</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            fuga reiciendis odio nulla asperiores aperiam ipsum et explicabo,
+            provident tempora aspernatur, pariatur id consequuntur eum ducimus,
+            accusantium nostrum consequatur nihil.
+          </p>
+        </article>
+      </section>
+      </Link>
+    </main>
+  );
+}
