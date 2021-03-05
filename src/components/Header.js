3ddc7d4fd6fbb8c71a007a28c921296f18859d9c
Sebastian Montoya <seb246@gmail.com>
2021-03-04 21:59:21 -0500

    iteration 2

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <p>🏠</p>
+      </nav>
+    );
+  }
+}
+
+export default Header;
