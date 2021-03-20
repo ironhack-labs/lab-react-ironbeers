0b224c865c616e34cf298f44b49da657f62dc341
Anna Lefour <anna.lefour@laposte.net>
2021-03-20 12:07:58 +0100

    It4 done

--- src/Components/DetailsBeer.jsx
+++ src/Components/DetailsBeer.jsx
@@ -1,42 +1,50 @@
-import axios from 'axios'
-import React from 'react'
+import axios from 'axios';
+import React from 'react';
 
 class DetailsBeer extends React.Component {
+  state = {
+    beer: null,
+  };
 
-    state = {
-        beer: null,
+  componentDidMount() {
+    const beerId = this.props.match.params.beerID;
+    // console.log(beerId)
+
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`)
+      .then((resp) => {
+        this.setState({ beer: resp.data });
+        console.log('yo', resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    // console.log("hey",this.props.match)
+
+    if (this.state.beer === null) {
+      return <div>Wait for it...</div>;
     }
-
-    componentDidMount() {
-        const beerId = this.props.match.params.beerID
-        console.log(beerId)
-
-        axios.get(`http://localhost:3000/beers/${beerId}`)
-        .then((resp) => {
-            this.setState({beer : resp.data});
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    }
-
-    render(){
-        console.log(this.state.beer)
-
-        if (this.state.beer === null) {
-            return <div>Wait for it...</div>
-        }
 
     return (
-        <div>
-            <h1>Details</h1>
-            <ul>
-                <li>{this.state.beer.name}</li>
-                {/* <li>{this.state.beer.</li> */}
-            </ul>
-        </div>
-    )
-}
+      <div>
+        <h1>Details</h1>
+        <ul>
+          <li>
+            <img src={this.state.beer.image_url} alt=""></img>
+          </li>
+          <li>Name : {this.state.beer.name}</li>
+          <li>Tagline :{this.state.beer.tagline}</li>
+          <li>First brewed : {this.state.beer.first_brewed}</li>
+          <li>Attenuation level : {this.state.beer.attenuation_level}</li>
+          <li>Description : <p>{this.state.beer.description}</p></li>
+          <li>Contributed by: <p>{this.state.beer.contributed_by}</p> </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default DetailsBeer
+export default DetailsBeer;
