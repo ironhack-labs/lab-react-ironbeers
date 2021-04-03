7b31c8baa56f3ebd00897fdbc6e6dd5619f56f89
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-04-03 15:14:08 +0200

    Add iteration 1, 2

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -4,6 +4,8 @@ import { Route, Switch } from 'react-router-dom'
 //views
 import Home from '../views/Home/Home'
 import AllBeers from '../views/AllBeers/AllBeers'
+import RandomBeer from '../views/RandomBeer/RandomBeer'
+import NewBeer from '../views/NewBeer/NewBeer'
 
 const Router = () => {
 
@@ -11,6 +13,8 @@ const Router = () => {
              <Switch>
                  <Route exact path="/home" component={Home} />
                  <Route exact path="/beers" component={AllBeers} />
+                 <Route exact path="/random-beer" component={RandomBeer} />
+                 <Route exact path="/new-beer" component={NewBeer} />
              </Switch>
     )
 
---
+++ src/components/views/RandomBeer/RandomBeer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RandomBeer = () => {
+    return (
+        <div>
+            <h1>Random beer</h1>
+        </div>
+    );
+};
+
+export default RandomBeer;
