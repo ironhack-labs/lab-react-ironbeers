f484ef5b4105d9f7e6c91becba5a555c71d0fd44
luisga32 <luisga32@hotmail.com>
2021-04-04 20:32:24 +0200

    Iteration 1. Titles changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
     return (
         <div className="Home">
             <SectionHome title='All Beers' img='/assets/beers.png' url='/beers'/>
-            <SectionHome title='All Beers' img='/assets/random-beer.png' url='/random-beer'/>
-            <SectionHome title='All Beers' img='/assets/new-beer.png' url='/new-beer'/>
+            <SectionHome title='Random Beer' img='/assets/random-beer.png' url='/random-beer'/>
+            <SectionHome title='New Beer' img='/assets/new-beer.png' url='/new-beer'/>
 
         </div>
     )
