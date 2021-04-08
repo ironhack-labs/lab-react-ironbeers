ac010ca52cc29d0e3406a94b68a7112f4a035074
monteromario <59968975+monteromario@users.noreply.github.com>
2021-04-08 23:49:05 +0200

    +bonus

--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -4,17 +4,35 @@ import CardColumns from 'react-bootstrap/CardColumns'
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 
 const axios = require('axios').default;
 
 export default class BeerList extends Component {
     state = {
         beers: [],
-        fetch: false
+        fetch: false,
+        search: ''
     }
 
     componentDidMount() {
-        axios.get('https://ih-beers-api2.herokuapp.com/beers')
+            axios.get('https://ih-beers-api2.herokuapp.com/beers')
+            .then((response) => {
+                this.setState({beers: response.data, fetch: true})
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    handleInput = (e) => {
+        this.setState({search: e.target.value})
+        this.search()
+    }
+
+    search() {
+        axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${this.state.search}`)
         .then((response) => {
             this.setState({beers: response.data, fetch: true})
         })
@@ -27,6 +45,17 @@ export default class BeerList extends Component {
         return (
             this.state.fetch === false ? <p className="Loader"><i className="fas fa-beer"></i><br />Loading beer list...</p> : 
             <CardColumns className="List">
+                <InputGroup className="m-2 px-4 pt-2">
+                    <InputGroup.Prepend>
+                    <InputGroup.Text id="search"><i className="fas fa-search"></i></InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                    placeholder="Find beers"
+                    aria-label="search"
+                    aria-describedby="search"
+                    onChange={this.handleInput}
+                    />
+                </InputGroup>
                 {this.state.beers.map(beer => {
                     return (<Link to={{
                         pathname: `/beers/${beer._id}`,
