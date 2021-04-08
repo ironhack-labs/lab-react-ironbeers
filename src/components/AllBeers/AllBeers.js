eb58db15476e3dd0d0299aef6a42640630a9b6b0
karuvsentor <karuvsentor@gmail.com>
2021-04-08 20:53:33 +0200

    It 3 done

--- src/components/AllBeers/AllBeers.js
+++ src/components/AllBeers/AllBeers.js
@@ -1,18 +1,61 @@
 import React from 'react';
+import { Component } from 'react';
 import '../AllBeers/AllBeers.css';
-import Header from '../Header/Header' ;
+import Header from '../Header/Header';
+import Axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const AllBeers = () => {
+class AllBeers extends Component {
 
-   return (
-     
-     <div className="AllBeers">
-         <Header />
-         <h1>All Beers</h1>
-     </div>
+  state = {
+    data: null,
+    loading: true
+  }
 
-   )
+  componentDidMount() {
+    Axios.get(`https://ih-beers-api2.herokuapp.com/beers`)
+      .then(res => {
+        this.setState({
+          data: res.data,
+          loading: false
+        })
+      })
+  }
 
+  render() {
+    return (
+
+      <div className="AllBeers">
+        <Header />
+        <h1>All Beers</h1>
+
+        {this.state.loading ? (<div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>) :
+          <div className="listBeers container">
+            {this.state.data.map((beer) => (
+              <Link to={`/allbeers/${beer._id}`} className="card-list">
+              <div className="row mx-5 my-5" key={beer._id}>
+                <div className="">
+                  <img src ={beer.image_url} alt="beer" height="250"/>
+                </div>
+                <div className="mx-5 d-flex flex-column align-items-start justify-content-center">
+                  <h2>{beer.name}</h2>
+                  <h4>{beer.tagline}</h4>
+                  <p><strong>Created by:</strong>{beer.contributed_by}</p>
+                </div>
+
+              </div>
+              </Link>
+            ))}
+
+          </div>
+        }
+
+      </div>
+
+    )
+  }
 }
 
 export default AllBeers
--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -11,7 +11,7 @@ const AppRouter = () => {
     return (
         <Switch>
             <Route exact path='/' component={Home} />
-            <Route exact path='/Allbeers' component={AllBeers} />
+            <Route exact path='/allbeers' component={AllBeers} />
             <Route exact path='/RandomBeers' component={RandomBeers} />
             <Route exact path='/NewBeers' component={NewBeer} />
         </Switch>
