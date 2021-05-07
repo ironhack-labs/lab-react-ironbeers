cb4e59ed53325af66ef165398b2dcb5a639707e9
Pablo del Barrio <pablodbb@gmail.com>
2021-05-07 15:35:55 +0200

    Iteration 1 done :tada:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router';
 import './App.css';
+import Beers from './pages/Beers/Beers';
+import Home from './pages/Home/Home';
+import NewBeer from './pages/NewBeer/NewBeer';
+import RandomBeer from './pages/RandomBeer/RandomBeer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/beers" component={Beers} />
+        <Route exact path="/random-beer" component={RandomBeer} />
+        <Route exact path="/new-beer" component={NewBeer} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+export default function Home() {
+    return (
+      <div>
+           <Link to="/beers">
+            Beers
+            
+          </Link>
+          <Link to="/random-beer">
+            Random Beer
+        
+          </Link>
+          <Link to="/new-beer">
+            New Beer
+            
+          </Link>
+      </div>
+    )
+  }
