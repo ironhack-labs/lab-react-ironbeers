976a888237bf2cf4969d35756fc9285e98eeb286
Celinaclm <celinaclm@gmail.com>
2021-05-09 22:53:50 +0200

    Iteration 2

--- src/components/pageHeader/PageHeader.js
+++ src/components/pageHeader/PageHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class PageHeader extends React.Component {
     
     render() {
         return(
             <div>
+            <header><Link className="is-size-3" to="/" >&#8962;</Link></header>
             <img src={this.props.image} alt={this.props.title}/>
             <h1 className="title">{this.props.title}</h1>
             <p>{this.props.description}</p>
