636d14b263ab23b667850cbefa37a50e2dbe4f36
Pablo del Barrio <pablodbb@gmail.com>
2021-05-09 13:53:03 +0200

    Styles of beers page completed

--- src/components/Beer/Beer.js
+++ src/components/Beer/Beer.js
@@ -6,13 +6,16 @@ export default function Beer({image_url, name, tagline, contributed_by}) {
     return (
         <Link className="details-link" to="/beers/:id">
         <div className="beer-card">
-            <img src={image_url} alt="image beer" />
-            <div>
+            <div className="beerimg">
+                <img src={image_url} alt="image beer" />
+            </div>
+            <div className="cardtext">
                 <p className="cardname">{name}</p>
                 <p className="cardtag">{tagline}</p>
                 <p className="cardcreated"><span className="cardby">Created by: </span>{contributed_by}</p>
             </div>
         </div>
+            <hr className="bar"/>
         </Link>
     )
 }
