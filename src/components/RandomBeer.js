b064be59982bd48a01c4165cf60f25ba48c50db8
Pietro Ballarin <pietroballarin@outlook.com>
2021-05-10 15:52:32 +0200

    iteration 5 added

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import axios from 'axios';
 
-export default class RandomBeer extends React.Component {
+export default class BeerDetails extends Component {
+
+    state = {
+        beer: {}
+      }
+    
+    
+      componentDidMount() {
+        const beerId = this.props.match.params.id;
+        axios.get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+          .then(response => {
+            this.setState({
+              beer: response.data
+            })
+          })
+          .catch(err => console.log(err))
+      }
+
     render() {
+        const beer = this.state.beer
         return (
-            <div>
-                <header><Link to="/">Home</Link></header>
+            <div key={beer._id}>
+                <img src={beer.image_url} style={{width:"100px", height:"200px"}}/>
+                <h2>{beer.name}</h2>
+                <p>{beer.tagline}</p>
+                <p>{beer.contributed_by}</p>
             </div>
         )
     }
