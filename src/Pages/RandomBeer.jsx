e2e57382f02c920abe28d63cfee955ff03f2edfc
Jordan Caley <jordancaley10@gmail.com>
2021-05-28 16:05:29 +0200

    Iteration 5 without CSS

--- src/Pages/RandomBeer.jsx
+++ src/Pages/RandomBeer.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 import { NavLink } from "react-router-dom";
 
-const RandomBeer = () => {
-  return (
-    <div>
-      <NavLink exact to="/">
-        Home
-      </NavLink>
-      <h1>Random Beer</h1>
-    </div>
-  )
+class RandomBeer extends React.Component {
+  state = {
+    beer: null,
+  };
+
+  componentDidMount() {
+
+    axios
+      .get("https://ih-beers-api2.herokuapp.com/beers/random")
+      .then((response) => {
+        this.setState({
+          beer: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+
+    if (!this.state.beer) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+
+        {this.state.beer && (
+        <img className="beerImg" src={this.state.beer.image_url} alt={this.state.beer.name} />
+        )}
+        <h1>{this.state.beer && this.state.beer.name}</h1>
+        <h2>{this.state.beer && this.state.beer.tagline}</h2>
+        <p>{this.state.beer && this.state.beer.first_brewed}</p>
+        <p>{this.state.beer && this.state.beer.attenuation_level}</p>
+        <p>{this.state.beer && this.state.beer.description}</p>
+        <p>{this.state.beer && this.state.beer.contributed_by}</p>
+      </div>
+    );
+  }
+
 }
 
-export default RandomBeer
+export default RandomBeer;
+
