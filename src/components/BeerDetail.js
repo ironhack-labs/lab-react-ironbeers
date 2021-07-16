98616789a90475917083aede26b888092381a893
giorgiagalasso <giorgiagalasso5@gmail.com>
2021-07-16 13:19:40 +0200

    home and beers done

---
+++ src/components/BeerDetail.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import axios from 'axios';
+
+
+
+export default BeerDetail;
---
+++ src/components/Home.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function Home () {
+return (
+    <>
+    <h1>Home</h1>
+
+    <section className="home-page">
+        <div className="beers">
+          <img src="../assets/beers.png" alt="craft station img" />
+        </div>
+        <div className="beers-text">
+          <Link activeStyle={{color: "red"}} to="/beers">All Beers</Link>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+
+
+      <section className="home-page">
+        <div className="beers">
+          <img src="../assets/random-beer.png" alt="craft close-up" />
+        </div>
+        <div className="beers-text">
+        <Link activeStyle={{color: "red"}} to="/random-beers">Random Beers</Link>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+
+
+
+      <section className="home-page">
+        <div className="beers">
+          <img src="../assets/new-beer.png" alt="beer-bottle"/>
+        </div>
+        <div className="beers-text">
+        <Link activeStyle={{color: "red"}} to="/new-beer">New Beer</Link>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+
+
+   
+    
+    
+    <div className="randombeer"></div>
+    <div className="newbeer"></div>
+
+    </>
+)
+}
+
+export default Home;
