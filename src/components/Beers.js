5cfd8f3d2b89a1c52dfd0bd921c2ca96af14fa15
Beatriz JB <bjbilrero@gmail.com>
2021-07-16 12:19:29 +0100

    home + beers done

---
+++ src/components/Beers.js
@@ -0,0 +1,52 @@
+//import { render } from '@testing-library/react';
+import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class Beers extends React.Component {
+  state = {
+    beers: [],
+  };
+
+  async componentDidMount() {
+    const response = await axios.get(
+      'https://ih-beers-api2.herokuapp.com/beers'
+    );
+    console.log(response.data);
+    this.setState({
+      beers: response.data,
+    });
+  }
+
+  render() {
+    const { beers } = this.state;
+    return (
+      <>
+        {beers.map((beer) => {
+          return (
+            <div className="beers-list">
+              <div className="beers-list-img">
+                <img src={beer.image_url} alt="beer imgs" />
+              </div>
+              <div className="beers-list-text">
+                <Link
+                  activeStyle={{ color: 'red' }}
+                  to={`/${beer._id}`}
+                >
+                  {beer.name}
+                </Link>
+                <h3>{beer.tagline}</h3>
+                <p>
+                  <strong>Created by: </strong>
+                  {beer.contributed_by}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  }
+}
+
+export default Beers;
---
+++ src/components/Home.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <section className="home-page">
+        <div className="beers-img">
+          <img src="../assets/beers.png" alt="craft station" />
+        </div>
+        <div className="beers-text">
+          <Link activeStyle={{ color: 'red' }} to="/beers">
+            All Beers
+          </Link>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+      <section className="home-page">
+        <div className="beers-img">
+          <img src="../assets/random-beer.png" alt="craft tap close-up" />
+        </div>
+        <div className="beers-text">
+          <Link activeStyle={{ color: 'red' }} to="/random-beer">
+            Random Beer
+          </Link>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+      <section className="home-page">
+        <div className="beers-img">
+          <img src="../assets/new-beer.png" alt="beer bottle and glass" />
+        </div>
+        <div className="beers-text">
+          <Link activeStyle={{ color: 'red' }} to="/new-beer">
+            New Beer
+          </Link>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;
