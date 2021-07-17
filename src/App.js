459e2b491177a870dd371d8da3ab6ce64749bffc
elyzx <ellenchildren@icloud.com>
2021-07-17 23:23:37 +0200

    iteration 3 without styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
@@ -6,25 +6,28 @@ import Beers from './components/Beers';
 import NewBeer from './components/NewBeer';
 import RandomBeer from './components/RandomBeer';
 
-function App() {
-  return (
-    <>
-      <Switch>
-        <Route exact path={'/'} render={() => {
-          return <Home />
-        }} />
-        <Route path={'/beers'} render={() => {
-          return <Beers />
-        }} /> 
-        <Route path={'/random-beer'} render={() => {
-          return <RandomBeer />
-        }} />
-        <Route path={'/new-beer'} render={() => {
-          return <NewBeer />
-        }} />
-      </Switch>
-    </>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <>
+        <Switch>
+          <Route exact path={'/'} render={() => {
+            return <Home />
+          }} />
+          <Route path={'/beers'} render={() => {
+            return <Beers />
+          }} /> 
+          <Route path={'/random-beer'} render={() => {
+            return <RandomBeer />
+          }} />
+          <Route path={'/new-beer'} render={() => {
+            return <NewBeer />
+          }} />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default withRouter(App);
--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import Nav from './MyNav';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 class Beers extends Component {
+
+    state = {
+        beers: []
+      }
+    
+    async componentDidMount() {
+        try {
+            let response = await axios.get('https://ih-beers-api2.herokuapp.com/beers')
+            console.log(response.data, 'hello')
+            this.setState({
+            beers: response.data
+          })
+        }
+        catch(error) {
+          console.log('Failed to fetch beers ', error)
+        }
+      }
+
     render() {
+        const {beers} = this.state
         return (
             <div>
                 <Nav />
-                hello
+                <h2>Beers</h2>
+                {
+                    beers.map((beer, i) => {
+                        return <div key={i}>
+                                    <img src={beer.image_url} style={{width: '50px'}}/>
+                                    <Link to={`/all-the-beers/${beer._id}`}>{beer.name}</Link>
+                                    <p>{beer.tagline}</p>
+                                    <p>Contributor: {beer.contributed_by} </p>
+
+                        </div>
+                    })
+                }
             </div>
         )
     }
