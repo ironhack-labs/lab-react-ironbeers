71cbce14820f0a8bd6e42741f519d15a6d9827b0
dechilds1@gmail.com <dechilds1@gmail.com>
2021-07-21 09:59:43 +0200

    Iteration 6 working

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,17 @@ import React from "react"
 export default function () {
     return (
         <div>
-            <Link to="/ListBeers">
+            <Link className="homeLink" to="/ListBeers">
                 <img src= "assets/beers.png" alt="beers"/>
                 <h1>All Beers</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
             </Link>
-            <Link to="/RandomBeer">
+            <Link className="homeLink" to="/RandomBeer">
              <img src= "assets/random-beer.png" alt="beers"/>
                 <h1>Random Beer</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
             </Link>
-            <Link to="/NewBeer">
+            <Link className="homeLink" to="/NewBeer">
              <img src= "assets/new-beer.png" alt="beers"/>
                 <h1>New Beer</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
--- src/components/NewBeer.js
+++ src/components/NewBeer.js
@@ -1,10 +1,58 @@
+import React , { useState }from 'react'
 import {Link} from "react-router-dom"
-import React from "react"
+import axios from 'axios'
 
-export default function NewBeer(){
+function NewBeer(){
+
+    const initialFormState = {
+        name: "",
+        tagline: "",
+        description: "",
+        first_brewed: "",
+        brewers_tips: "",
+        attenuation_level: 0,
+        contributed_by: ""
+    }
+
+    const [formState, setFormState] = useState(initialFormState)
+
+    function handleChange (event) {  
+        const {name, value} = event.target;
+        setFormState({...formState, [name]: value});
+    }
+
+    function handleFormSubmit(event){
+        event.preventDefault()
+        const {name, tagline, description, first_brewed, brewers_tips, attenuation_level, contributed_by} = formState
+
+        axios.post('https://ih-beers-api2.herokuapp.com/beers/new', {name, tagline, description, first_brewed, brewers_tips, attenuation_level, contributed_by})
+        .then(setFormState(initialFormState))
+        .catch(err=>console.log(err))
+    }
+
     return (
         <div>
-            <header className="layoutHead"><Link to="/"><img src="assets/house.png" alt="house"/></Link></header>
-        </div>
+        <header className="layoutHead"><Link to="/"><img src="assets/house.png" alt="house"/></Link></header>
+        <form onSubmit={handleFormSubmit}>
+          <label>Name:</label>
+          <input type="text" name="name" value={formState.name} onChange={ e => handleChange(e)}/>
+          <label>Tagline:</label>
+          <input type="text" name="tagline" value={formState.tagline} onChange={ e => handleChange(e)}/>
+          <label>Description:</label>
+          <input type="text" name="description" value={formState.description} onChange={ e => handleChange(e)}/>
+          <label>First Brewed:</label>
+          <input type="text" name="first_brewed" value={formState.first_brewed} onChange={ e => handleChange(e)}/>
+          <label>Brewers Tips:</label>
+          <input type="text" name="brewers_tips" value={formState.brewers_tips} onChange={ e => handleChange(e)}/>
+          <label>Attenuation Level:</label>
+          <input type="number" name="attenuation_level" value={formState.attenuation_level} onChange={ e => handleChange(e)}/>
+          <label>Contributed By:</label>
+          <input type="text" name="contributed_by" value={formState.contributed_by} onChange={ e => handleChange(e)}/>
+          
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     )
 }
+
+export default NewBeer
