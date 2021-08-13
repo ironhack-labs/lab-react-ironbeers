f40e28ab894c48d4e11a3b08cd0f98e1b4cdfb82
Chenjia Li <cligrenet@gmail.com>
2021-08-13 22:34:59 +0200

    did some cleaning

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import { Switch } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './Components/HomePage';
 import AllBeers from './Components/AllBeers';
 import RandomBeer from './Components/RandomBeer';
 import NewBeer from './Components/NewBeer';
 import OneBeer from './Components/OneBeer';
-// import Navbar from './Components/Navbar';
-// import axios from 'axios';
 
-class App extends Component {
-  state = {
-    beers: [],
-  };
-
-  // getBeersData() {
-  //   axios
-  //     .get('https://ih-beers-api2.herokuapp.com/beers')
-  //     .then((res) => {
-  //       console.log(res);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }
-
-  render() {
-    return (
-      <div className="App">
-        <main>
-          {/* <Switch> */}
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Switch>
           <Route exact path="/" component={HomePage} />
 
           <Route exact path="/beers" component={AllBeers} />
@@ -39,11 +21,10 @@ class App extends Component {
           <Route exact path="/random" component={RandomBeer} />
 
           <Route exact path="/create" component={NewBeer} />
-          {/* </Switch> */}
-        </main>
-      </div>
-    );
-  }
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/AllBeers.jsx
+++ src/Components/AllBeers.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Navbar from './Navbar';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-// import OneBeer from './OneBeer';
 
 class AllBeers extends Component {
   state = {
