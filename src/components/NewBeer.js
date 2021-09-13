7901b8d54f57076def37ac96ee008ad03b6ac0c7
michaelporreca <michaelporreca@gmail.com>
2021-09-13 15:39:19 -0700

    created new beer page

---
+++ src/components/NewBeer.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import React from 'react';
+import {useState} from 'react';
+
+
+const NewBeer = () => {
+	let newBeerName
+	
+	let [ beer, setBeer] = useState("");
+	
+
+
+	const handleChange = (e) => {
+				setBeer(e.target.value);
+	}
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(beer)
+		axios.post("https://ih-beers-api2.herokuapp.com/beers/new",{name:beer}).then(function(res){
+			console.log(res.data)
+		})
+	}
+
+	return (
+		<div>
+			<h1>New Beers</h1>
+
+			<form onSubmit={handleSubmit}>
+				<input onChange={handleChange} placeholder="something funny"></input>
+				<button>Add Beer</button>
+			</form>
+		</div>
+	);
+};
+
+export default NewBeer;
