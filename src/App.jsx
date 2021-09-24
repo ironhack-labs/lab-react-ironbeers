07110b891629a0ead17114d7e6852c0f798246f9
Ana Martins <ana.af.martins@gmail.com>
2021-09-24 18:42:43 +0200

    lab done with team AWESOME

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Beers from './pages/Beers';
+import NewBeers from './pages/NewBeer';
+import RandomBeers from './pages/RandomBeer';
+import BeerDetails from './pages/BeerDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/beers/:id" component={BeerDetails} />
+        <Route exact path="/new-beer" component={NewBeers} />
+        <Route exact path="/random-beer" component={RandomBeers} />
+        <Route exact path="/beers" component={Beers} />
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/NewBeer.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import Header from '../components/Header';
+import { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+export default function NewBeer() {
+  const [name, setName] = useState('');
+  const [tagline, setTagline] = useState('');
+  const [description, setDescription] = useState('');
+  const [first_brewed, setFirst_brewed] = useState('');
+  const [brewers_tips, setBrewers_tips] = useState('');
+  const [attenuation_level, setAttenuation_level] = useState(0);
+  const [contributed_by, setContributed_by] = useState('');
+
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const body = {
+      name,
+      tagline,
+      description,
+      first_brewed,
+      brewers_tips,
+      attenuation_level,
+      contributed_by,
+    };
+
+    try {
+      const response = await axios.post(
+        'https://ih-beers-api2.herokuapp.com/beers/new',
+        body
+      );
+      console.log(response);
+      //Reset the state of the form fields!
+      setName('');
+      setTagline('');
+      setDescription('');
+      setFirst_brewed('');
+      setBrewers_tips('');
+      setAttenuation_level('');
+      setContributed_by('');
+      //redirect user
+      history.push('/beers');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="AddApartmentPage">
+      <Header />
+      <h3>Add New Beer</h3>
+
+      <form onSubmit={handleSubmit}>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <label>tagline</label>
+        <input
+          type="text"
+          name="tagline"
+          onChange={(e) => setTagline(e.target.value)}
+          value={tagline}
+        />
+        <label>description</label>
+        <input
+          type="text"
+          name="description"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
+        <label>first brewed</label>
+        <input
+          type="text"
+          name="first_brewed"
+          onChange={(e) => setFirst_brewed(e.target.value)}
+          value={first_brewed}
+        />
+        <label>attenuation level</label>
+        <input
+          type="number"
+          name="attenuation_level"
+          onChange={(e) => setAttenuation_level(e.target.value)}
+          value={attenuation_level}
+        />
+
+        <label>contributed by</label>
+        <input
+          type="text"
+          name="contributed_by"
+          onChange={(e) => setContributed_by(e.target.value)}
+          value={contributed_by}
+        />
+
+        <button type="submit">Create new Beer</button>
+      </form>
+    </div>
+  );
+}
