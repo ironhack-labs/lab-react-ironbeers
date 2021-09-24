410459623b82d25a9a181d28d472c13cc56e9d03
Esteban <esteban.github@gmail.com>
2021-09-24 16:04:34 +0200

    fix images links in Homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,17 +6,17 @@ export default function Home() {
         <div>
             <Link to="/beers">All Beers</Link>
             <br />
-            <img src="../assets/beers.png" alt="All Beers logo" />
+            <img src="../assets/beers.png" width="300px" alt="All Beers logo" />
             <br />
 
             <Link to="/random-beer">Random Beer</Link>
             <br />
-            <img src="../assets/random-beer.png" alt="Random Beer logo" />
+            <img src="../assets/random-beer.png" width="300px" alt="Random Beer logo" />
             <br />
 
             <Link to="/new-beer">New Beer</Link>
             <br />
-            <img src="../assets/new-beer.png" alt="New Beer logo" />
+            <img src="../assets/new-beer.png" width="300px" alt="New Beer logo" />
             <br />
 
 
