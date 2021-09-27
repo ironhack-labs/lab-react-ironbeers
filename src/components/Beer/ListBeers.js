9d30738ac5e29430f3494daa54d98466541cd4aa
andrekleine <andrekleine@gmail.com>
2021-09-27 10:28:47 -0300

    Done

--- src/components/Beer/ListBeers.js
+++ src/components/Beer/ListBeers.js
@@ -3,9 +3,11 @@ import React, { useState, useEffect } from 'react';
 
 import Header from '../Header';
 import BeerCard from '../Cards/ListBeerCard';
+import FormField from '../FormField';
 
 function ListBeers() {
   const [beers, setBeers] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios
@@ -16,19 +18,40 @@ function ListBeers() {
       .catch((err) => console.error(err));
   }, []);
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${search}`)
+      .then((response) => {
+        setBeers([...response.data]);
+      })
+      .catch((err) => console.error(err));
+  }, [search]);
+
   return (
     <div>
       <Header />
-      <div className="container mt-2">
+      <div className="container mt-5">
+        <FormField
+          label="Search for a beer"
+          name="name"
+          onChange={handleChange}
+          value={search}
+        />
         {beers.map((beerObj) => {
-          return <BeerCard 
-          img={beerObj.image_url} 
-          name={beerObj.name}
-          key={beerObj._id}
-          tagline={beerObj.tagline}
-          contributed={beerObj.contributed_by}
-          id={beerObj._id}
-          />;
+          return (
+            <BeerCard
+              img={beerObj.image_url}
+              name={beerObj.name}
+              key={beerObj._id}
+              tagline={beerObj.tagline}
+              contributed={beerObj.contributed_by}
+              id={beerObj._id}
+            />
+          );
         })}
       </div>
     </div>
