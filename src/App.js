ec60299bc25241f3efc4136b3f400ff8a68c7b60
Alice <alice.jost@outlook.de>
2021-11-09 00:15:09 +0100

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -6,19 +6,51 @@ import { Link } from 'react-router-dom';
 import RandomBeer from './components/RandomBeer';
 import NewBeer from './components/NewBeer';
 import Beers from './components/Beers';
+import axios from 'axios'
 
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/random-beer' component={RandomBeer} />
-        <Route exact path='/new-beer' component={NewBeer} />
-        <Route exact path='/beers' component={Beers} />
-      </Switch>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      listOfBeers: []
+    }
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route exact path='/random-beer'>
+            <RandomBeer/>
+          </Route>
+          <Route exact path='/new-beer'>
+            <NewBeer/>
+          </Route>
+          <Route exact path='/beers'>
+            <Beers beers={this.state.listOfBeers}/>
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://ih-beers-api2.herokuapp.com/beers')
+      .then((response) => {
+        console.log('list of beers....', response.data);
+        this.setState({
+          listOfBeers: response.data,
+        });
+      })
+
+      .catch((e) => {
+        console.log('error getting characters from API', e);
+      });
+
+    }
+  }
--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Header from './Header'
 
-export default function Beers() {
+export default function Beers(props) {
     return (
         <div>
-            <Header/>            
+            <Header/>
+            {props.beers.map((elm) => { return (
+                <>
+                <h2>{elm.name}</h2>
+                <p>{elm.tagline}</p>
+                <p>By: {elm.contributed_by}</p> 
+                <img src={elm.image_url}/>
+                <Link exact to={`/beers/${elm._id}`}>See more</Link>
+                </>
+            )})}
         </div>
     )
 }
