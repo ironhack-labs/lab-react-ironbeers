c9b0fa99b19619e5c3a846e3daa58b310af96169
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-11-08 21:01:03 +0100

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AllBeers from './components/AllBeers';
+import RandomBeer from './components/RandomBeer';
+import NewBeer from './components/NewBeer';
+import Homepage from './components/Homepage';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  render(){
+      return (
+
+              <div className="App">
+                <Switch>
+                  <Route path="/"><Homepage/></Route>
+                </Switch>                
+              </div>
+      )
+  }
+
+  }
+ 
 
 export default App;
---
+++ src/components/Homepage.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {NavLink} from "react-router-dom";
+import "../App.css"
+import imgAllBeers from "../assets/beers.png"
+import imgNewBeer from "../assets/new-beer.png"
+import imgRandomeBeer from "../assets/random-beer.png"
+
+
+
+class Homepage extends React.Component {
+    render(){
+        return (
+            <div className="Homepage">
+                        <img src={imgAllBeers}/>
+                        <NavLink to="/beers" activeClassName="selected">
+                            <h2>All Beers</h2>  
+                        </NavLink>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                    
+
+                        <img src={imgNewBeer}/>
+                        <NavLink to="/new-beer" activeClassName="selected">                       
+                            <h2>New Beer</h2>
+                        </NavLink>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                        
+                    
+
+                        <img src={imgRandomeBeer}/>
+                        <NavLink to="/random-beer" activeClassName="selected">                           
+                            <h2>Random Beer</h2>
+                        </NavLink>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                        
+                    
+                         
+            </div>        
+        )
+    }
+}
+   
+
+
+export default Homepage
