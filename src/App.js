d0a44d10ed840663442c179dd531b345890e4f1e
Kati Fe <k-felgitsch@hotmail.com>
2021-11-08 22:43:42 +0000

    Iteration 3 displaying error

--- src/App.js
+++ src/App.js
@@ -6,11 +6,34 @@ import RandomBeer from './components/RandomBeer';
 import NewBeer from './components/NewBeer';
 import Homepage from './components/Homepage';
 import { Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 
 
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      beers: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://ih-beers-api2.herokuapp.com/beers')
+      .then((response) => {
+        this.setState({
+          beers: response.data,
+        });
+        console.log("getting info from the API", response.data)
+      })
+      .catch((e) => {
+        console.log('error getting beers from API', e);
+      });
+  }
+
+
   render(){
     return(
 
@@ -19,7 +42,9 @@ class App extends Component {
          
         <Switch>
         <Route exact path="/" component={Homepage} />
-          <Route exact path="/beers" component={Beers} />
+          <Route exact path="/beers">
+          <Beers {this.state.beers}/>
+          </Route>
           <Route exact path="/new-beer" component={NewBeer} />
           <Route exact path="/random-beer" component={RandomBeer} />
         </Switch>
--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import { Link } from 'react-router-dom';
 
 
 
@@ -9,8 +10,23 @@ class Beers extends React.Component {
     return (
         <div className='beer-container'>
         <Header/>
-
         <h2>All Beers</h2>
+        {this.beers.map((beer)=>{
+            return(
+                <div>
+                    <div>
+                        <img src={beer.imgage_url} alt="" />
+                    </div>
+                    <div>
+                    <h4>{beer.name}</h4>
+                    <h5>{beer.tagline}</h5>
+                    <p>{beer.contributed_by}</p>
+                  </div>
+                  <Link to={`/beers/${beer.id}`} >More details</Link>
+                </div>
+            )
+        })}
+       
         </div>
     )
     }
