af2e51c9eecee1920724c04bcaec4820a1f1006e
CMCandidate <CMCandidate@tw.com>
2021-12-02 20:51:12 +0100

    header añadido, pero no en forma de imagen

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './Header.css'
+// import { NavLink } from 'react-router-dom'
+
+
+const Header = () => {
+
+    return (
+
+        <header className='headerHome'>
+            <a href="/"><h2>HOME</h2></a>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/HomePage.js
@@ -0,0 +1,30 @@
+import React from "react"
+import beers1 from '../assets/beers.png'
+import beers2 from '../assets/new-beer.png'
+import beers3 from '../assets/random-beer.png'
+import './HomePage.css'
+
+const HomePage = () => {
+    return (
+        <> <div className='home'>
+            <div>
+                <img src={beers1} alt='beers1img' />
+                <h2>All Beers</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, eligendi vero inventore neque corrupti nisi? Fuga soluta impedit ex aut architecto. Accusamus id magni eaque temporibus voluptatem eos odit amet.</p>
+            </div>
+
+            <div>
+                <img src={beers2} alt="" />
+                <h2>Random Beer</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, eligendi vero inventore neque corrupti nisi? Fuga soluta impedit ex aut architecto. Accusamus id magni eaque temporibus voluptatem eos odit amet.</p>            </div>
+
+            <div>
+                <img src={beers3} alt="" />
+                <h2>New Beer</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, eligendi vero inventore neque corrupti nisi? Fuga soluta impedit ex aut architecto. Accusamus id magni eaque temporibus voluptatem eos odit amet.</p>            </div>
+        </div>
+        </>
+    )
+}
+
+export default HomePage
