25fb845f30556ae06493e6f9cf916a119673632c
abdelmalek.saksioui <abdelmalek.saksioui@cgm.com>
2021-12-03 19:31:51 +0100

    BONUS

--- src/components/BeersList.js
+++ src/components/BeersList.js
@@ -4,20 +4,25 @@ import {Link} from "react-router-dom";
 
 export const BeersList = () => {
     const [beers, setBeers] = useState(null)
+    const [query, setQuery] = useState("")
 
     useEffect(() => {
 
-        axios.get("https://ih-beers-api2.herokuapp.com/beers")
+        axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${query}`)
             .then(response => setBeers(response.data))
             .catch(err => console.log(err))
 
-    }, []);
+    }, [query]);
 
 
     if (!beers) return <></>
     console.log(beers)
     return (
         <>
+            <form style={{padding: '1rem'}}>
+                <input value={query} type="search" name="search" placeholder="Search..."
+                       onChange={e => setQuery(e.target.value)}/>
+            </form>
             {
                 beers.map(beer => {
                     return <div key={beer._id} className="beer">
