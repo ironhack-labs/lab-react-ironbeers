b39423ebae64a75e1e79db69fae63cd462c083b0
Analia Lopez <analialopezrodriguez@gmail.com>
2021-12-03 07:46:01 +0100

    Lab sin terminar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+// import Beers from './components/Beers'
+// import RandomBeer from '../RandomBeer'
+// import NewBeer from '../NewBeer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    function App() {
+      return (
+        <div className="App">
+          <Switch>
+            <Route exact path='/' component={Home} />
+              {/* <Route exact path='/beers'>
+                <Beers />
+              </Route> 
+              <Route  path='/new-beer'>
+                <NewBeer />
+              </Route>
+              <Route  path='/random-beer'>
+                <RandomBeer />
+              </Route> */}
+          </Switch>
+        </div>
+      )
+    }
+  
 
 export default App;
