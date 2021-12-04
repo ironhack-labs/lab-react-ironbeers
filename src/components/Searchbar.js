45850eba14a9183b86442c6b11c5723c364c0f31
fabioFG <fabio.fernando.guerreiro@gmail.com>
2021-12-04 20:05:19 +0000

    lab finished

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,39 +1,27 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-function Searchbar() {
+function Searchbar({ filterBeerList }) {
   const [search, setSearch] = useState();
-  const [searchErrorMessage, setSearchErrorMessage] = useState('');
+
 
   //search input handle
 
-  const handleSearch = (e) => setSearch(e.target.value);
-
-  const handleSearchSubmit = async (e) => {
-    try {
-      //prevent page reload on search
-      e.preventDefault();
-    } catch (error) {
-      setSearchErrorMessage('Beer not found');
-    }
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+    filterBeerList(event.target.value);
   };
 
   return (
     <div>
       <h2>Searchbar</h2>
 
-      {/* SEARCHBAR */}
-      <form onSubmit={handleSearchSubmit}>
-        <input
-          type="text"
-          placeholder="Search for a Beer"
-          name="search"
-          value={search}
-          onClick={handleSearch}
-        />
-
-        <button type="submit">Search</button>
-      </form>
+      <input
+        type="text"
+        placeholder="Search for a Beer"
+        value={search}
+        onChange={handleSearch}
+      />
     </div>
   );
 }
--- src/pages/AllBeersPage.js
+++ src/pages/AllBeersPage.js
@@ -7,49 +7,63 @@ import Searchbar from '../components/Searchbar';
 
 const apiURL = 'https://ih-beers-api2.herokuapp.com/beers';
 
-console.log(apiURL);
-
 function AllBeersPage() {
   //set a state for the objects in the API
   const [beers, setBeers] = useState([]);
-  console.log('Initial state');
+  //Import API beers in order to get a second array with all the beers that remains unchanged so the search function works whenever there is no direct match
+  const [searchBeers, setSearchBeers] = useState([]);
 
   //use effect to run the mounting and using axios to get the data
   useEffect(() => {
     const fetchData = async () => {
-      console.log('effect');
       const response = await axios.get(`${apiURL}/`);
       const beersData = response.data;
 
-      console.log('beersData', beersData);
       setBeers(beersData);
+      setSearchBeers(beersData);
     };
 
     fetchData();
   }, []);
 
+  //Search for a specific beer out of all beers
+
+  const filterBeerList = (char) => {
+    let filteredBeer;
+    if (char === '') {
+      filteredBeer = searchBeers; //if empty string it shows all beers
+    } else {
+      filteredBeer = beers.filter((oneBeer) => {
+        return oneBeer.name.toLowerCase().includes(char.toLowerCase());
+      });
+    }
+
+    setBeers(filteredBeer);
+  };
+
   return (
     <div>
       <Header />
-      <Searchbar />
-      
+      <Searchbar filterBeerList={filterBeerList} />
+
       <h2>All Beers</h2>
       {beers &&
         beers.map((oneBeer) => {
-          console.log('onebeer', oneBeer);
           return (
-            <Link to={"/beers/details/" + oneBeer._id} key={oneBeer._id}>
-              <div className="container">
-                <div>
+            <div className="container">
+              <Link to={'/beers/details/' + oneBeer._id} key={oneBeer._id}>
+                <div classname="image-container">
                   <img src={oneBeer.image_url} alt="" height="200px" />
                 </div>
                 <div>
                   <p>{oneBeer.name}</p>
-                  <p>{oneBeer.tagline}</p>
-                  <p>{oneBeer.contributed_by}</p>
                 </div>
+              </Link>
+              <div>
+                <p>{oneBeer.tagline}</p>
+                <p>{oneBeer.contributed_by}</p>
               </div>
-            </Link>
+            </div>
           );
         })}
     </div>
