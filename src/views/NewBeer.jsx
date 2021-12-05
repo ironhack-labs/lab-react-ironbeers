6c7ebd0539252b422dda888beec1b48af35d4d5c
Monica Saiz <moni.sm@gmail.com>
2021-12-05 19:25:52 +0100

    New Beer component and axios call

--- src/views/NewBeer.jsx
+++ src/views/NewBeer.jsx
@@ -1,47 +1,82 @@
 import React from 'react'
 import { useState } from 'react'
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import Header from '../components/Header';
+
 
 const NewBeer = () => {
 
     const [formState, setFormState] = useState({})
+    const history = useHistory()
 
     const handleSubmit = (e) => {
         e.preventDefault()
 
+        const urlNewBeer = "https://ih-beers-api2.herokuapp.com/beers/new/"
+
+        axios
+        .post(urlNewBeer, formState)
+        .then( (response) => {
+            setFormState({}) 
+            history.push("/")        //history is an array!  
+        })
+        .catch( console.log )
     }
 
     const handleInput = (e) => {
-        console.log("I am in handle input")
+        setFormState({...formState, [e.target.name]: e.target.value})
     }
 
-    
     return (
-        <div className="add-beer-form">
-            <h3>New Beer</h3>
+        <>
+            <Header />
+            <div className="add-beer-form">
+                <h3>New Beer</h3>
             
-            <form onSubmit={handleSubmit} >
-            <label>Title</label>
-            <input
-                type="text"
-                name="headline"
-                //onChange={(e) => setHeadline(e.target.value)}
-                onChange={ (e) => handleInput}
-                value={formState.headline}
-            />
+                <form onSubmit={handleSubmit} >
+                    
+                    <div className="fields">
+                        <label>Name</label>
+                        <input type="text" name="name" value={formState.name || ""} 
+                                onChange={ (e) => handleInput(e)} />
+                    </div>
 
-            <label>Price per Day</label>
-            <input
-                type="number"
-                name="pricePerDay"
-                //onChange={(e) => setPrice(e.target.value)}
-                onChange={ (e) => handleInput}
-                value={formState.price}
-            />
-            
-            <button type="submit">Create Apartment</button>
-            
-            </form>
-        </div>
+                    <div className="fields">
+                        <label>Tagline</label>
+                        <input type="text" name="tagline" value={formState.tagline || ""} 
+                                onChange={ (e) => handleInput(e)} />
+                    </div>
+
+                    <div className="fields">
+                        <label>Description</label>
+                        <input type="text" name="description" value={formState.description || ""} 
+                                onChange={ (e) => handleInput(e)} />
+                    </div>
+
+                    <div className="fields">
+                        <label>Brewers Tips</label>
+                        <input type="text" name="brewers_tips" value={formState.brewers_tips || ""} 
+                                onChange={ (e) => handleInput(e)} />
+                    </div>
+
+                    <div className="fields">
+                        <label>Attenuation Level</label>
+                        <input type="number" name="attenuation_level" value={formState.attenuation_level || ""} 
+                                onChange={ (e) => handleInput(e)} />
+                    </div>
+
+                    <div className="fields">
+                        <label>Contributed By</label>
+                        <input type="text" name="contributed_by" value={formState.contributed_by || ""} 
+                                onChange={ (e) => handleInput(e)} />
+                    </div>
+                    
+                    <button type="submit">Add New</button>
+                
+                </form>
+            </div>
+        </>
     )
 }
 
