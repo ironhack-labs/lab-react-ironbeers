b4be18a5aa6387902b9e57892b115e6a09076f6a
Michael Seitz <michael.s.seitz@gmail.com>
2022-01-29 23:38:55 +0100

    iteration 3

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://ih-beers-api2.herokuapp.com/beers",
+});
 
 function Beers() {
+  async function getBeers() {
+    const { data } = await api.get("/");
+    setBeers(data);
+  }
+  const [beers, setBeers] = React.useState([]);
+  console.log(beers);
+  React.useEffect(() => {
+    getBeers();
+  }, []);
+
   return (
     <div>
       <header>
@@ -13,6 +28,25 @@ function Beers() {
         </Link>
       </header>
       <h1>Beers</h1>
+      {beers.map((beer) => {
+        return (
+          <div key={beer._id}>
+            <h2>{beer.name}</h2>
+            <Link to={`/beers/${beer._id}`}>
+            <img
+                src={beer.image_url}
+                alt={beer.name}
+                style={{ width: "100px" }}
+            />
+            </Link>
+
+            <p>{beer.tagline}</p>
+            <p>by</p>
+            <h4>{beer.contributed_by}</h4>
+            <hr />
+          </div>
+        );
+      })}
     </div>
   );
 }
