06335aedb6709f9aa9fe04ccef8b8a524051b3d1
Philipp Kastl <picastle@posteo.de>
2022-01-30 16:56:24 +0100

    iteration 5

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,9 +1,26 @@
 import React from "react";
-
+import axios from 'axios'
+import { getAllByPlaceholderText } from "@testing-library/react";
 function RandomBeer() {
+
+  const api = axios.create({baseURL: "https://ih-beers-api2.herokuapp.com/beers/"})
+
+  const [beer, setBeer] = React.useState({})
+
+  const getData = async() =>{
+    const {data} = await api.get('random')
+    setBeer(data)
+  }
+
+  React.useEffect(()=>{
+    getData();
+  }, [])
+
   return (
     <div>
       <h1>Random Beer</h1>
+      <p>{beer.name}</p>
+      <img width="150px" src={beer.image_url} atl={beer.name} />
     </div>
   );
 }
