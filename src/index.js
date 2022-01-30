3b4e7a234eb4b083f79b4a44bf1b5fb8904a69d8
Philipp Kastl <picastle@posteo.de>
2022-01-30 22:13:52 +0100

    try bugfix for gh pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
     </HashRouter>
   </React.StrictMode>,
