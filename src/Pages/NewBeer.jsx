406391e0897260ab322602095b8a849d9b43e150
Chun Lam <Chun01@hotmail.fr>
2022-01-30 23:25:54 +0100

    SolvedLab

--- src/Pages/NewBeer.jsx
+++ src/Pages/NewBeer.jsx
@@ -1,12 +1,124 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const NewBeer = () => {
+  const [name, setName] = useState("");
+  const [tagline, setTagline] = useState("");
+  const [description, setDescription] = useState("");
+  const [firstBrewed, setFirstBrewed] = useState("");
+  const [brewersTips, setBrewersTips] = useState("");
+  const [attenuationLevel, setAttenuationLevel] = useState("");
+  const [contributedBy, setContributedBy] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      name,
+      tagline,
+      description,
+      firstBrewed,
+      brewersTips,
+      attenuationLevel,
+      contributedBy,
+    };
+    axios
+      .post("https://ih-beers-api2.herokuapp.com/beers/new", data)
+      .then((response) => {
+        console.log(response);
+        navigate("/beer");
+      })
+      .catch((e) => {
+        console.log(e);
+        setError("There was an error, please try again");
+      });
+  };
+
   return (
     <div>
       <h1>HIHI</h1>
-      <Link to="/"><i class="fas fa-home"></i>Home</Link>
-
+      <Link to="/">
+        <i class="fas fa-home"></i>Home
+      </Link>
+      <form onSubmit={handleSubmit}>
+        {error !== "" && <p>{error}</p>}
+        <div>
+          <label htmlFor="name">name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="tagline">tagline</label>
+          <input
+            type="text"
+            name="tagline"
+            id="tagline"
+            value={tagline}
+            onChange={(e) => setTagline(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="description">description</label>
+          <input
+            type="text"
+            id="description"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="firstBrewed">firstBrewed</label>
+          <input
+            type="text"
+            id="firstBrewed"
+            name="firstBrewed"
+            value={firstBrewed}
+            onChange={(e) => setFirstBrewed(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="brewersTips">brewersTips</label>
+          <input
+            type="text"
+            id="brewersTips"
+            name="brewersTips"
+            value={brewersTips}
+            onChange={(e) => setBrewersTips(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="attenuationLevel">attenuationLevel</label>
+          <input
+            type="number"
+            id="attenuationLevel"
+            name="attenuationLevel"
+            value={attenuationLevel}
+            onChange={(e) => setAttenuationLevel(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="contributedBy">contributedBy</label>
+          <input
+            type="text"
+            id="contributedBy"
+            name="contributedBy"
+            value={contributedBy}
+            onChange={(e) => setContributedBy(e.target.value)}
+          />
+        </div>
+        <button>Create a beer !</button>
+      </form>
     </div>
   );
 };
--- src/Pages/RandomBeer.jsx
+++ src/Pages/RandomBeer.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-const RandomBeer = () => {
+const RandomBeer = (props) => {
+  const [randomBeers,SetrandomBeers]= useState([])
+  useEffect(() => {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+      .then((response) => {
+        console.log(response);
+        SetrandomBeers(response.data);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+
   return (
-    <div>
+    <div style={{display:"flex",flexDirection:"column"}}>
       <h1>HAHA</h1>
       <Link to="/"><i class="fas fa-home"></i>Home</Link>
-
+      <img  style={{width:"80px"}} src={randomBeers.image_url} alt='Beer'/>
+  <p>{randomBeers.name}</p>
+  <p>{randomBeers.tagline}</p>
+  <p>{randomBeers.first_brewed}</p>
+  <p>{randomBeers.attenuation_level}</p>
+  <p>{randomBeers.description}</p>
+  <p>{randomBeers.contributed_by}</p>   
     </div>
   );
 };
