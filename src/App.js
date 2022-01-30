570d971dd377bf54f400ff66ea0b8ca6127f20f8
Philipp Kastl <picastle@posteo.de>
2022-01-30 14:35:03 +0100

    iteration 1
    Co-authored-by: Gk1mtd <Gk1mtd@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import {Link} from "react-router-dom"
+import imgBeers from "./assets/beers.png"
+import imgRandomBeer from "./assets/random-beer.png"
+import imgNewBeer from "./assets/new-beer.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to='/beers'><img src={imgBeers} alt='bar of a pub'/>Show me your Beers!</Link>
+      <Link to='/random-beers'><img src={imgRandomBeer} alt='beer dispensers'/>A Random Beer!</Link>
+      <Link to='/new-beer'><img src={imgNewBeer} alt='beer'/>Add me a new Beer!</Link>
     </div>
   );
 }
