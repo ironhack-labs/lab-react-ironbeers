b4ef34329ac3d485b62e3ee4ee377cfb0c1d9d9d
Philipp Kastl <picastle@posteo.de>
2022-01-30 19:50:23 +0100

    ghpages test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
