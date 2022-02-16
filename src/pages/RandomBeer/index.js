d50c8e86a8292a8a7a7ff8ca3a3f2010446b49e5
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2022-02-16 15:29:43 -0600

    Get random beer done

--- src/pages/RandomBeer/index.js
+++ src/pages/RandomBeer/index.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+const axios = require("axios");
 
 const RandomBeer = () => {
-  return <div>RandomBeer</div>;
+  const [beerData, setBeerData] = useState([]);
+  const params = useParams();
+
+  const { id } = params;
+  useEffect(() => {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+      .then((response) => {
+        setBeerData(response.data);
+      })
+      .catch((err) => console.log("Error en fetching api on App.js", err));
+  }, []);
+  return (
+    <div className="single-beer-container">
+      <img
+        className="single-beer-img"
+        src={beerData.image_url}
+        alt={beerData.name}
+      />
+      <h2>{beerData.name}</h2>
+      <p>{beerData.tagline}</p>
+      <p>{beerData.first_brewed}</p>
+      <p>{beerData.attenuation_level}</p>
+      <p>{beerData.description}</p>
+      <p>{beerData.contributed_by}</p>
+    </div>
+  );
 };
 
 export default RandomBeer;
