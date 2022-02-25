66c431995e5a49a83d454f76f5a92d2604742d51
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-02-25 14:52:54 +0100

    done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, NavLink, useParams, useSearchParams } from "react-router-dom";
 import BeersPage from "./Beers";
+import imageAllBeers from "../assets/beers.png";
+import imagerandom from "../assets/random-beer.png";
+import imagenewbeer from "../assets/new-beer.png";
 
 function Home() {
   return (
@@ -9,20 +12,38 @@ function Home() {
       <h1>Home</h1>
       <Link to={"/beers"}>
         <div className="card">
-          <img src="./src/assets/beers.png" alt="beers image" />
+          <img src={imageAllBeers} alt="beers image" className="img" />
           <h3>All Beers</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            scelerisque tincidunt justo et ultrices. Duis non rhoncus augue, a
+            egestas arcu. Etiam sed cursus lacus. Integer id tortor at mi congue
+            elementum. Quisque ac mi consequat, tempor purus a, facilisis odio.
+          </p>
         </div>
       </Link>
       <Link to={"random-beer"}>
         <div className="card">
-          <img src="./src/assets/beers.png" alt="beers image" />
+          <img src={imagerandom} alt="beers image" className="img" />
           <h3>Random Beer</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            scelerisque tincidunt justo et ultrices. Duis non rhoncus augue, a
+            egestas arcu. Etiam sed cursus lacus. Integer id tortor at mi congue
+            elementum. Quisque ac mi consequat, tempor purus a, facilisis odio.
+          </p>
         </div>
       </Link>
       <Link to={"new-beer"}>
         <div className="card">
-          <img src="./src/assets/beers.png" alt="beers image" />
+          <img src={imagenewbeer} alt="beers image" className="img" />
           <h3>New Beer</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            scelerisque tincidunt justo et ultrices. Duis non rhoncus augue, a
+            egestas arcu. Etiam sed cursus lacus. Integer id tortor at mi congue
+            elementum. Quisque ac mi consequat, tempor purus a, facilisis odio.
+          </p>
         </div>
       </Link>
     </div>
