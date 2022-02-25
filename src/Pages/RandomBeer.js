7e8a6138d5d72b24312efd769cd4157886803212
Clara Villa <Clara.Villa@lego.com>
2022-02-25 16:02:54 +0000

    iteration 5

--- src/Pages/RandomBeer.js
+++ src/Pages/RandomBeer.js
@@ -1,7 +1,24 @@
 import Navbar from "../components/Navbar";
+import SingleBeerDetails from "../components/SingleBeerDetails";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function RandomBeer() {
-  return <Navbar />;
+  const url = "https://ih-beers-api2.herokuapp.com/beers/random";
+  const [randomBeer, setRandomBeer] = useState({});
+
+  useEffect(() => {
+    axios(url).then((data) => {
+      setRandomBeer(data.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <SingleBeerDetails beer={randomBeer} />
+    </div>
+  );
 }
 
 export default RandomBeer;
