347b8b85d675bf0772af673fb8f58190f4606a07
dorrrn <doan7tran@gmail.com>
2022-02-26 13:49:20 +0100

    query doesnt work properly

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 
 export default function Search(props) {
-    
+
   const [searchInput, setSearchInput] = useState("");
+  const [query, setQuery] = useSearchParams ()
  
   const handleInputChange = (event) => {
+      event.preventDefault();
       setSearchInput (event.target.value);
-      props.filterBeers(event.target.value)
+      props.filterBeers(event.target.value);
+      setQuery(event.target.value);
   }
 
   return (
