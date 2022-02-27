9be967a43d13c6cf320dc41a246fbd79cb9f6757
Zena Bos <zta.bos@gmail.com>
2022-02-27 12:46:24 +0100

    Iteration 5 done

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function RandomBeer() {
+  const [details, setDetails] = useState(undefined);
+
+  useEffect(() => {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+      .then((result) => {
+        setDetails(result.data);
+        console.log(result.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div>RandomBeer</div>
-  )
+    <div>
+      {details && (
+        <div>
+          <img src={details.image_url} alt={details.name} />
+          <h2>{details.name}</h2>
+          <p>{details.tagline}</p>
+          <p>{details.first_brewed}</p>
+          <p>{details.attenuation_level}</p>
+          <p>{details.description}</p>
+          <p>{details.contributed_by}</p>
+        </div>
+      )}
+    </div>
+  );
 }
