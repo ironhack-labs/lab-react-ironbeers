f25bc7336f92255851b3352c1c9bffb4e107c24e
David Vekony <dave.vekony@gmail.com>
2022-02-28 21:17:47 +0000

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import BeersListPage from "./pages/BeersListPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/beers" element={<BeersListPage />} />
       </Routes>
     </div>
   );
