199fca720caf8083bc46fe6410972e552813d1f8
David Vekony <dave.vekony@gmail.com>
2022-02-28 21:26:26 +0000

    iteration 5 done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import BeersListPage from "./pages/BeersListPage";
 import BeerDetailsPage from "./pages/BeerDetailsPage";
+import RandomBeerPage from "./pages/RandomBeerPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/beers" element={<BeersListPage />} />
         <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
+        <Route path="/random-beer" element={<RandomBeerPage />} />
       </Routes>
     </div>
   );
