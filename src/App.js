ec379a07064de1b76946a08f051a64e011942bcb
David Vekony <dave.vekony@gmail.com>
2022-02-28 21:23:45 +0000

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import BeersListPage from "./pages/BeersListPage";
+import BeerDetailsPage from "./pages/BeerDetailsPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/beers" element={<BeersListPage />} />
+        <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
       </Routes>
     </div>
   );
