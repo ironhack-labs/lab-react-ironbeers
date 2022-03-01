913a05a5a928d84ea4d3eaf03481d2311a01176e
Olayinka Oginni <olayinkaoginni2016@gmail.com>
2022-03-01 09:00:24 +0100

    iteration 5

--- src/pages/BeerDetails.js
+++ src/pages/BeerDetails.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import HomePage from './HomePage'
+import axios from 'axios'
 
-export default function BeerDetails(props) {
+export default function BeerDetails() {
   const params = useParams()
-  const beerId = params.id
+  const id = params.id
+  const [beer, setBeer] = useState([])
+  useEffect(() => {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/${id}`)
+      .then((response) => {
+        console.log(response.data)
+        setBeer(response.data)
+      })
+      .catch((err) => console.log(err))
+  }, [id])
 
-  return <h1>beer details</h1>
+  return (
+    <div
+      key={beer._id}
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}
+    >
+      <img src={beer.image_url} style={{ height: 100 }} alt='beer-pic' />
+      <div>
+        <h1>{beer.name}</h1>
+        <h3>{beer.tagline}</h3>
+        <h3>{beer.attenuation_level}</h3>
+        <h3>{beer.first_brewed}</h3>
+        <h3>{beer.description}</h3>
+        <h3>{beer.contributed_by}</h3>
+      </div>
+    </div>
+  )
 }
--- src/pages/RandomBeer.js
+++ src/pages/RandomBeer.js
@@ -1,5 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function RandomBeer() {
-  return <div>RandomBeer</div>
+  const [beer, setBeer] = useState([])
+  useEffect(() => {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+      .then((response) => {
+        console.log(response.data)
+        setBeer(response.data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  return (
+    <div
+      key={beer._id}
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}
+    >
+      <img src={beer.image_url} style={{ height: 100 }} alt='beer-pic' />
+      <div>
+        <h1>{beer.name}</h1>
+        <h3>{beer.tagline}</h3>
+        <h3>{beer.attenuation_level}</h3>
+        <h3>{beer.first_brewed}</h3>
+        <h3>{beer.description}</h3>
+        <h3>{beer.contributed_by}</h3>
+      </div>
+    </div>
+  )
 }
