4304abfe40a0ae84cf2d8220ae684f820c64ceb7
Vitor Afonso <vittokm@hotmail.com>
2022-03-25 11:31:36 +0000

    iteration 1 done

---
+++ src/components/HomePage.js
@@ -0,0 +1,42 @@
+//jshint esversion:9
+
+import { Link, NavLink } from "react-router-dom";
+import allBeers from './../assets/beers.png';
+import randomBeer from './../assets/random-beer.png';
+import newBeer from './../assets/new-beer.png';
+
+export const HomePage = () => {
+
+
+  return (
+    <div className="HomePage">
+
+        <ul>
+            <>
+                <img src={allBeers} alt="beers"/>
+                <Link to="/beers">
+                    <h2>All Beers</h2>
+                </Link>
+                <p>Donec sollicitudin molestie malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec sollicitudin molestie malesuada.</p>
+            </>
+
+            <>
+                <img src={randomBeer} alt="random beer"/>
+                <Link to="/random-beer">
+                    <h2>Random Beer</h2>
+                </Link> 
+                <p>Sed porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Nulla quis lorem ut libero malesuada feugiat.</p>
+            </>
+
+            <>
+                <img src={newBeer} alt="new beer"/>
+                <Link to="/new-beer">
+                    <h2>New Beer</h2>
+                </Link>
+                <p>Cras ultricies ligula sed magna dictum porta. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.</p>
+            </>
+        </ul>
+    </div>
+
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+//jshint esversion:9
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
