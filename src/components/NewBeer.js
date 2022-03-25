5250c9298c88816d92e9b4ade3d2c6cd0fae3143
Vitor Afonso <vittokm@hotmail.com>
2022-03-25 21:29:21 +0000

    iteration 6 done

--- src/components/NewBeer.js
+++ src/components/NewBeer.js
@@ -9,7 +9,44 @@ export const NewBeer = () => {
             <Link to="/"> <h3>Home</h3> </Link>
         </nav>
         
-        <h1>New Beer</h1>
+        <h1>Create New Beer</h1>
+
+        <form action="https://ih-beers-api2.herokuapp.com/beers/new" method="post">
+
+          <label>
+            <span><b>Name:</b> </span>
+            <input type="text" className='new-beer-input' id='name' name='name'/>
+          </label>
+          <label>
+            <span><b>Tagline:</b> </span>
+            <input type="text" className='new-beer-input' id='tagline' name='tagline'/>
+          </label>
+          <label>
+            <span><b>Description:</b> </span>
+            <textarea className='new-beer-input' id='description' name='description'/>
+          </label>
+          <label>
+            <span><b>First Brewed</b></span>
+            <input type="text" className='new-beer-input' id='first_brewed' name='first_brewed' />
+          </label>
+          <label>
+            <span><b>Brewers Tips</b></span>
+            <input type="text" className='new-beer-input' id='brewers_tips' name='brewers_tips' />
+          </label>
+          <label>
+            <span><b>Attenuation Level</b></span>
+            <input type="number" className='new-beer-input' id='attenuation_level' name='attenuation_level' />
+          </label>
+          <label>
+            <span><b>Contributed By</b></span>
+            <input type="text" className='new-beer-input' id='contributed_by' name='contributed_by' />
+          </label>
+
+          <button type='submit'>ADD NEW</button>
+
+        </form>
+
+
     </div>
   )
 }
