056dada5567b493cb0477e14c6236bebc0d48db7
joaonacho <joaonacho@gmail.com>
2022-03-28 17:42:15 +0100

    done

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import { useLocation } from "react-router-dom";
 function App() {
   const location = useLocation();
 
-  // console.log(location.pathname);
   return (
     <div className="App">
       {location.pathname !== "/" && (
