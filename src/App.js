8649f3bebe9e30cc8dd6e7f120a3fc950eacd428
Arturo Vivar <arthas1237@hotmail.com>
2022-03-30 13:32:00 +0200

    details doesnt work

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import './App.css';
 import HomePage from './components/HomePage/HomePage'
 import { Routes, Route } from 'react-router-dom'
+import React, { useEffect, useState, useCallback } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import BeerList from './components/BeerList/BeerList'
+import axios from 'axios';
+import BeerDetails from './components/BeerDetails/BeerDetails';
 
 // info para usar react-router-dom https://v5.reactrouter.com/web/guides/quick-start
 //No se si es buena practica pero he puesto Navbar fuera de routes de esta manera siempre sera visible, presupongo que
 //con el footer pasara igual
 
 function App() {
+
+
+ 
+  const [beers, setBeers] = useState(null)
+  
+  // seguidos los pasos de la clase anterior de Pablo
+
+  const fetchBeers = useCallback(() => {
+      return axios.get("https://ih-beers-api2.herokuapp.com/beers")
+  }, [])
+
+  useEffect(() => {
+      fetchBeers()
+      .then(response => setBeers(response.data))
+  }, [fetchBeers])
+
+  
+
+
+
   return (
     <div className="App">
       <Navbar/>
       <Routes>
         <Route path="/" element={ <HomePage/> }></Route>
-        <Route path="/beers" element={ <BeerList/> }></Route>
+        <Route path="/beers" element={ <BeerList beers={ beers }/> }></Route>
+        <Route patch="/beers/:beerId" element={ <BeerDetails beers={ beers }/> }></Route>
       </Routes>
-      
     </div>
   );
 }
--- src/components/BeerDetails/BeerDetails.js
+++ src/components/BeerDetails/BeerDetails.js
@@ -1,30 +1,23 @@
-import './BeerList.css'
-import React, { useEffect, useState } from 'react';
-import Loading from '../Loading/Loading';
-import { Link } from 'react-router-dom'
+import React from 'react'
+import './BeerDetails.css'
+import { useParams } from 'react-router-dom'
 
 
 
-const BeersList = (props) => {
 
-    const { beers } = props
-   
-    const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        if(beers) {
-            setLoading(false)
-        }
-    }, [beers])
-  
+function BeerDetails(props) {
 
-    return (
+    const { beerId } = useParams();
+
+    const beer = props.beer.find(beer => beer._id === beerId)
+
+    console.log(beer)
+
+    return(
         <>
         <div>
-            {loading ? (<p><Loading/></p>) : (
-                beers && beers.map(beer => (
                     <div className="wrapper">
-                    <div key={beer._id}>
                         <div className='product-img'>
                         <img src={beer.image_url} width="150" height="350" alt="beer" className="BeerImg"/>
                         </div>
@@ -35,23 +28,15 @@ const BeersList = (props) => {
                             <div className='text-description'>
                             <h6>{beer.description}</h6>
                             </div>
-                            <>
-                            <Link to={ `/beers/${beer._id}`}>{beer.name}</Link>
-                            </>
                             </div>
                             <p><strong>Created by: </strong>{beer.contributed_by}</p>
                         </div>
-                    </div>
                 </div>
-                ))
-            )}
+                
+           
         </div>
         </>
     )
 }
 
-
-
-export default BeersList;
-
-
+export default BeerDetails
