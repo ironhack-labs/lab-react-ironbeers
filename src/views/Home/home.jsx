c71f03a1161799da3e636d090af9505127a00524
Pablo Mateos <pmateos805@gmail.com>
2022-04-02 08:12:24 +0200

    Setup done

---
+++ src/views/Home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+        <div>
+            <div>
+                <img src="../../assets/beers.png" alt="All beers" />
+                <h2>All beers</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ex consequuntur corrupti tempore necessitatibus inventore est aspernatur unde. Dicta pariatur voluptate laborum iure, eum iste quis! Cumque, sit nostrum? Aspernatur!</p>
+            </div>
+        </div>
+    )
+}
+export default Home
