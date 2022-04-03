f23b3f5fb5bb0fcf3130e5e0c0907222fed7aaa0
“Giovanni” <giovannitakeshisesta@gmail.com>
2022-04-03 10:14:15 +0200

    refresh page with use locTIONS

--- src/views/RandomBeer/RandomBeer.js
+++ src/views/RandomBeer/RandomBeer.js
@@ -1,15 +1,20 @@
 import React,{ useState, useEffect } from 'react';
+import { useLocation } from 'react-router';
 
 import Itemview from '../../components/ItemView/ItemView';
 import {beerRandom} from './../../services/BeersService'
 
 export default function RandomBeer() {
     const [randomBeer, setRandomBeer] = useState();
-
+    const { key } = useLocation()
+    
     useEffect(() => {
         beerRandom()
-        .then(response => { setRandomBeer(response)})  
-    }, []);
+        .then(response => { 
+          setRandomBeer(response)
+        }) 
+    }, [key]);
+    //console.log(key)
     
   return (
     <div>
