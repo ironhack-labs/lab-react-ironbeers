24d7ce338553cafcdb7d4a494e3584758679f6dd
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-05-14 18:31:58 +0200

    solve iteration 5

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,12 +1,38 @@
 import { Link } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
+const baseUrl = process.env.REACT_APP_BASE_URL;
+
 export default function RandomBeer() {
+    const [randomBeer, setRandomBeer] = useState("");
+
+    useEffect(() => {
+        axios.get(baseUrl + "/random")
+            .then(response => {
+                setRandomBeer(response.data)
+            })
+            .catch(e => console.log("Error getting data from API: ", e));
+    }, [])
 
     return (
         <div>
             <Link to="/">HOME</Link>
-            <section>
-                <h1>This is the RandomBeer Component</h1>
-            </section>
+
+            {
+                randomBeer ?
+                <section>
+                    <img src={randomBeer.image_url} alt={randomBeer.name} />
+                    <div>
+                        <h2>Name:<br />{randomBeer.name}</h2>
+                        <h3>{randomBeer.tagline}</h3>
+                        <p>First brewed:<br />{randomBeer.first_brewed}</p>
+                        <p>Attenuation level:<br />{randomBeer.attenuation_level}</p>
+                        <p>Description:<br />{randomBeer.description}</p>
+                        <p>Contributor:<br />{randomBeer.contributed_by}</p>
+                    </div>
+                </section>
+                : <p>Loading...</p>
+            }
         </div>
     )
 }
