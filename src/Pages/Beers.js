0b58d6802df05b17b8ac9fb6614d4c397c550486
jidekickpush <jeandenisdrane@gmail.com>
2022-05-15 23:22:14 +0200

    Iteration 3 done : add axios, create a service folder to store a class with all methods to control the API

--- src/Pages/Beers.js
+++ src/Pages/Beers.js
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import beersService from "../services/beersService"
 import Header from "../components/Header"
 
-function Beers(){
+function Beers() {
+    const [beersArr, setBeersArr] = useState([])
 
-    return(
+    useEffect(() => {
+        loadBeers()
+    })
+
+    const loadBeers = () => {
+        beersService
+            .getAllBeers()
+            .then(({ data }) => {
+                setBeersArr(data)
+            })
+            .catch(e => console.log("error getting data from API", e))
+    }
+
+    return (
         <>
-            <Header/>
+            <Header />
+            {beersArr.map(beer => {
+                return (
+                    <div key={beer._id} >
+                        <img style={{ maxHeight: "12vh" }} src={beer.image_url} alt="" />
+                        <div style={{display: "inline"}}>
+                            <Link to={beer._id}><p>{beer.name}</p></Link>
+                            <p>{beer.tagline}</p>
+                            <p>Created by: {beer.contributed_by}</p>
+                        </div>
+                    </div>
+                )
+            })}
         </>
     )
 }
---
+++ src/services/beersService.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+class BeersService {
+
+    constructor() {
+        this.api = axios.create({ baseURL: 'https://ih-beers-api2.herokuapp.com' })
+    }
+
+    getAllBeers = () => {
+        return this.api.get('/beers')
+    }
+
+    getBeersWithQuery = query => {
+        return this.api.get(`beers/search?q=${query}`)
+    }
+
+    getOneBeer = id => {
+        return this.api.get(`/beers/${id}`)
+    }
+
+    getRandomBeer = () => {
+        return this.api.get(`/beers/random`)
+    }
+
+    saveBeer = beer => {
+        return this.api.post(`/beers/new`, beer)
+    }
+}
+
+const beersService = new BeersService()
+
+export default beersService
