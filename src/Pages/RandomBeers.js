4f377500007de130b075bba98397df9c36a430d9
jidekickpush <jeandenisdrane@gmail.com>
2022-05-16 00:19:44 +0200

    Iteration 5 done

--- src/Pages/RandomBeers.js
+++ src/Pages/RandomBeers.js
@@ -1,9 +1,39 @@
-import Header from "../components/Header"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import beersService from "../services/beersService";
 
-function RandomBeer(){
-    return(
+function RandomBeer() {
+    const [details, setDetails] = useState({});
+
+    const { beerId } = useParams();
+
+    useEffect(() => {
+        loadBeer()
+    },[])
+
+    const loadBeer = () => {
+        beersService
+            .getRandomBeer()
+            .then(({ data }) => {
+                setDetails(data)
+            })
+            .catch(e => console.log("error getting data from API", e))
+    }
+
+    return (
         <>
             <Header />
+            <div>
+                <img src={details.image_url} alt="" style={{ maxHeight: "20vh" }} />
+                <p>{details.name} </p>
+                <p>{details.attenuation_level} </p>
+                <p>{details.tagline}</p>
+                <p>{details.first_brewed}</p>
+                <p>{details.description}</p>
+                <p>{details.contributed_by}</p>
+            </div>
+
         </>
     )
 }
