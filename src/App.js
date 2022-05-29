79acb226c53f3ae06f723f3231289bc9eea3f1f5
unknown <cesyrub@gmail.com>
2022-05-29 13:56:50 +0200

    remove navbar from home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Beers from './pages/Beers';
 import RandomBeer from './pages/RandomBeer';
 import NewBeer from './pages/NewBeer';
@@ -9,10 +9,12 @@ import Error from './pages/Error'
 import NotFound from './pages/NotFound'
 import OneBeer from './pages/OneBeer';
 
+
 function App() {
+  const location = useLocation().pathname;
   return (
     <div className="App">
-      <Navbar />
+      {location !== "/" && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/beers" element={<Beers />} />
