e24580642814bd43b52928ae6c2c846614f4a111
WHL99 <xjv298347@gmail.com>
2022-07-16 23:36:47 +0200

    mobile first homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,26 +8,25 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
     return (
-        <div>
-            <div>
+        <div className="wrap">
+            <div className="item">
                 <img src={beersImg} alt="page-img" className="page-img" />
-
                 <h1> <Link to='/allbeers'>All Beers</Link></h1>
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <span className="home-page-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </span>
             </div>
 
-            <div>
+            <div className="item" >
                 <img src={ranImg} alt="page-img" className="page-img" />
                 <h1> <Link to={'/random'}>Random Beer</Link></h1>
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <span >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempore magna aliqua. Ut enim ad minr in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </span>
             </div>
 
-            <div>
+            <div className="item">
                 <img src={newImg} alt="page-img" className="page-img" />
                 <h1> <Link to={'/addnewbeer'}>New Beer</Link></h1>
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <span className="home-page-text">Lorem ipsum dolor sit amet, consectetur a do eiusmod tempor incididunt ut labore et dolore ma ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officiunt mollit anim id est laborum.
                 </span>
             </div>
 
