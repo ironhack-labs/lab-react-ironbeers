6ff4efcf663d9a794701792211dbeca13e7b13bc
WHL99 <xjv298347@gmail.com>
2022-07-17 16:29:49 +0200

    mobile first add new beer

--- src/pages/NewBeer.js
+++ src/pages/NewBeer.js
@@ -16,22 +16,11 @@ export default function NewBeer() {
     const [contributedBy, setContributedBy] = useState('')
     const navigate = useNavigate()
 
-
-    //     name - must be type text
-    // tagline - must be type text
-    // description - must be type text
-    // first_brewed - must be type text
-    // brewers_tips - must be type text
-    // attenuation_level - must be type number !!!
-    // contributed_by - must be type text
-
-
-
     function handleSubmit(e) {
 
         e.preventDefault()
 
-        const body = { name: beerName, tagline: tagline, description: description, first_brewed: firstBrewed,brewers_tips: brewersTips,attenuation_level:attenuationLevel,contributed_by:contributedBy}
+        const body = { name: beerName, tagline: tagline, description: description, first_brewed: firstBrewed, brewers_tips: brewersTips, attenuation_level: attenuationLevel, contributed_by: contributedBy }
 
         axios
             .post(`https://ih-beers-api2.herokuapp.com/beers/new`, body)
@@ -47,39 +36,49 @@ export default function NewBeer() {
             })
     }
 
-
-
-
     return (
         <div>
             <Header />
-            <form onSubmit={handleSubmit}>
-                <label>Beer Name:</label>
-                <input type='text' name='beerName' onChange={(e) => setBeerName(e.target.value)} value={beerName} />
+            <form onSubmit={handleSubmit} className="form-container-new-beer">
+                <div className="label-to-left">
+                    <label>Beer Name:</label>
+                    <input type='text' name='beerName' onChange={(e) => setBeerName(e.target.value)} value={beerName} />
 
-                <label>Tagline:</label>
-                <input type='text' name='tagline' onChange={(e) => setTagline(e.target.value)} value={tagline} />
+                </div>
 
-                <label>Description:</label>
-                <input type='text' name='description' onChange={(e) => setDescription(e.target.value)} value={description} />
+                <div className="label-to-left">
+                    <label>Tagline:</label>
+                    <input type='text' name='tagline' onChange={(e) => setTagline(e.target.value)} value={tagline} />
+                </div>
+               
+                <div className="label-to-left">
+                    <label>Description:</label>
+                    <input type='long-text' name='description' onChange={(e) => setDescription(e.target.value)} value={description} />
+                </div>
 
-                <label>First Brewed:</label>
-                <input type='text' name='firstBrewed' onChange={(e) => setFirstBrewed(e.target.value)} value={firstBrewed} />
+                <div className="label-to-left">
+                    <label>First Brewed:</label>
+                    <input type='text' name='firstBrewed' onChange={(e) => setFirstBrewed(e.target.value)} value={firstBrewed} />
+                </div>
 
-                <label>Brewers Tips:</label>
-                <input type='text' name='brewersTips' onChange={(e) => setBrewersTips ( e.target.value )} value={brewersTips} />
-                   
-                <label>attenuationLevel:</label>
-                <input type='number' name='attenuationLevel' onChange={(e) => setAttenuationLevel ( e.target.value )} value={attenuationLevel} />
-                   
-                <label>Contributed By:</label>
-                <input type='text' name='contributedBy' onChange={(e) => setContributedBy ( e.target.value )} value={contributedBy} />
-                   
+                <div className="label-to-left">
+                    <label>Brewers Tips:</label>
+                    <input type='text' name='brewersTips' onChange={(e) => setBrewersTips(e.target.value)} value={brewersTips} />
+                </div>
+
+                <div className="label-to-left">
+                    <label>Attenuation Level:</label>
+                    <input type='number' name='attenuationLevel' onChange={(e) => setAttenuationLevel(e.target.value)} value={attenuationLevel} />
+                </div>
+                < div className="label-to-left">
+                    <label>Contributed By:</label>
+                    <input type='text' name='contributedBy' onChange={(e) => setContributedBy(e.target.value)} value={contributedBy} />
+                </div>
                 <button type='submit'> Add the Beer</button>
 
-            </form>
+            </form >
 
 
-        </div>
+        </div >
     )
 }
