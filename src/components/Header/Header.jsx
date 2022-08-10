3afe86e99ace2b71807d705bce974ed4bac1f327
ana luz martinez <almadigital.mx@gmail.com>
2022-08-09 22:46:58 -0700

    iteration 1 and 2, no stylings yet

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+function Header() {
+    return(
+        <header className="header-container">
+            <nav className="home-logo"><a href="/">🏠</a></nav>
+            </header>
+
+    );
+}
+
+export default Header
---
+++ src/components/HomeCard/index.js
@@ -0,0 +1,1 @@
+export {default as HomeCard} from "./HomeCard"
