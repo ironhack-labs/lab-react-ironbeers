309ab652893431f967145a59d86d0522d83cb57d
Matulan Mahenthra <Matu_25@hotmail.com>
2022-08-22 08:16:59 +0100

    FTLI072022

--- src/Components/RandomBeer.jsx
+++ src/Components/RandomBeer.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, NavLink } from 'react-router-dom';
+import hme from '../assets/hme.png'
+
 
 function RandomBeer() {
     const [randomBeer, setRandomBeer] = useState("")
@@ -22,10 +24,9 @@ function RandomBeer() {
 
   return (
     <>
-
         <Link to={`/`}>
             <header>
-                <img src="https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png" alt="return-home" className='return-home' />
+                <img src={hme} alt="return-home" />
             </header>
         </Link>
     <div>
