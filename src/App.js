f914e4141c283d94235156efaac2ea004c83203e
unknown <Jossuha@hotmail.fr>
2022-09-13 00:34:49 +0200

    Debut de style scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import './App.css';
+import './App.scss';
 import {Routes,Route} from 'react-router-dom'
 import Home from "./pages/Home"
-import RandomBeer from './pages/RandomBeer';
 import NewBeer from './pages/NewBeer';
 import Beers from './pages/Beers'
 import BeerDetails from './pages/BeerDetails'
@@ -13,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/beers" element={<Beers/>}></Route>
-        <Route path="/beers/:beerId" element={<BeerDetails/>}></Route>
-        <Route path="/random-beer" element={<RandomBeer/>}></Route>
+        <Route path="/beers/:beerId" element={<BeerDetails request="detail"/>}></Route>
+        <Route path="/random-beer" element={<BeerDetails request="random"/>}></Route>
         <Route path="/new-beer" element={<NewBeer/>}></Route>
       </Routes>
     </div>
--- src/pages/BeerDetails.js
+++ src/pages/BeerDetails.js
@@ -3,35 +3,45 @@ import { useState, useEffect } from "react"
 import axios from 'axios'
 
 
-export default function BeerDetails() {
-    const {beerId} = useParams()
+export default function BeerDetails(props) {
     const [beer,setBeer]=useState({image_url:""})
+    const {beerId} = useParams()
+
     useEffect(()=>{
-        axios.get(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`)
-        .then((response)=>{
-            console.log('response.data:',response.data)
-            setBeer(response.data)})
-        .catch((err)=>console.log('error getting beerDetails', err))
-    },[beerId])
+        if (props.request === "detail"){ // Page de detail de bierre
+            axios.get(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`)
+            .then((response)=>{
+                console.log('response.data:',response.data)
+                setBeer(response.data)})
+            .catch((err)=>console.log('error getting beerDetails', err))
+
+        }else if (props.request === "random"){ //Page de bierre random
+            axios.get('https://ih-beers-api2.herokuapp.com/beers/random')
+            .then((response)=>{
+                console.log("response:",response)
+                setBeer(response.data)
+            })
+            .catch(err=>console.log('error getting random beer',err))
+        }
+    },[beerId, props.request])
 
     return (
-        <div className="BeerDetails">
-            <NavLink to="/"><img src={"https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"} alt="home" /></NavLink>
+        <div className="beerDetails">
+            <NavLink to="/"><img className="returnToHome" src={"https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"} alt="home" /></NavLink>
             
-            <img src={beer.image_url} alt="beer"/>
-            <div >
+            <img className="beerImage" src={beer.image_url} alt="beer"/>
+            <div className="beerGrid">
                 <div>
                     <h2>{beer.name}</h2>
                     <h3>{beer.tagline}</h3>
                 </div>
                 <div>
-                    <p className="attenuation">{beer.attenuation_level}</p>
-                    <p className="firstBrewed">{beer.first_brewed}</p>
-                </div>
-                
+                    <p>{beer.attenuation_level}</p>
+                    <p>{beer.first_brewed}</p>
+                </div> 
             </div>
-            <p>{beer.description}</p>
-            <p>{beer.contributed_by}</p>
+            <p class="beerDescription">{beer.description}</p>
+            <p class="beerAuthor">{beer.contributed_by}</p>
         </div>
     )
 }
