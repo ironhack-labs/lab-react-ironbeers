ab5b5256366a316256a877ffaf9be5928bfe3117
Alan ROGER <aa.roger404@gmail.com>
2022-09-12 21:55:41 +0200

    iteration 6 done

--- src/pages/NewBeer.js
+++ src/pages/NewBeer.js
@@ -1,9 +1,59 @@
-import {NavLink} from "react-router-dom"
+import { NavLink,useNavigate } from "react-router-dom"
+import { useState } from 'react'
+import axios from 'axios'
 
 export default function NewBeer() {
-    return(
-        <div className="NewBeer">
-            <NavLink to="/"><img src={"https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"} alt="home"/></NavLink>
-        </div>
-    )
-}
+    const [name, setName] = useState('')
+    const [tagline, setTagline] = useState('')
+    const [description, setDescription] = useState('')
+    const [firstBrewed, setFirstBrewed] = useState('')
+    const [brewerTips, setBrewerTips] = useState('')
+    const [attenuation, setAttenuation] = useState('')
+    const [creator, setCreator] = useState('')
+    const navigate = useNavigate();
+    function coucou(e) {
+        e.preventDefault()
+        const body = { name, tagline, description, first_brewed: firstBrewed, brewer_tips: brewerTips, attenuation_level: attenuation, contributed_by: creator }
+        axios.post("https://ih-beers-api2.herokuapp.com/beers/new", body)
+            .then((response) => {
+                console.log("response", response)
+                setName('')
+                setTagline('')
+                setDescription('')
+                setFirstBrewed('')
+                setBrewerTips('')
+                setAttenuation('')
+                setCreator('')
+                navigate('/beers')
+            })
+        }
+        return (
+            <div className="NewBeer">
+                <NavLink to="/"><img src={"https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"} alt="home" /></NavLink>
+                <form onSubmit={coucou}>
+                    <label>
+                        Name:<input type="text" name="name" value={name} onChange={(event) => { setName(event.target.value) }} />
+                    </label>
+                    <label>
+                        Tagline:<input type="text" name="tagline" value={tagline} onChange={(event) => { setTagline(event.target.value) }} />
+                    </label>
+                    <label>
+                        Description:<input type="text" name="description" value={description} onChange={(event) => { setDescription(event.target.value) }} />
+                    </label>
+                    <label>
+                        First Brewed:<input type="text" name="first_brewed" value={firstBrewed} onChange={(event) => { setFirstBrewed(event.target.value) }} />
+                    </label>
+                    <label>
+                        Brewer Tips:<input type="text" name="brewer_tips" value={brewerTips} onChange={(event) => { setBrewerTips(event.target.value) }} />
+                    </label>
+                    <label>
+                        Attenuation Level<input type="number" name="attenuation_level" value={attenuation} onChange={(event) => { setAttenuation(event.target.valueAsNumber) }} />
+                    </label>
+                    <label>
+                        Created by:<input type="text" name="contributed_by" value={creator} onChange={(event) => { setCreator(event.target.value) }} />
+                    </label>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
--- src/pages/RandomBeer.js
+++ src/pages/RandomBeer.js
@@ -1,23 +1,23 @@
-import {NavLink} from "react-router-dom"
-import {useState,useEffect} from 'react'
+import { NavLink } from "react-router-dom"
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default function RandomBeer() {
-    const [randomBeer,setRandomBeer] = useState()
+    const [randomBeer, setRandomBeer] = useState({})
 
-    useEffect(()=>{
+    useEffect(() => {
         axios.get('https://ih-beers-api2.herokuapp.com/beers/random')
-        .then((response)=>{
-            console.log("response:",response)
-            setRandomBeer(response.data)
-        })
-        .catch(err=>console.log('error getting random beer',err))
-    },[randomBeer])
+            .then((response) => {
+                console.log("response:", response)
+                setRandomBeer(response.data)
+            })
+            .catch(err => console.log('error getting random beer', err))
+    }, [])
 
-    return(
+    return (
         <div className="RandomBeer">
-            <NavLink to="/"><img src={"https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"} alt="home"/></NavLink>
-            <img src={randomBeer.image_url} alt="beer"/>
+            <NavLink to="/"><img src={"https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"} alt="home" /></NavLink>
+            <img src={randomBeer.image_url} alt="beer" />
             <div >
                 <div>
                     <h2>{randomBeer.name}</h2>
@@ -27,7 +27,7 @@ export default function RandomBeer() {
                     <p className="attenuation">{randomBeer.attenuation_level}</p>
                     <p className="firstBrewed">{randomBeer.first_brewed}</p>
                 </div>
-                
+
             </div>
             <p>{randomBeer.description}</p>
             <p>{randomBeer.contributed_by}</p>
