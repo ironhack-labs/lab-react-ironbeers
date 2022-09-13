2a8607104b6c52f5f24278038bb0a70bc4dbdb4e
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-09-13 21:35:36 +0200

    lab started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router";
+import HomeScreen from "./screens/home-screen/HomeScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Routes >
+      <Route path="/" element={<HomeScreen />}></Route>
+    </Routes >
     </div>
   );
 }
---
+++ src/screens/home-screen/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeScreen() {
+  return (
+    <div>HomeScreen</div>
+  )
+}
+
+export default HomeScreen
