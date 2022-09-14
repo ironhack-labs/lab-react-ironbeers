74ee3e1582de6cbd0798b1024053d4713d6592c4
Shania <shaniaantezana@gmail.com>
2022-09-13 22:48:20 -0400

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Routes,Route} from "react-router-dom";
 import { HomePage } from './pages/HomePage';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
+    { window.location.pathname !== '/' ? <Header/> : null}
     <Routes>
       <Route path="/" element={<HomePage/>} />
     </Routes>
