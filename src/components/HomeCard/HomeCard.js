479146c0d597b79d9ee31d884e621153b588fc1b
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-09-14 17:04:29 +0200

    feat: iteration 1 done

---
+++ src/components/HomeCard/HomeCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function HomeCard({img, title, url}) {
+  return (
+    <>
+      <Link to={url} className="text-decoration-none">
+        <div className="card mb-3 text-dark">
+          <img src={img} className="card-img-top" alt={title} />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias obcaecati, sed nobis repellat ea libero a dolorum magni id beatae dolor enim, incidunt exercitationem blanditiis neque quam! Molestiae, eum neque.</p>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+}
+
+
+export default HomeCard;
---
+++ src/screens/home/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HomeCard from "../../components/HomeCard/HomeCard";
+import beers from "../../assets/beers.png";
+import randomBeer from "../../assets/random-beer.png";
+import newBeer from "../../assets/new-beer.png";
+
+function HomePage() {
+  return (
+    <div className="d-flex flex-column">
+      <HomeCard img={beers} title="All Beers" url="/beers" />
+      <HomeCard img={randomBeer} title="Random Beer" url="/random-beer" />
+      <HomeCard img={newBeer} title="New Beer" url="/new-beer" />
+    </div>
+  );
+}
+
+export default HomePage;
