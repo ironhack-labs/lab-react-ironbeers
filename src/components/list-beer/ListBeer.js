104d2f8b8ffc10eb1dc78c932805df638f6f6a04
CrisArranz <c.arranz.arevalo@gmail.com>
2022-09-14 17:49:03 +0200

    Iteration 7

--- src/components/list-beer/ListBeer.js
+++ src/components/list-beer/ListBeer.js
@@ -11,19 +11,30 @@ import './ListBeer.css'
 
 function ListBeer() {
 
-  const [beers, setBeers] = useState(null);
+  const [ beers, setBeers ] = useState(null);
+
+  const getBeers = (value) => axios.get(`https://ih-beers-api2.herokuapp.com/beers${value?.length ? `/search?q=${value}` : ''}`)
+    .then(res => setBeers(res.data))
+    .catch(error => console.error(error));
 
   useEffect(() => {
-    axios.get('https://ih-beers-api2.herokuapp.com/beers')
-      .then(res => setBeers(res.data))
-      .catch(error => console.error(error))
+    getBeers()
   }, [])
+
+  const handleChange = event => {
+    const { value } = event.target;
+    getBeers(value);
+  }
 
   return (
     <div className="list-beer">
       <Link to={'/'}>
         <img className="list-beer__header" src={HomeBanner} alt="Home" />
       </Link>
+      <div class="input-group mb-3 d-flex mt-2 list-beer__search">
+        <span class="input-group-text"><i class="fa fa-search fa-fw"></i></span>
+        <input type="text" class="form-control" onChange={handleChange} />
+      </div>
       {
         beers ? 
         beers.map(beer => 
