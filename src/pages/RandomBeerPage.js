106f1952685806b6e3197c2a8c354f0f231d9598
Shania <shaniaantezana@gmail.com>
2022-09-15 08:32:06 -0400

    iteration 5 done

--- src/pages/RandomBeerPage.js
+++ src/pages/RandomBeerPage.js
@@ -1,20 +1,30 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-
 export const RandomBeerPage = () => {
- 
+  const [randomBeer,setRandomBeer] = useState({})
+  const getRandomBeer = async()=>{
+    try{
+      let {data} = await axios.get('https://ih-beers-api2.herokuapp.com/beers/random')
+      setRandomBeer(data)
+    }catch(error){
+       return error
+    }
+  }
+  useEffect(()=>{
+    getRandomBeer()
+  },[])
   return (
-    <div>RandomBeerPage</div>
-    /*<div style={{ marginTop: 40 }}>
-    <div><img src={randomBeer[0].image_url} alt=""/></div>
-      <div>{randomBeer[0].name}</div>
-      <div>{randomBeer[0].tagline}</div>
-      <div>{randomBeer[0].first_brewed}</div>
-      <div>{randomBeer[0].attenuation_level}</div>
-      <div>{randomBeer[0].description}</div>
-      <div>{randomBeer[0].contributed_by}</div>
+    <div>RandomBeerPage
+    <div style={{ marginTop: 40 }}>
+    <div><img src={randomBeer.image_url} alt=""/></div>
+      <div>{randomBeer.name}</div>
+      <div>{randomBeer.tagline}</div>
+      <div>{randomBeer.first_brewed}</div>
+      <div>{randomBeer.attenuation_level}</div>
+      <div>{randomBeer.description}</div>
+      <div>{randomBeer.contributed_by}</div>
     </div>
-    </div>*/
+    </div>
   )
 }
