97b01d1ae0db4607182fcc7165d7e5200a8a61f8
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-09-15 16:24:31 +0200

    updated text

--- src/screens/home-screen/HomeScreen.js
+++ src/screens/home-screen/HomeScreen.js
@@ -7,7 +7,7 @@ import MainCard from '../../components/main-card/MainCard'
 function HomeScreen() {
   return (
     <div className='container my-1 d-flex flex-column gap-2'>
-      <MainCard image={beers} title={"All beers"} text={"Get a list of all beers in our database"} to={"/all-beers"} />
+      <MainCard image={beers} title={"All beers"} text={"Get a list of all beers that we have in our database"} to={"/all-beers"} />
       <MainCard image={randomBeer} title={"Radnom beer"} text={"Get a random beer from our database"} to={"/random-beer"} />
       <MainCard image={newBeer} title={"New beer"} text={"Create a new beer by yourself"} to={"/new-beer"} />
     </div>
