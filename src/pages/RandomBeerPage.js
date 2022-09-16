35348b8459d4ccc4e886df4eb9911e685300ac17
Shania <shaniaantezana@gmail.com>
2022-09-16 00:45:55 -0400

    style in RandomBeerPage.js

--- src/pages/RandomBeerPage.js
+++ src/pages/RandomBeerPage.js
@@ -1,30 +1,44 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export const RandomBeerPage = () => {
-  const [randomBeer,setRandomBeer] = useState({})
-  const getRandomBeer = async()=>{
-    try{
-      let {data} = await axios.get('https://ih-beers-api2.herokuapp.com/beers/random')
-      setRandomBeer(data)
-    }catch(error){
-       return error
+  const [randomBeer, setRandomBeer] = useState({});
+  const getRandomBeer = async () => {
+    try {
+      let { data } = await axios.get(
+        "https://ih-beers-api2.herokuapp.com/beers/random"
+      );
+      setRandomBeer(data);
+    } catch (error) {
+      return error;
     }
-  }
-  useEffect(()=>{
-    getRandomBeer()
-  },[])
+  };
+  useEffect(() => {
+    getRandomBeer();
+  }, []);
   return (
-    <div>RandomBeerPage
-    <div style={{ marginTop: 40 }}>
-    <div><img src={randomBeer.image_url} alt=""/></div>
-      <div>{randomBeer.name}</div>
-      <div>{randomBeer.tagline}</div>
-      <div>{randomBeer.first_brewed}</div>
-      <div>{randomBeer.attenuation_level}</div>
-      <div>{randomBeer.description}</div>
-      <div>{randomBeer.contributed_by}</div>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+    <div class="card" style={{ margin: 40, width: "40%" }}>
+      <div class="card-body">
+      <div style={{display:'flex',justifyContent:'center'}} >
+        <img
+          style={{ width: 80, height: 300 }}
+          src={randomBeer.image_url}
+          alt="imageBeer"
+        />
+        </div>
+        <div style={{display:'flex',justifyContent:'space-between'}} >
+          <p style={{fontSize:40}} >{randomBeer.name}</p>
+          <p style={{fontSize:40,color:'#D3D3D3'}} >{randomBeer.attenuation_level}</p>
+        </div>
+        <div style={{display:'flex',justifyContent:'space-between'}} >
+          <p style={{fontSize:25,color:'#9C9C9C'}}>{randomBeer.tagline}</p>
+          <p style={{fontWeight:'bolder',fontSize:20}} >{randomBeer.first_brewed}</p>
+        </div>
+        <div style={{fontWeight:500,fontSize:20}} >{randomBeer.description}</div>
+        <div style={{color:'#959595',fontWeight:'bolder'}} >{randomBeer.contributed_by}</div>
+      </div>
     </div>
-    </div>
-  )
-}
+  </div>
+  );
+};
