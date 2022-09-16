a3335a8cbc4af41f9e469239b12dd020655fe5b8
Shania <shaniaantezana@gmail.com>
2022-09-16 00:35:04 -0400

    style in  BeerPage.js

--- src/pages/BeerPage.js
+++ src/pages/BeerPage.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 export const BeerPage = (props) => {
   const { id } = useParams();
   const [beer, setBeer] = useState({});
-  const [load,setLoad] = useState(true);
+  const [load, setLoad] = useState(true);
   const getBeerProfile = () => {
     let beerProfile = props.beers.filter((beerProfile) => {
       return beerProfile._id === id;
@@ -16,14 +16,28 @@ export const BeerPage = (props) => {
     getBeerProfile();
   }, []);
   return !load ? (
-    <div style={{ marginTop: 40 }}>
-    <div><img src={beer[0].image_url} alt=""/></div>
-      <div>{beer[0].name}</div>
-      <div>{beer[0].tagline}</div>
-      <div>{beer[0].first_brewed}</div>
-      <div>{beer[0].attenuation_level}</div>
-      <div>{beer[0].description}</div>
-      <div>{beer[0].contributed_by}</div>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div class="card" style={{ margin: 40, width: "40%" }}>
+        <div class="card-body">
+        <div style={{display:'flex',justifyContent:'center'}} >
+          <img
+            style={{ width: 80, height: 300 }}
+            src={beer[0].image_url}
+            alt="imageBeer"
+          />
+          </div>
+          <div style={{display:'flex',justifyContent:'space-between'}} >
+            <p style={{fontSize:40}} >{beer[0].name}</p>
+            <p style={{fontSize:40,color:'#D3D3D3'}} >{beer[0].attenuation_level}</p>
+          </div>
+          <div style={{display:'flex',justifyContent:'space-between'}} >
+            <p style={{fontSize:25,color:'#9C9C9C'}}>{beer[0].tagline}</p>
+            <p style={{fontWeight:'bolder',fontSize:20}} >{beer[0].first_brewed}</p>
+          </div>
+          <div style={{fontWeight:500,fontSize:20}} >{beer[0].description}</div>
+          <div style={{color:'#959595',fontWeight:'bolder'}} >{beer[0].contributed_by}</div>
+        </div>
+      </div>
     </div>
   ) : (
     <div>Load... </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,23 @@
-import {Link} from 'react-router-dom';
-import { Beers } from '../components/Beers';
-import { NewBeer } from '../components/NewBeer';
-import { RandomBeers } from '../components/RandomBeers';
-
+import { Link } from "react-router-dom";
+import { Beers } from "../components/Beers";
+import { NewBeer } from "../components/NewBeer";
+import { RandomBeers } from "../components/RandomBeers";
 
 export const HomePage = () => {
   return (
-    <div style={{width:'90%', marginLeft:60}}>
-    <Link to="/beers" style={{textDecoration:'none',color:'black'}} ><Beers/></Link>
-    <Link to="/random-beer"  style={{textDecoration:'none',color:'black'}} ><RandomBeers/></Link>
-    <Link  to="/new-beer" style={{textDecoration:'none',color:'black'}}><NewBeer/></Link>
+    <div style={{ width: "90%", marginLeft: 60 }}>
+      <Link to="/beers" style={{ textDecoration: "none", color: "black" }}>
+        <Beers />
+      </Link>
+      <Link
+        to="/random-beer"
+        style={{ textDecoration: "none", color: "black" }}
+      >
+        <RandomBeers />
+      </Link>
+      <Link to="/new-beer" style={{ textDecoration: "none", color: "black" }}>
+        <NewBeer />
+      </Link>
     </div>
-  )
-}
+  );
+};
