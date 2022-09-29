5247cbb33f0a151ed237eee73f99c1757e0eea91
Marina Laporte Boni <marinaolboni@gmail.com>
2022-09-29 16:17:07 +0200

    delete strictmode

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
 );
 
 reportWebVitals();
