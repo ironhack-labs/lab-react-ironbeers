c3cfca8154f584fb0a5a37325e2b7344ac8050df
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-10-01 16:35:03 +0200

    finish iteration 3

--- src/pages/AllBeers.jsx
+++ src/pages/AllBeers.jsx
@@ -1,12 +1,61 @@
 import Header from "../components/Header";
+import axios from "axios";
 
 import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function AllBeers() {
+  const [beers, setBeers] = useState([]);
+  const API_URL = "https://ih-beers-api2.herokuapp.com/beers";
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}`)
+      .then((response) => {
+        setBeers(response.data);
+      })
+      .catch((err) => console.log("err:", err));
+  }, []);
+
   return (
     <div>
       <Header />
-      <h1>All Beers</h1>
+      <div className="container">
+        {beers.map((beer) => {
+          return (
+            <div
+              class="card mb-3 py-3 mx-auto"
+              style={{
+                maxWidth: "540px",
+              }}
+            >
+              <div className="row">
+                <div className="col-sm-4">
+                  <img src={beer.image_url} alt="img_url" height="150px" />
+                </div>
+                <div className="col-sm-8 text-start">
+                  <div className="card-body">
+                    <h2 className="card-title mt-2 mb-3">
+                      <Link
+                        to={`/beers/${beer._id}`}
+                        style={{ textDecoration: "none", color: "black" }}
+                      >
+                        {beer.name}
+                      </Link>
+                    </h2>
+                    <h5 className="card-text text-muted">{beer.tagline}</h5>
+                    <p style={{ fontSize: "0.8rem" }}>
+                      <b>Created by: </b>
+                      {beer.name}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,12 @@ function Home() {
             <img src={beersImg} alt="beers" />
             <div className="card-body mx-1">
               <h5 className="card-title">
-                <Link to="/beers">All Beers</Link>
+                <Link
+                  to="/beers"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  All Beers
+                </Link>
               </h5>
               <p className="card-text text-start">
                 <small class="text-muted">
@@ -30,7 +35,12 @@ function Home() {
             <img src={randomBeerImg} alt="beers" />
             <div className="card-body mx-1">
               <h5 className="card-title">
-                <Link to="/random-beer">Random Beer</Link>
+                <Link
+                  to="/random-beer"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Random Beer
+                </Link>
               </h5>
               <p className="card-text text-start">
                 <small class="text-muted">
@@ -47,7 +57,12 @@ function Home() {
             <img src={newBeerImg} alt="beers" />
             <div className="card-body mx-1">
               <h5 className="card-title">
-                <Link to="/new-beer">New Beer</Link>
+                <Link
+                  to="/new-beer"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  New Beer
+                </Link>
               </h5>
               <p className="card-text text-start">
                 <small class="text-muted">
