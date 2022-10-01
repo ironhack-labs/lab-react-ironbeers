f572c5b284153cb3d1c0a8cfeedfd92fcfe163ed
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-10-01 15:21:09 +0200

    finish iteration 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Home.jsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+
+import beersImg from "../assets/beers.png";
+import randomBeerImg from "../assets/random-beer.png";
+import newBeerImg from "../assets/new-beer.png";
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="row my-3">
+        <div className="col-md-4">
+          <div className="card">
+            <img src={beersImg} alt="beers" />
+            <div className="card-body mx-1">
+              <h5 className="card-title">
+                <Link to="/beers">All Beers</Link>
+              </h5>
+              <p className="card-text text-start">
+                <small class="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna.
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card">
+            <img src={randomBeerImg} alt="beers" />
+            <div className="card-body mx-1">
+              <h5 className="card-title">
+                <Link to="/random-beer">Random Beer</Link>
+              </h5>
+              <p className="card-text text-start">
+                <small class="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna.
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card">
+            <img src={newBeerImg} alt="beers" />
+            <div className="card-body mx-1">
+              <h5 className="card-title">
+                <Link to="/new-beer">New Beer</Link>
+              </h5>
+              <p className="card-text text-start">
+                <small class="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna.
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
