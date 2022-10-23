ca4b6d9109d78f4d99e0bbf5596d689d753e6505
berkayyildirim <huseyinberkayyildirim@gmail.com>
2022-10-23 16:33:27 +0300

    implement listing all the beers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import axios from "axios";
+
 import "./App.css";
 import HomePage from "./components/HomePage";
 import ListBeers from "./components/ListBeers";
 import RandomBeer from "./components/RandomBeer";
 import NewBeer from "./components/NewBeer";
+import { useEffect } from "react";
+import { useState } from "react";
 
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const [beersArray, setBeersArray] = useState([]);
+
+  useEffect(() => {
+    fetchBeers();
+  }, []);
+
+  const fetchBeers = () => {
+    axios
+      .get(process.env.REACT_APP_API_URL + "/beers")
+      .then((response) => {
+        setBeersArray(response.data);
+      })
+      .catch((e) => console.log("error getting beers from API", e));
+  };
+
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/beers' element={<ListBeers />} />
+        <Route path='/beers' element={<ListBeers beers={beersArray} />} />
         <Route path='/random-beer' element={<RandomBeer />} />
         <Route path='/new-beer' element={<NewBeer />} />
       </Routes>
--- src/components/ListBeers.js
+++ src/components/ListBeers.js
@@ -1,10 +1,39 @@
 import Header from "./Header";
+import { Link } from "react-router-dom";
 
-function ListBeers() {
+function ListBeers({ beers }) {
+  console.log(beers);
   return (
     <div>
       <Header />
-      <h1>List Beers</h1>
+      {beers.map((beer) => {
+        return (
+          <div className='card d-flex flex-row' style={{ width: "30rem" }}>
+            <img
+              src={beer.image_url}
+              className='card-img-top'
+              alt={beer.name}
+            />
+            <div className='card-body'>
+              <h5 className='card-title'>
+                <Link
+                  className='list-group-item list-group-item-action'
+                  to={`/beers/${beer._id}`}
+                >
+                  {beer.name}
+                </Link>
+              </h5>
+              <p className='card-text' style={{ color: "grey" }}>
+                {beer.tagline}
+              </p>
+              <p className='card-text'>
+                <span style={{ fontWeight: "bold" }}>Created By: </span>
+                {beer.contributed_by}
+              </p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
