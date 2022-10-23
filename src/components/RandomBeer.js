5ea7175f901a4b112587165df6935f040879aad3
Julia Spriggs <ironhack@julia-spriggs.fr>
2022-10-23 18:17:42 +0200

    finished iteration 5

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,12 +1,33 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function RandomBeer() {
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://ih-beers-api2.herokuapp.com/beers/random')
+      .then((response) => {
+        setBeers(response.data);
+      })
+      .catch((error) => {
+        console.log("error getting beer from the API", error);
+      });
+  }, []);
+
   return (
     <div>
       <header>
         <Link to="/">Home</Link>
       </header>
-      <h1>this is the page for a random beer</h1>
+      <img src={beers.image_url} alt={beers.name} />
+      <h3>{beers.name}</h3>
+      <p>{beers.tagline}</p>
+      <p>{beers.first_brewed}</p>
+      <p>{beers.attenuation_level}</p>
+      <p>{beers.description}</p>
+      <p>{beers.contributed_by}</p>
     </div>
   );
 }
