add47b5e3de4d5a9437d4e8a794ae72bdee7c2a0
Julia Spriggs <ironhack@julia-spriggs.fr>
2022-10-23 19:00:18 +0200

    finished iteration 6

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
-import { Routes, Route } from 'react-router-dom';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import './App.css';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
-import BeerDetails from './components/BeerDetails';
-import BeersList from './components/BeersList';
-import CreateBeer from './components/CreateBeer';
-import HomePage from './components/HomePage';
-import RandomBeer from './components/RandomBeer';
-
+import BeerDetails from "./components/BeerDetails";
+import BeersList from "./components/BeersList";
+import CreateBeer from "./components/CreateBeer";
+import HomePage from "./components/HomePage";
+import RandomBeer from "./components/RandomBeer";
 
 function App() {
-
   const [beers, setBeers] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
-    .get('https://ih-beers-api2.herokuapp.com/beers')
-    .then(response => {
-      setBeers(response.data)
-    })
-    .catch(error => {
-      console.log('error getting beers from the API', error)
-    })
-  }, [])
+      .get("https://ih-beers-api2.herokuapp.com/beers")
+      .then((response) => {
+        setBeers(response.data);
+      })
+      .catch((error) => {
+        console.log("error getting beers from the API", error);
+      });
+  }, []);
+
+  const createBeer = (beerDetails) => {
+    axios
+      .post("https://ih-beers-api2.herokuapp.com/beers/new", beerDetails)
+      .then((response) => {
+        setBeers([response.data, ...beers]);
+        navigate("/beers");
+      })
+      .catch((error) => {
+        console.log("error creating a new beer", error);
+      });
+  };
 
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/beers' element={<BeersList />} />
-        <Route path='/random-beer' element={<RandomBeer />} />
-        <Route path='/new-beer' element={<CreateBeer />} />
-        <Route path='/beers/:beerId' element={<BeerDetails beers={beers} />} /> 
+        <Route path="/" element={<HomePage />} />
+        <Route path="/beers" element={<BeersList />} />
+        <Route path="/random-beer" element={<RandomBeer />} />
+        <Route
+          path="/new-beer"
+          element={<CreateBeer callbackToCreate={createBeer} />}
+        />
+        <Route path="/beers/:beerId" element={<BeerDetails beers={beers} />} />
       </Routes>
     </div>
   );
--- src/components/CreateBeer.js
+++ src/components/CreateBeer.js
@@ -1,12 +1,101 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-function CreateBeer() {
+function CreateBeer({callbackToCreate}) {
+  const [name, setName] = useState("");
+  const [tagline, setTagline] = useState("");
+  const [first_brewed, setFirst_brewed] = useState("");
+  const [description, setDescription] = useState("");
+  const [brewers_tips, setBrewers_tips] = useState("");
+  const [attenuation_level, setAttenuation_level] = useState("");
+  const [contributed_by, setContributed_by] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const newBeer = {
+      name: name,
+      tagline: tagline,
+      first_brewed: first_brewed,
+      description: description,
+      brewers_tips: brewers_tips,
+      attenuation_level: attenuation_level,
+      contributed_by: contributed_by,
+    };
+
+    callbackToCreate(newBeer);
+
+    // to clear form
+    setName("");
+    setTagline("");
+    setFirst_brewed("");
+    setDescription("");
+    setBrewers_tips("");
+    setAttenuation_level("");
+    setContributed_by("");
+  };
+
   return (
     <div>
       <header>
         <Link to="/">Home</Link>
       </header>
-      <h1>this is the page to create a new beer</h1>
+
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type='text'
+          name='name'
+          placeholder='beer name'
+          value={name}
+          onChange={(e) => { setName(e.target.value) }}
+        />
+        <input
+          type='text'
+          name='tagline'
+          placeholder='beer tagline'
+          value={tagline}
+          onChange={(e) => { setTagline(e.target.value) }}
+        />
+        <input
+          type='text'
+          name='description'
+          placeholder='beer description'
+          value={description}
+          onChange={(e) => { setDescription(e.target.value) }}
+        />
+        <input
+          type='text'
+          name='first brewed'
+          placeholder='first brewed'
+          value={first_brewed}
+          onChange={(e) => { setFirst_brewed(e.target.value) }}
+        />
+        <input
+          type='text'
+          name='brewers tips'
+          placeholder='brewers tips'
+          value={brewers_tips}
+          onChange={(e) => { setBrewers_tips(e.target.value) }}
+        />
+        <input
+          type='number'
+          name='attenuation_level'
+          placeholder='attenuation level'
+          value={attenuation_level}
+          min={0}
+          max={100}
+          onChange={(e) => { setAttenuation_level(e.target.value) }}
+        />
+        <input
+          type='text'
+          name='contributed_by'
+          placeholder='contributed by'
+          value={contributed_by}
+          onChange={(e) => { setContributed_by(e.target.value) }}
+        />
+
+        <button>Create a new beer</button>
+      </form>
     </div>
   );
 }
