6d3dc19a701b3656ffbd1e4a84b45cd2dd16ec01
MoniIgnacio <moniignacio02@gmail.com>
2022-11-04 23:39:29 +0400

    Add it 3 done

--- src/assets/pages/ListBeers.jsx
+++ src/assets/pages/ListBeers.jsx
@@ -1,18 +1,82 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function ListBeers() {
+  // estado
+  const [list, setList] = useState([]);
+
+  const [isFetching, setIsFetching] = useState(true);
+
+  // contact api
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "https://ih-beers-api2.herokuapp.com/beers"
+      );
+      console.log(response.data);
+      // actualizar estado
+      setList(response.data);
+      setIsFetching(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // loading
+  if (isFetching === true) {
+    return <h3>...loading</h3>;
+  }
+
+  const contributed = list;
+
+  // render
   return (
     <div>
-      <nav style={{backgroundColor:'lightblue'}}>
+      <nav
+        style={{
+          backgroundColor: "lightblue",
+          height: "80px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Link to={"/"}>
-          <img src={require("../home.png")} alt="Home" width={'30px'} />
+          <img src={require("../home.png")} alt="Home" width={"40px"} />
         </Link>
       </nav>
 
-      <div>
-        ListBeers
-        <p>Lorem ipsum dolor sit amet.</p>
+      <div style={{ padding: "80px 0px 0px 15%", border:'1px solid black' }}>
+        {list.map((eachBeer) => {
+          return (
+            <div key={eachBeer._id} style={{ display: "flex", gap: "50px" }}>
+            <Link to={`/beers/${eachBeer._id}`}><img
+                src={eachBeer.image_url}
+                style={{ width: "150px", padding: "20px"}} alt='imgBeer'
+              /></Link>
+              
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  maxWidth: "200px",
+                }}
+              >
+                <h3>{eachBeer.name}</h3>
+                <h5>{eachBeer.tagline}</h5>
+                <p> Created by: {eachBeer.contributed_by.split("<", 1)}</p>
+              </div>
+            </div>
+            
+          );
+        })}
       </div>
     </div>
   );
--- src/assets/pages/SingleBeer.jsx
+++ src/assets/pages/SingleBeer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function SingleBeer() {
   return (
     <div>
-        <nav style={{backgroundColor:'lightblue'}}>
+      <nav style={{backgroundColor:'lightblue', height: '80px', display:'flex', justifyContent:'center', alignItems:'center'}}>
         <Link to={"/"}>
           <img src={require("../home.png")} alt="Home" width={'30px'} />
         </Link>
