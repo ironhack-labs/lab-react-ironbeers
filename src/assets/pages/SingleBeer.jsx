382f0eca851c273febf4a1f05420cdb798b78682
MoniIgnacio <moniignacio02@gmail.com>
2022-11-05 23:02:35 +0400

    Add It 4 done

--- src/assets/pages/SingleBeer.jsx
+++ src/assets/pages/SingleBeer.jsx
@@ -5,14 +5,14 @@ import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 function SingleBeer() {
-  const {beerId} = useParams()
+  const { beerId } = useParams();
 
-  const [details,setDetails] = useState([])
+  const [details, setDetails] = useState([]);
   const [isFetching, setIsFetching] = useState(true);
 
-  useEffect(()=>{
-    getData()
-  },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
   const getData = async () => {
     try {
@@ -20,7 +20,7 @@ function SingleBeer() {
         `https://ih-beers-api2.herokuapp.com/beers/${beerId}`
       );
       // actualizar estado
-      // details(response);
+      setDetails(response.data);
       setIsFetching(false);
       console.log(response.data);
     } catch (error) {
@@ -32,17 +32,47 @@ function SingleBeer() {
     return <h3>...loading</h3>;
   }
 
-
-
-  
   return (
     <div>
-      <nav style={{backgroundColor:'lightblue', height: '80px', display:'flex', justifyContent:'center', alignItems:'center'}}>
+      <nav
+        style={{
+          backgroundColor: "lightblue",
+          height: "80px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Link to={"/"}>
-          <img src={require("../home.png")} alt="Home" width={'30px'} />
+          <img src={require("../home.png")} alt="Home" width={"30px"} />
         </Link>
       </nav>
-      SingleBeer
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "20px",
+        }}
+      >
+        <img
+          src={details.image_url}
+          alt="img-beer"
+          style={{ width: "100px" }}
+        />
+        <div >
+          <h2>{details.name}</h2>
+          <div style={{display:'flex',justifyContent:'flex-end'}}>
+            <p><b>{details.first_brewed}</b></p>
+            <p>{details.attenuation_level}</p>
+          </div>
+          <h5>{details.tagline}</h5>
+          <p></p>
+          <p>{details.description}</p>
+          <p> Created by: {details.contributed_by.split("<", 1)}</p>
+        </div>
+      </div>
     </div>
   );
 }
