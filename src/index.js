d39b45346d21c6ec7068690817cf0e56a3d3f10a
Patrick-Groot <patrick@candis.io>
2022-11-14 09:21:33 +0100

    Fix Home icon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import { IconContext } from "react-icons";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <IconContext.Provider value={{ size: 40, color: "white" }}>
+      <Router>
+        <App />
+      </Router>
+    </IconContext.Provider>
   </React.StrictMode>
 );
 
