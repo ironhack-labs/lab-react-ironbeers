ffa6ced63a3c2e4b8ac24b251d38deb080c8e91c
Bruno Rocha <bruno.miguel.rocha@hotmail.com>
2022-11-18 18:32:05 +0000

    lab done

--- src/pages/BeerDetails.jsx
+++ src/pages/BeerDetails.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import axios from 'axios';
 
 function BeerDetails() {
@@ -23,6 +24,11 @@ function BeerDetails() {
 
   return (
     <div>
+    <div>
+    <Link to='/'>
+        <img src="https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png" alt="navbar" />
+    </Link>
+    </div>
         {thisBeer && <div>
             <img src={thisBeer.image_url} alt="beer" />
             <h3>{thisBeer.name}</h3>
