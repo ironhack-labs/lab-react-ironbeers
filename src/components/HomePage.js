e44db396e9594c5934c883f170ac3ab56647ca38
Luluyan2022 <emmymagao@gmail.com>
2023-01-21 00:18:33 +0100

    fixed my import img error

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,20 @@
 
 import { Link } from 'react-router-dom';
-
+import beers from '../assets/beers.png'
+import newBeer from '../assets/new-beer.png'
+import randomBeer from '../assets/random-beer.png'
 
 export default function HomePage() {
+
     return (
         <>
-            <img src='/assets/beers.png' alt='beers' />
+            <img src={beers} alt='beers' />
             <Link to="/beers">All Beers</Link>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum orci a sapien imperdiet maximus. Suspendisse vel ex eget mauris efficitur finibus eu a enim. Aenean tellus neque, consectetur sit amet lacus a, hendrerit ornare urna. Fusce venenatis lorem vitae augue suscipit, eu tempor massa vulputate. Praesent iaculis velit libero, vel faucibus sem pulvinar vel. Curabitur viverra nisl dui, a venenatis felis pellentesque id. Donec non lacus diam.</p>
-            <img src='/assets/new-beer.png' alt='beers' />
+            <img src={randomBeer} alt='randomBeer' />
             <Link to="/random-beer">Random Beer</Link>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum orci a sapien imperdiet maximus. Suspendisse vel ex eget mauris efficitur finibus eu a enim. Aenean tellus neque, consectetur sit amet lacus a, hendrerit ornare urna. Fusce venenatis lorem vitae augue suscipit, eu tempor massa vulputate. Praesent iaculis velit libero, vel faucibus sem pulvinar vel. Curabitur viverra nisl dui, a venenatis felis pellentesque id. Donec non lacus diam.</p>
-            <img src='/assets/random-beer.png' alt='beers' />
+            <img src={newBeer} alt='newBeer' />
             <Link to="/new-beer">New Beer</Link>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum orci a sapien imperdiet maximus. Suspendisse vel ex eget mauris efficitur finibus eu a enim. Aenean tellus neque, consectetur sit amet lacus a, hendrerit ornare urna. Fusce venenatis lorem vitae augue suscipit, eu tempor massa vulputate. Praesent iaculis velit libero, vel faucibus sem pulvinar vel. Curabitur viverra nisl dui, a venenatis felis pellentesque id. Donec non lacus diam.</p>
         </>
