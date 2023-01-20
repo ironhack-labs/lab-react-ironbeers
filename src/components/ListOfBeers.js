838fe948786ef87bd8e10bedb0fbed6f97a50544
Laura Bährs <laura.baehrs@googlemail.com>
2023-01-20 20:23:12 +0100

    finish iteration 7

--- src/components/ListOfBeers.js
+++ src/components/ListOfBeers.js
@@ -1,51 +1,74 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import './ListOfBeers.css';
+import "./ListOfBeers.css";
 
+function ListOfBeers() {
+  const [beersArray, setBeersArray] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
 
-function ListOfBeers(){
+  useEffect(() => {
+    getBeersFromApi();
+  });
 
-    const [beersArray, setBeersArray] = useState(null);
+  const url = "https://ih-beers-api2.herokuapp.com/beers";
 
-    useEffect(() => {
-      getBeersFromApi();
-    }, []);
-    
-    const url = "https://ih-beers-api2.herokuapp.com/beers";
-  
-    const getBeersFromApi = () => {
-      axios.get(url)
-        .then((response) => {
-          console.log(response.data);
-  
-          setBeersArray(response.data);
+  const getBeersFromApi = () => {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response.data);
+
+        const beersToDisplay = response.data.filter( (beer) => {
+            return beer.name.toLowerCase().includes(searchQuery.toLowerCase());
         })
-        .catch((e) => {
-          console.log(e);
-        });
-    }
+        setBeersArray(beersToDisplay);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-    return(
-        <>
-        <h1>This is ListOfBeers</h1>
-        {beersArray === null
-                ? "loading..."
-                : beersArray.map((beerDetails) => {
-                    return (
-                        <div className="beerBox" key={beerDetails._id} >
-                            <img className="beerImage" src={beerDetails.image_url} alt="beer"></img>
-                            <div className="beerText">
-                            <h2>{beerDetails.name}</h2>
-                            <h3>{beerDetails.tagline}</h3>
-                            <h6>Created By: {beerDetails.contributed_by}</h6>
-                            <Link to={"/beers/" + beerDetails._id}>More Details</Link>
-                            </div>
-                        </div>
-                    )
-                })}
-        </>
-    )
+  return (
+    <>
+      <h1>This is ListOfBeers</h1>
+      <hr />
+      <form>
+        <label>
+          Search by Title:
+          <input
+            type="text"
+            name="searchQuery"
+            placeholder="Search by title"
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+          />
+        </label>
+      </form>
+      <hr />
+      {beersArray === null
+        ? "loading..."
+        : beersArray.map((beerDetails) => {
+            return (
+              <div className="beerBox" key={beerDetails._id}>
+                <img
+                  className="beerImage"
+                  src={beerDetails.image_url}
+                  alt="beer"
+                ></img>
+                <div className="beerText">
+                  <h2>{beerDetails.name}</h2>
+                  <h3>{beerDetails.tagline}</h3>
+                  <h6>Created By: {beerDetails.contributed_by}</h6>
+                  <Link to={"/beers/" + beerDetails._id}>More Details</Link>
+                </div>
+              </div>
+            );
+          })}
+    </>
+  );
 }
 
 export default ListOfBeers;
