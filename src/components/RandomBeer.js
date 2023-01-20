df5442325ead2fea501d31ee0564ac9ed63c6d72
Rudolf Teibtner <rudolfteibtner@mailbox.org>
2023-01-20 20:09:30 +0100

    add single and random  beer, not fully working yet

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,12 +1,47 @@
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const RandomBeer = () => {
+const RandomBeer = (devAPI) => {
+  const [randomBeer, setRandomBeer] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getRandomBeer();
+  }, []);
+
+  const getRandomBeer = () => {
+    axios
+      .get(devAPI + "/beers/random")
+      .then((aRandomBeer) => {
+        console.log(aRandomBeer);
+
+        setRandomBeer(aRandomBeer); // I want to access and display the API's response
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  if (isLoading) {
+    return <div>isLoading</div>;
+  }
+
   return (
-    <nav>
-      <NavLink className="App-link" to="/">
-        Home
-      </NavLink>
-    </nav>
+    <div>
+      <nav>
+        <NavLink className="App-link" to="/">
+          Home
+        </NavLink>
+      </nav>
+      <h2>Title: {randomBeer.name}</h2>
+      <p>Tagline: {randomBeer.tagline}</p>
+      <p>First Brewed: {randomBeer.first_brewed}</p>
+      <p>Attenuation Level: {randomBeer.attenuation_level}</p>
+      <p>Description: {randomBeer.description}</p>
+      <p>Contributed by: {randomBeer.contributed_by}</p>
+    </div>
   );
 };
 export default RandomBeer;
--- src/components/SingleBeer.js
+++ src/components/SingleBeer.js
@@ -1,12 +1,47 @@
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const SingleBeer = () => {
+  const [beerArr, setBeersArr] = useState();
+  const { beerId } = useParams();
+
+  const oneSingleBeer = beerArr.find((beerDetails) => {
+    return beerDetails.id === beerId;
+  });
+
+  useEffect(() => {
+    getSingleBeer();
+  }, []);
+
+  const getSingleBeer = () => {
+    axios
+      .get(process.env.REACT_APP_API_URL + "/beers/" + beerId)
+      .then((response) => {
+        console.log(response);
+
+        setBeersArr(response);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   return (
-    <nav>
-      <NavLink className="App-link" to="/">
-        Home
-      </NavLink>
-    </nav>
+    <div>
+      <nav>
+        <NavLink className="App-link" to="/">
+          Home
+        </NavLink>
+      </nav>
+      <h2>Title: {oneSingleBeer.name}</h2>
+      <p>Tagline: {oneSingleBeer.tagline}</p>
+      <p>First Brewed: {oneSingleBeer.first_brewed}</p>
+      <p>Attenuation Level: {oneSingleBeer.attenuation_level}</p>
+      <p>Description: {oneSingleBeer.description}</p>
+      <p>Contributed by: {oneSingleBeer.contributed_by}</p>
+    </div>
   );
 };
 
