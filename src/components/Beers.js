cd3143da11b15e7664776bb2f6f08bedfebceb77
Thalita dos Reis <dosreistha@gmail.com>
2023-01-21 11:35:31 +0100

    iteration bonus done

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,14 +1,30 @@
-import { Card } from 'react-bootstrap';
-import { Link} from 'react-router-dom'
+import { useState } from 'react';
+import { Card, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 import Header from './Header';
 
 export default function Beers({beerList}) {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const beerDisplay = beerList.filter((beer) => {
+    return beer.name.toLowerCase().includes(searchQuery.toLowerCase())
+  })
 
   return (
     <>
       <Header />
 
-      {beerList.map((beer) => (
+      <Form style={{ width: "50%", margin: "auto", padding: "15px" }}>
+        <Form.Label >Search for your favourite beer </Form.Label> 
+        <Form.Control type="text"
+          name="searchQuery"
+          value={searchQuery}
+          onChange={(e) => { setSearchQuery(e.target.value) }} 
+        /> 
+      </Form>
+
+      {beerDisplay.map((beer) => (
         <Card key={beer._id} style={{ display: "flex", flexDirection: "row", width: "50%", margin: "auto", padding: "15px" }}>
           <Card.Img src={beer.image_url} alt="" style={{ width: "auto", height: "250px" }} />
           <Card.Body style={{ display: "flex", flexDirection: "column", margin: "auto 20px"}}>
