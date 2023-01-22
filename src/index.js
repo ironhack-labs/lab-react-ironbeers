0e70afdfa5df4a1a364062ae52bc2cac0afd4906
Alfie Collins <alfiecollins44@hotmail.co.uk>
2023-01-22 19:35:14 +0000

    iteration 1-3

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Router>
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>
-  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Beers.js
+++ src/pages/Beers.js
@@ -1,9 +1,38 @@
+import { useState, useEffect } from "react";     // <== IMPORT useEffect
+import axios from "axios";     
+import { Link } from "react-router-dom"; 
+
 import Header from "../components/Header";
 
 function Beers(){
+    const [beers, setBeers] = useState([]);
+    useEffect(() => {                                // <== ADD THE EFFECT
+        axios
+          .get("https://ih-beers-api2.herokuapp.com/beers")
+          .then((response) => {
+            console.log('response.data', response.data);
+            setBeers(response.data)
+          });
+        
+      }, [] );
+
     return(
 <div>
         <Header/>
+        <h1>All Beers</h1>
+
+{beers.map((beer) => (
+  <div key={beer._id} className="beerList">
+    <img src={beer.image_url} alt="beerPic" />
+    <Link to={`beers/${beer._id}`}> 
+    <h2>{beer.name}</h2>
+    </Link>
+    <p>{beer.tagline}</p>
+    <p>Created By: {beer.contributed_by}</p>
+  </div>
+))}
+
+        
 </div>
     )
 }
