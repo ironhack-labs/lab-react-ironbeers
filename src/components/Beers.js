6bd79a88bb3bf9c4d3270c386884ca5069581418
Lai In Chang <lavender880331@gmail.com>
2023-01-22 02:44:47 +0100

    finish iteration 3 - list all the beers

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,11 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 
 function Beers() {
+    const [beers, setBeers] = useState([])
+
+    useEffect(() => {
+        axios.get(process.env.REACT_APP_API_URL)
+            .then((res) => {
+                setBeers(res.data);
+            })
+            .catch((err) => {
+                console.log("error getting beers from API", err);
+            })
+    }, [])
 
     return (
         <div>
             <Header />
-            <h1>It's Beers page</h1>
+            {beers === []
+                ? <h1>Loading...</h1>
+                : beers.map((beer, index) => {
+                    return (
+                        <div className="beerCard" key={index}>
+                            <img src={beer.image_url} alt="" />
+                            <div className="beerText">
+                                <h1>{beer.name}</h1>
+                                <h2>{beer.tagline}</h2>
+                                <p><strong>Created by: </strong>{beer.contributed_by}</p>
+                            </div>
+
+
+
+                        </div>
+
+                    )
+                })
+            }
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 import { NavLink } from "react-router-dom";
 import Home from "./Home";
-import "./Header.css"
 
 function Header() {
     return (
-        <NavLink to="/" element={<Home />}><img src="https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png" alt=""></img></NavLink>
+        <NavLink to="/" element={<Home />}><img className="header" src="https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png" alt=""></img></NavLink>
     )
 }
 
