2ff42a48961455f9ab72a1dd2b9702d19152909f
Lovro Boric <lovro.boric@gmail.com>
2023-01-22 22:03:26 +0100

    iteration1

---
+++ src/components/HomeCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HomeCard = ({ title, img }) => {
+    return (
+        <div>
+            <h2>{title}</h2>
+            <img src={img} alt={title} />
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus facilis nulla aperiam eaque sapiente consectetur neque dolorem quaerat saepe et.
+            </p>
+        </div>
+    );
+};
+
+export default HomeCard;
