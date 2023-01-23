84aa601f5f92e7c7c39fe26364e70798e1310980
Anil Ozsoy <anilozsoy@gmail.com>
2023-01-23 15:03:40 +0100

    Iteration 2 | Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import homeIcon from "./assets/homeIcon48.svg";
 
 function App() {
   return (
     <div className="App">
+      <header
+        style={{
+          backgroundColor: "blue",
+          height: "56px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Link to={"/"}>
+          <img src={homeIcon} alt="" height={32} width={32} />
+        </Link>
+      </header>
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
