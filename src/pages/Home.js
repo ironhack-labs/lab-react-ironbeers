a283c7959bd0abdf7c8829dfd8726aae056a7bba
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-02-25 10:38:38 +0100

    fix links to images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,26 @@
+import beers from "../assets/beers.png";
+import randomBeer from "../assets/random-beer.png";
+import newBeer from "../assets/new-beer.png";
+
 function Home() {
   return (
     <>
       <img
-        src="../assets/beers.png/"
+        src={beers}
         alt="all beers"
       />
       <h3>All beers</h3>
       <p>Check out all beers in our collection!</p>
 
       <img
-        src="../assets/random-beer.png/"
+        src={randomBeer}
         alt="random beer"
       />
       <h3>Random beer</h3>
       <p>Check out a random beer!</p>
 
       <img
-        src="../assets/new-beer.png/"
+        src={newBeer}
         alt="new beer"
       />
       <h3>New beer</h3>
