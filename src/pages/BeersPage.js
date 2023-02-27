c8d7b1cbf44bb78c0b5ced999fe955752cc7c4fd
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-02-27 16:31:28 +0100

    start iteration 7

--- src/pages/BeersPage.js
+++ src/pages/BeersPage.js
@@ -5,36 +5,50 @@ import { useEffect, useState } from "react";
 const imgURL = "https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"
 
 function BeersPage() {
-const [beers, setBeers] = useState([])
+  const [beers, setBeers] = useState([])
+  const [query, setQuery] = useState("")
 
-useEffect(() => {
-axios.get("https://ih-beers-api2.herokuapp.com/beers")
-.then (response => {
-console.log(response.data)
-setBeers(response.data)
-})
-},[])
+  useEffect(() => {
+    axios.get("https://ih-beers-api2.herokuapp.com/beers")
+      .then(response => {
+        console.log(response.data)
+        setBeers(response.data)
+      })
+  }, [])
 
-    return ( 
-       <div>
-        <header>
-        <Link to="/"> <img src={imgURL} alt="header" style={{width: "100vw"}} /></Link>
-       </header>
-       {beers.map(beer => {
-        return (         
-         <div key={beer._id} className="beer">
-          <img src={beer.image_url} alt="" style={{height: "10vh"}}/>
-          <div className="beer-description">
-          <h3><Link to={`/beers/${beer._id}`} >{beer.name}</Link></h3>
-          <p>{beer.tagline}</p>
-          <h6>{beer.contributed_by}</h6>
-          <hr style={{width: "100vw"}} />
-          </div>
+    const handleSelect = (e) => {
+        setQuery(e.target.value)
+      }
+
+
+  return (
+    <div>
+      <header>
+        <Link to="/"> <img src={imgURL} alt="header" style={{ width: "100vw" }} /></Link>
+      </header>
+      <div className="search">
+        <h3>Search</h3>
+        <form>
+          <label htmlFor="name"></label>
+          <input type="text" name="name" value={query} onChange={(e) => handleSelect(e)} />
+        </form>
       </div>
+
+      {beers.map(beer => {
+        return (
+          <div key={beer._id} className="beer">
+            <img src={beer.image_url} alt="" style={{ height: "10vh" }} />
+            <div className="beer-description">
+              <h3><Link to={`/beers/${beer._id}`} >{beer.name}</Link></h3>
+              <p>{beer.tagline}</p>
+              <h6>{beer.contributed_by}</h6>
+              <hr style={{ width: "100vw" }} />
+            </div>
+          </div>
         )
-       })}
-       </div>
-     );
+      })}
+    </div>
+  );
 }
 
 export default BeersPage;
