238027995db2156bc7e15dda332da555a72801c3
Alexandre Alvaro <a.borges.alvaro@gmail.com>
2023-03-03 17:11:52 +0000

    bonus iteration done

--- src/pages/Beers.jsx
+++ src/pages/Beers.jsx
@@ -5,11 +5,11 @@ import { ApiContext } from '../contexts/api.context'
 
 import Header from '../components/Header'
 
-
-
 function Beers() {
     const [beers, setBeers] = useState([])
     const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState('');
+    const [searchedBeers, setSearchedBeers] = useState(null)
     const {apiUrl} = useContext(ApiContext)
 
     const getBeers = async () => {
@@ -26,11 +26,51 @@ function Beers() {
         getBeers();
       }, []);
 
+    const handleSearch = (e) => setSearch(e.target.value);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        //make the call to the api
+        try {
+            const response = await axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${search}`)
+            setSearchedBeers(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
   return (
     <div>
         <Header />
+		<form onSubmit={handleSubmit}>
+			<label htmlFor='search'>Search Beer</label>
+			<input
+				type='text'
+				name='search'
+				value={search}
+				onChange={handleSearch}
+			/>
+			<button type='submit'>Search</button>
+		</form>
+
         {loading && <p>Loading ...</p>}
-        {beers && (
+
+        {searchedBeers && (
+            searchedBeers.map((beer) => {
+                return (
+                    <div key={beer._id}>
+                        <img src={beer.image_url} alt={`${beer.name}`}/>
+                        <Link to={`/beers/${beer._id}`}>
+                            <h2>{beer.name}</h2>
+                        </Link>
+                        <h3>{beer.tagline}</h3>
+                        <p><span style={{fontWeight: 500}}>Created by: </span>{beer.contributed_by}</p>
+                    </div>
+                )
+            })
+        )}
+        
+        {beers && !searchedBeers && (
             beers.map((beer) => {
                 return (
                     <div key={beer._id}>
