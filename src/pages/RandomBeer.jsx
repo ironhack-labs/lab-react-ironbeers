933ca19e164fcdc8b6f78699a921eaa59887cdf9
Cristina Chaparro <cricbau@gmail.com>
2023-03-05 17:18:48 +0100

    iteration 5 done

--- src/pages/RandomBeer.jsx
+++ src/pages/RandomBeer.jsx
@@ -1,9 +1,35 @@
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 
+import axios from "axios";
+
 function RandomBeer() {
+  const [randomBeer, setRandomBeer] = useState("");
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "https://ih-beers-api2.herokuapp.com/beers/random"
+      );
+      setRandomBeer(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <Header />
+      <img src={randomBeer.image_url} alt={randomBeer.name} width={"250px"} />
+      <h3>{randomBeer.name}</h3>
+      <p>{randomBeer.tagLine}</p>
+      <p>{randomBeer.first_brewed}</p>
+      <p>{randomBeer.attenuation_level}</p>
+      <p>{randomBeer.description}</p>
+      <p>{randomBeer.contributed_by}</p>
     </div>
   );
 }
