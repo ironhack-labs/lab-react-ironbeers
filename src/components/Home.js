a70937dd7adaa07a6416a2bb67425f167ef9d83a
Caio Mazur <caiomazur1@gmail.com>
2023-03-05 13:01:41 +0000

    bonus iteration done ✔

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
     <div>
-        <a href="/beers">Beers</a>
-        <a href="/random">Random Beer</a>
-        <a href="/new">New-Beer</a>
+      <a href="/beers">Beers</a>
+      <a href="/random">Random Beer</a>
+      <a href="/new">New-Beer</a>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
