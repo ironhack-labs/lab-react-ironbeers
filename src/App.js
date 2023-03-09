e64ecdfee758b8c89d38b671790d51ff468fa1af
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-03-09 20:44:35 +0530

    iteration 5 random beer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Beers from './pages/Beers';
 import BeerDetails from './pages/BeerDetails';
+import RandomBeer from './pages/RandomBeer';
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/beers" element={<><NavBar /> <Beers /></>} />
           <Route path="/beers/:beerId" element={<><NavBar /> <BeerDetails /></>} />
+          <Route path="/random-beer" element={<><NavBar /> <RandomBeer /></>} />
         </Routes>
       </div>
     </div>
--- src/pages/RandomBeer.jsx
+++ src/pages/RandomBeer.jsx
@@ -1,10 +1,40 @@
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function RandomBeer() {
 
-    return (
-        <div>
-this is the random beer page
-        </div>
-    )
+    const [beerRandom, setRandom] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+            .then((result) => {
+                setRandom(result.data)
+            })
+    }, []);
+
+    if (beerRandom) {
+        return (
+            <div className="beer-details">
+                <div className="beer-div1" >
+                    <img src={beerRandom.image_url} alt="" className="beer-image" />
+                </div>
+                <div className="beer-div2">
+                    <h1>{beerRandom.name}</h1>
+                    <h1 style={{ color: "gray" }}>{beerRandom.attenuation_level}</h1>
+                </div>
+                <div className="beer-div3">
+                    <h4 style={{ color: "gray" }}>{beerRandom.tagline}</h4>
+                    <h4>{beerRandom.first_brewed}</h4>
+                </div>
+                <div className="beer-div4">
+                    <p>{beerRandom.description}</p>
+                    <p style={{ color: "gray" }}>Created by: {beerRandom.contributed_by}</p>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default RandomBeer;
