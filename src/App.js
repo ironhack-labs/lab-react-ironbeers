5b3223310433d80030cc85f9d76615b3f0d67636
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-03-09 21:39:36 +0530

    functioning form for iteration 6

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
 import React from 'react'
-// import { useState, useEffect } from 'react';
-// import axios from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Beers from './pages/Beers';
 import BeerDetails from './pages/BeerDetails';
 import RandomBeer from './pages/RandomBeer';
+import NewBeer from './pages/NewBeer';
 
 
 function App() {
@@ -23,6 +22,7 @@ function App() {
           <Route path="/beers" element={<><NavBar /> <Beers /></>} />
           <Route path="/beers/:beerId" element={<><NavBar /> <BeerDetails /></>} />
           <Route path="/random-beer" element={<><NavBar /> <RandomBeer /></>} />
+          <Route path="/new-beer" element={<><NavBar /> <NewBeer /></>} />
         </Routes>
       </div>
     </div>
--- src/pages/NewBeer.jsx
+++ src/pages/NewBeer.jsx
@@ -1,10 +1,72 @@
+import React from 'react'
+import { useState } from "react";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
 function NewBeer() {
 
-    return(
-        <div>
-this is the new beer page
+    const [name, setName] = useState('')
+    const [tagline, setTagline] = useState('')
+    const [description, setDescription] = useState('')
+    const [first_brewed, setFirstBrewed] = useState('')
+    const [brewers_tips, setBrewersTips] = useState('')
+    const [attenuation_level, setAttenuationLevel] = useState(0)
+    const [contributed_by, setContributedBy] = useState('')
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const beerToPost = { name, tagline, description, first_brewed, brewers_tips, attenuation_level, contributed_by }
+        // axios.post has two arguments, one where we post to 2 is what we are posting 
+        axios.post('https://ih-beers-api2.herokuapp.com/beers/new', beerToPost)
+            .then(() => {
+                setName('')
+                setTagline('')
+                setDescription('')
+                setFirstBrewed('')
+                setBrewersTips('')
+                setAttenuationLevel(0)
+                setContributedBy('')
+                alert("Cheers, your beer was created!")
+            })
+        navigate('/')
+    }
+
+    return (
+        <div className="AddBeer">
+            <form action="" onSubmit={handleSubmit}>
+                <label htmlFor="">
+                    Name:
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                </label>
+                <label htmlFor="">
+                    Tagline:
+                    <input type="text" value={tagline} onChange={(e) => setTagline(e.target.value)} />
+                </label>
+                <label htmlFor="">
+                    Description:
+                    <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+                </label>
+                <label htmlFor="">
+                    First Brewed:
+                    <input type="date" value={first_brewed} onChange={(e) => setFirstBrewed(e.target.value)} />
+                </label>
+                <label htmlFor="">
+                    Brewers Tips:
+                    <input type="text" value={brewers_tips} onChange={(e) => setBrewersTips(e.target.value)} />
+                </label>
+                <label htmlFor="">
+                    Attenuation Level:
+                    <input type="number" value={attenuation_level} onChange={(e) => setAttenuationLevel(e.target.value)} />
+                </label>
+                <label htmlFor="">
+                    Contributed By:
+                    <input type="text" value={contributed_by} onChange={(e) => setContributedBy(e.target.value)} />
+                </label>
+                <button>ADD NEW</button>
+            </form>
         </div>
-    )
+    );
 }
 
 export default NewBeer;
