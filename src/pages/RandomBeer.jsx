a9e5b90384579aa3ce642096a54829ee59090a85
AzizaHalder <aziza.halder@sap.com>
2023-03-12 20:25:35 +0100

    feat(RandomBeers): display a random beer, iteration 5

--- src/pages/RandomBeer.jsx
+++ src/pages/RandomBeer.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function RandomBeer() {
+  // const {beerId} = useParams()
+  const [randomBeer, setRandomBeer] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-beers-api2.herokuapp.com/beers/random")
+      .then((response) => {
+        console.log(response.data);
+        setRandomBeer(response.data);
+      })
+      .catch((error) => console.log("Error retrieving random beer", error));
+  }, []);
+
   return (
     <div>
       <nav>
         <Navbar />
       </nav>
-      <h1>Random Beer</h1>
+      <div>
+        <img
+          src={randomBeer.image_url}
+          alt={randomBeer.name}
+          id="single-beer-img"
+        />
+        <h1>{randomBeer.name}</h1>
+        <h2>{randomBeer.tagline}</h2>
+        <h3>{randomBeer.first_brewed}</h3>
+        <h1>{randomBeer.attenuation_level}</h1>
+        <p>{randomBeer.description}</p>
+        <p>
+          <b>{randomBeer.contributed_by}</b>
+        </p>
+      </div>
     </div>
   );
 }
