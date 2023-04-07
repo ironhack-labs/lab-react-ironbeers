1d6bedddc967081392deadebbfc0ec06405eb5a0
Pim Blom <pim.blom@icloud.com>
2023-04-07 22:30:33 +0200

    iteration 5

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,15 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"
 
 
 function RandomBeer() {
+
+    const [randomBeer, setRandomBeer]= useState(null)
+
+    useEffect(()=>{
+        axios.get("https://ih-beers-api2.herokuapp.com/beers/random")
+        .then(response =>{
+            setRandomBeer(response.data)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }, [])
+
+    const renderRandomBeer= ()=>{
+        return(
+            <section>
+                <img src={randomBeer.image_url} alt={randomBeer.name}/>
+                <h3>{randomBeer.name}</h3>
+                <p>{randomBeer.tagline}</p>
+                <p>{randomBeer.first_brewed}</p>
+                <p>{randomBeer.attenuation_level}</p>
+                <p>{randomBeer.description}</p>
+                <p>{randomBeer.contributed_by}</p>
+                <Link to={`/`}> Home</Link>
+            </section>
+        )
+    }
   return (
     <>
       <header>
         <Link to={"/"}> Home</Link>
       </header>
-      <section>
-        <h1> Hello</h1>
-      </section>
+      
+        {randomBeer 
+        ? renderRandomBeer()
+        : <h2>Loading...</h2>
+        }
+    
     </>
   );
 }
