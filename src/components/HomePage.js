6548a835377d02f8ddea2c5111d96c5a30122f3a
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-04-08 22:34:54 +0200

    iteration 1 done

---
+++ src/components/HomePage.js
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom"
+
+export default function HomePage() {
+
+    <div>
+        <NavLink to="/beers"> All Beers </NavLink>
+        <NavLink to="/random-beer"> Random Beer</NavLink>
+        <NavLink to="/new-beer">New Beer</NavLink>
+    </div>
+}
