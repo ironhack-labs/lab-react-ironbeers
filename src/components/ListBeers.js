ae9c9cd691d44c859f018e7da630c218358daef2
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-04-09 01:03:14 +0200

    added antd design

--- src/components/ListBeers.js
+++ src/components/ListBeers.js
@@ -1,43 +1,49 @@
 import { useEffect, useState } from "react";
-import axios from "axios"
+import axios from "axios";
 import { Link } from "react-router-dom";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Card, Col } from "antd";
 
 export default function ListBeers() {
-    const [beers, setBeers] = useState([])
+  const [beers, setBeers] = useState([]);
 
-useEffect(() => {
-axios
-.get(`${process.env.REACT_APP_APIURL}`)
-.then( response => {
-    setBeers(response.data)
-    console.log(response.data)
-})
-.catch(err => console.log("error getting beers from API", err))
-},[])
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_APIURL}`)
+      .then((response) => {
+        setBeers(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => console.log("error getting beers from API", err));
+  }, []);
 
-
-return (
+  return (
     <div>
       <h1>Beers List</h1>
       <ul>
+      
         {beers ? (
           beers.map((beer) => {
-            console.log(beer);
+
             return (
-              <div key={beer.id}>
-              <img src={beer.image_url} alt={beer.name} />
-                <h3>{beer.name}</h3>
-                <p>{beer.tagline}</p>
-                <p>Created by {beer.contributed_by}</p>
-             
-                <Link to={`/${beer._id}`}>More Details</Link>
-              </div>
+               
+                      <Col key={beer.id}>
+                      <Card >
+                        <img src={beer.image_url} alt={beer.name} />
+                        <h3>{beer.name}</h3>
+                        <p>{beer.tagline}</p>
+                        <p>Created by {beer.contributed_by}</p>
+
+                        <Link to={`/${beer._id}`}>More Details</Link>
+                        </Card>
+                      </Col>
+                 
             );
           })
         ) : (
           <p>Loading beers...</p>
         )}
+    
       </ul>
     </div>
   );
