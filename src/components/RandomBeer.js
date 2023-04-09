ab642474c391cba4b46bd59e096153aea8a2736b
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-04-09 12:03:17 +0200

    iteration 5 done

--- src/components/RandomBeer.js
+++ src/components/RandomBeer.js
@@ -1,11 +1,51 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+import { Card, Col } from "antd";
+
+
 export default function RandomBeer(){
+
+const [randomBeer, setRandomBeer] = useState(null);
+
+
+
+useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_APIURL}/random`)
+      .then((response) => {
+        setRandomBeer(response.data);
+      })
+      .catch((err) => console.log("error getting random beer from API", err));
+  }, []);
+
+
+const renderDetails = () => {
+
 return(
-<div>
 
-    
-</div>
+    <Col>
+   <Card>
+    <img src={randomBeer.image_url} alt={randomBeer.name} />
+    <h3>{randomBeer.name}</h3>
+    <p>{randomBeer.tagline}</p>
+    <p>{randomBeer.first_brewed}</p>
+    <p>{randomBeer.attenuation_level}</p>
+    <p>{randomBeer.description}</p>
+    <p>{randomBeer.contributed_by}</p>
+  
 
+    </Card>
+    </Col>
+)
+}
+
+
+return (
+ <>
+    {randomBeer ? renderDetails() :<p>Loading details...</p> }
+ </>
 )
 
-
 }
+
+
