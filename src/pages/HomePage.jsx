b9171dcc93bc4a1f499ce0764635f716e27d07df
David Reguart <reguartdavid@gmail.com>
2023-04-11 12:37:50 +0200

    Iteration 6 New Beer => Done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function HomePage() {
   return (
 
-    <div className="container flex flex-col items-center justify-center h-full w-full overflow-y-hidden">
+    <div className="container flex flex-col items-center justify-center h-full w-full overflow-y-hidden fixed">
 
       <Link to="/beers">
         <div className="w-full h-full -mt-8 mb-6">
@@ -25,7 +25,7 @@ function HomePage() {
       </Link>
 
       <Link to="/random-beer">
-        <div className="w-full h-full mt-4 mb-6">
+        <div className="w-full h-full mt-4">
           <div className="w-3/4 h-40 rounded-3xl mx-auto">
             <img className="object-cover h-full w-full rounded-3xl" src="../assets/random-beer.png" alt="" />
           </div>
--- src/pages/NewBeer.jsx
+++ src/pages/NewBeer.jsx
@@ -1,8 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function NewBeer() {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    tagline: "",
+    description: "",
+    first_brewed: "",
+    brewers_tips: "",
+    attenuation_level: 0,
+    contributed_by: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("https://ih-beers-api2.herokuapp.com/beers/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => {
+        if (res.ok) {
+          console.log("New beer created")
+          setFormData({
+            name: "",
+            tagline: "",
+            description: "",
+            first_brewed: "",
+            brewers_tips: "",
+            attenuation_level: 0,
+            contributed_by: "",
+          })
+        } else {
+          console.error((error) => `Error: ${error} creating the new beer`)
+        }
+      })
+      .catch((error) => {
+        console.error("Error creating the new beer", error)
+      })
+  }
+
   return (
-    <div>NewBeer</div>
+    <div>
+      <form className="px-8 pt-16" onSubmit={handleSubmit} autoComplete='off'>
+        <div className="relative z-0 w-full mb-6 group">
+          <input type="text" name="name" id="name" value={formData.name} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
+          <input type="text" name="tagline" id="tagline" value={formData.tagline} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <label htmlFor="tagline" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Tagline</label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
+          <textarea rows="6" type="text" name="description" id="description" value={formData.description} onChange={handleChange} className="resize-none block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <label htmlFor="description" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Description</label>
+        </div>
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <div className="relative z-0 w-full mb-6 group">
+            <input type="text" name="first_brewed" id="first_brewed" value={formData.first_brewed} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label htmlFor="first_brewed" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First brewed</label>
+          </div>
+          <div className="relative z-0 w-full mb-6 group">
+            <input type="text" name="brewers_tips" id="brewers_tips" value={formData.brewers_tips} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label htmlFor="brewers_tips" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Brewers tips</label>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <div className="relative z-0 w-full mb-6 group">
+            <input type="number" name="attenuation_level" id="attenuation_level" value={formData.attenuation_level} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label htmlFor="attenuation_level" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Attenuation Level</label>
+          </div>
+          <div className="relative z-0 w-full mb-6 group">
+            <input type="text" name="contributed_by" id="contributed_by" value={formData.contributed_by} onChange={handleChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label htmlFor="contributed_by" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Contributed by</label>
+          </div>
+        </div>
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 mt-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">ADD NEW</button>
+      </form>
+    </div>
   )
 }
 
