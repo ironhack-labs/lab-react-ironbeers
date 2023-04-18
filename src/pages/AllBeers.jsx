8fb3a5633f1e03c1ec48d141d4dee5fc20b29861
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-18 22:06:36 +0200

    Solve bonus iteration 7

--- src/pages/AllBeers.jsx
+++ src/pages/AllBeers.jsx
@@ -3,24 +3,42 @@ import Header from '../components/Header';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 const AllBeers = () => {
 	const [allBeers, setAllBeers] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
 
 	useEffect(() => {
 		axios
-			.get(`https://ih-beers-api2.herokuapp.com/beers`)
+			.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${searchTerm}`)
 			.then((res) => {
 				setAllBeers(res.data);
 			})
 			.catch((err) => {
 				console.error(err);
 			});
-	}, []);
+	}, [searchTerm]);
+
+	const handleSearch = (event) => {
+		setSearchTerm(event.target.value);
+	};
 
 	return (
 		<div className=''>
 			<Header />
+
+			<div className='form-group p-5'>
+				<label htmlFor='search'> Search Beer</label>
+				<input
+					className='form-control'
+					type='search'
+					name='search'
+					id='search'
+					value={searchTerm}
+					onChange={handleSearch}
+				/>
+			</div>
 
 			{allBeers.map((beer) => {
 				return (
