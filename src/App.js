0597b490700cb7bc7c230e7c2a02fd71ae547643
brea-practice <ihk.gitcomm@gmail.com>
2023-04-19 17:16:47 +0200

    Iteration 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import HomePage from './components/HomePage';
 import AllBeers from './components/AllBeers';
+import Beer from './components/Beer';
 
 import { Route, Routes } from 'react-router-dom';
 
@@ -9,8 +10,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/beers" element={<AllBeers />}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/beers" element={<AllBeers />} />
+        <Route path="/beer/:beerID" element={<Beer />} />
         <Route path="/random-beer" />
         <Route path="/new-beer" />
       </Routes>
---
+++ src/components/Beer.js
@@ -0,0 +1,44 @@
+import { useParams } from "react-router-dom";
+import Header from "./Header";
+
+import { useEffect, useState } from "react";
+
+function Beer() {
+    const [beer, setBeer] = useState(null)
+
+    const { beerID } = useParams();
+
+    useEffect(() => {
+        fetch("https://ih-beers-api2.herokuapp.com/beers/" + beerID)
+            .then(response => response.json())
+            .then(data => setBeer(data))
+            .catch(error => console.log(error))
+    }, [beerID])
+
+    return(
+        <>
+            <Header />
+            {beer !== null ? 
+                <div className="singleBeerDiv">
+                    <div className="singleBeerImageDiv">
+                        <img src={beer.image_url} alt="" />
+                    </div>
+                    <div className="singleBeerTextDiv">
+                        <div className="firstRowText">
+                            <h1>{beer.name}</h1>
+                            <h1 className="attenuation">{beer.attenuation_level}</h1>
+                        </div>
+                        <div className="secondRowText">
+                            <h3 className="tagline">{beer.tagline}</h3>
+                            <b><p>{beer.first_brewed}</p></b>
+                        </div>
+                        <p className="description">{beer.description}</p>
+                        <h4 className="contributedBy">{beer.contributed_by}</h4>
+                    </div>
+                </div>
+            : <></>}
+        </>
+    )
+}
+
+export default Beer;
