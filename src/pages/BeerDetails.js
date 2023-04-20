ecf3fd1db192201e4d1959df0a1f5413764ad0f0
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-04-20 15:29:31 +0200

    Iteration 5 - done

--- src/pages/BeerDetails.js
+++ src/pages/BeerDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import axios from "axios"
 import Header from '../components/Header';
 
