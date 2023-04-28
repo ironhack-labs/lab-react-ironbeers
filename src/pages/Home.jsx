1dd571995c4c440a20ca3ee13f503ebf2c3b13b0
michael07200 <mickmhtong@gmail.com>
2023-04-28 21:28:34 +0200

    fix: Iteration 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,39 @@
 import React from "react";
-import image from "../assets/beers.png";
+import { Link } from "react-router-dom";
+import Imgbeers from "../assets/beers.png";
+import ImgRandomBeer from "../assets/random-beer.png";
+import ImgNewbeer from "../assets/new-beer.png";
 
 const Home = () => {
   return (
     <div>
-      <h1>All Beer</h1>
-      <img src={image} alt="random-beer" />
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur,
-        porro dolores. Maxime fuga consequatur quisquam nostrum, voluptates
-        vitae odio porro!
-      </p>
+      <Link to="/beers">
+        <h1>All Beer</h1>
+        <img src={Imgbeers} alt="beers" />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur,
+          porro dolores. Maxime fuga consequatur quisquam nostrum, voluptates
+          vitae odio porro!
+        </p>
+      </Link>
+      <Link to="/random-beer">
+        <h1>New Beer</h1>
+        <img src={ImgRandomBeer} alt="random-beer" />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur,
+          porro dolores. Maxime fuga consequatur quisquam nostrum, voluptates
+          vitae odio porro!
+        </p>
+      </Link>
+      <Link to="/new-beer">
+        <h1>RandomBeer</h1>
+        <img src={ImgNewbeer} alt="new-beer" />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur,
+          porro dolores. Maxime fuga consequatur quisquam nostrum, voluptates
+          vitae odio porro!
+        </p>
+      </Link>
     </div>
   );
 };
