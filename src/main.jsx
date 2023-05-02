7add1fb3012638def230f48ed1c2651c2ede9db4
ross-u <cirkovic.uros.srb@gmail.com>
2023-05-02 20:32:41 +0200

    feat: update starter code

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
