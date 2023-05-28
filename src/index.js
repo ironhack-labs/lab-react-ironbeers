48e5e6d6ec57ea4de37d0a69fc0f8db657ce1fb0
Lucas Telles <lltellesss@gmail.com>
2023-05-28 11:09:49 -0300

    lab done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-no-undef */
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>
 );
 
