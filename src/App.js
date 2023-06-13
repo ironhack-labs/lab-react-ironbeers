f1f3ee6e477b91bef602a584f81389d1f7b31916
Wiebke Lueneburg <git@fraulueneburg.de>
2023-06-13 21:00:41 +0200

    finished iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Link, Outlet, Route, Routes } from 'react-router-dom'
+import imgAllBeers from './assets/beers.png'
+import imgRandomBeer from './assets/random-beer.png'
+import imgNewBeer from './assets/new-beer.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function PageLayout() {
+	return (
+		<div>
+			Navbar
+			<main>
+				<Outlet />
+			</main>
+		</div>
+	)
 }
 
-export default App;
+function Home() {
+	return (
+		<div>
+			<Link to="/beers">
+				<img src={imgAllBeers} alt="" />
+				<h2>All Beers</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
+					sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
+					pellentesque eu, pretium quis, sem
+				</p>
+			</Link>
+			<Link to="/random-beer">
+				<img src={imgRandomBeer} alt="" />
+				<h2>Random Beer</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
+					sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
+					pellentesque eu, pretium quis, sem
+				</p>
+			</Link>
+			<Link to="/new-beer">
+				<img src={imgNewBeer} alt="" />
+				<h2>New Beer</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
+					sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
+					pellentesque eu, pretium quis, sem
+				</p>
+			</Link>
+		</div>
+	)
+}
+
+function App() {
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<PageLayout />}>
+					<Route path="/" element={<Home />} />
+					<Route path="/beers" element="" />
+					<Route path="/random-beer" element="" />
+					<Route path="/new-beer" element="" />
+					<Route path="*" element={<h1>This page does not exist</h1>} />
+				</Route>
+			</Routes>
+		</div>
+	)
+}
+
+export default App
