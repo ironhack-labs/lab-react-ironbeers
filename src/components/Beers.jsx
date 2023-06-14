3d26846af8b19c6314112c1058942bc47ee24ca8
RonnySk <ronny_sk@hotmail.com>
2023-06-14 09:35:48 +0200

    bonus iteration not done

--- src/components/Beers.jsx
+++ src/components/Beers.jsx
@@ -1,44 +1,51 @@
-import { Link } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Beers(props) {
-	console.log(props.beers)
-	const query = ''
+  const [query, setQuery] = useState("");
 
-	const [beers, setBeers] = useState([])
-	useEffect(() => {
-		async function fetchBeers() {
-			const response = await fetch('https://ih-beers-api2.herokuapp.com/beers')
-			const data = await response.json()
-			setBeers([...data])
-		}
-		fetchBeers()
-	}, [])
+  const handleSearch = (event) => {
+    setQuery(event.target.value);
+  };
 
-	const handleChange = (e) => {
-		let searchTerm = e.target.value
+  console.log(query);
 
-		// https://ih-beers-api2.herokuapp.com/beers/search?q={query}
-	}
+  // const [beers, setBeers] = useState([])
+  // useEffect(() => {
+  // 	async function fetchBeers() {
+  // 		const response = await fetch('https://ih-beers-api2.herokuapp.com/beers')
+  // 		const data = await response.json()
+  // 		setBeers([...data])
+  // 	}
+  // 	fetchBeers()
+  // }, [])
 
-	return (
-		<div>
-			<input type="text" value={query} onChange={handleChange} placeholder="search for a beer" />
-			<h1>Beers</h1>
+  // 	// https://ih-beers-api2.herokuapp.com/beers/search?q={query}
+  // }
 
-			{props.beers.map((beer) => {
-				return (
-					<div key={beer._id}>
-						<img src={beer.image_url} alt={beer.name} width="100" />
-						<h2>{beer.name}</h2>
-						<p>{beer.tagline}</p>
-						<small>{beer.contributed_by}</small>
-						<Link to={`/beers/${beer._id}`}>Details</Link>
-					</div>
-				)
-			})}
-		</div>
-	)
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={handleSearch}
+        placeholder="search for a beer"
+      />
+      <h1>Beers</h1>
+
+      {props.beers.map((beer) => {
+        return (
+          <div key={beer._id}>
+            <img src={beer.image_url} alt={beer.name} width="100" />
+            <h2>{beer.name}</h2>
+            <p>{beer.tagline}</p>
+            <small>{beer.contributed_by}</small>
+            <Link to={`/beers/${beer._id}`}>Details</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Beers
+export default Beers;
