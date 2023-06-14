2e9c494b1ce36c6de3e2767e2327a88c005de79e
Lena Imdahl <mail@lenaimdahl.de>
2023-06-14 20:43:49 +0200

    added homepage and links

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import beers from "../assets/beers.png";
+import randomBeer from "../assets/random-beer.png";
+import newBeer from "../assets/new-beer.png";
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Home</h1>
+
+      <Link to="/beers">All Beers</Link>
+      <img src={beers} alt="" className="page-img" />
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </p>
+      <Link to="/randombeer">Random Beer</Link>
+      <img src={randomBeer} alt="" className="page-img" />
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </p>
+      <Link to="/newbeer">New Beer</Link>
+      <img src={newBeer} alt="" className="page-img" />
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </p>
+    </div>
+  );
+}
+
+export default Homepage;
