db1a551c73150ed312fcea4ae14162ff2a45efaf
Wiebke Lueneburg <git@fraulueneburg.de>
2023-06-14 09:18:43 +0200

    try at iteration 7 bonus

--- src/components/Beers.jsx
+++ src/components/Beers.jsx
@@ -1,10 +1,29 @@
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 function Beers(props) {
 	console.log(props.beers)
+	const query = ''
+
+	const [beers, setBeers] = useState([])
+	useEffect(() => {
+		async function fetchBeers() {
+			const response = await fetch('https://ih-beers-api2.herokuapp.com/beers')
+			const data = await response.json()
+			setBeers([...data])
+		}
+		fetchBeers()
+	}, [])
+
+	const handleChange = (e) => {
+		let searchTerm = e.target.value
+
+		// https://ih-beers-api2.herokuapp.com/beers/search?q={query}
+	}
 
 	return (
 		<div>
+			<input type="text" value={query} onChange={handleChange} placeholder="search for a beer" />
 			<h1>Beers</h1>
 
 			{props.beers.map((beer) => {
