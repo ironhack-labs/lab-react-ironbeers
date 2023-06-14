9ec8a66e6da80b21a2c2a903f9d75daf9ec6b5c8
“Zuzana <zuzana@getyourguide.com>
2023-06-14 08:13:16 +0200

    Iteration 5 unfinished

--- src/pages/NewBeer.jsx
+++ src/pages/NewBeer.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -12,27 +11,31 @@ function NewBeer() {
   const [attenuationLevel, setAttenuationLevel] = useState(0);
   const [contributedBy, setContributedBy] = useState("");
 
-  const submitHandler = (event) => {
+  
+
+  const submitHandler =  (event) => {
     event.preventDefault();
+    const newBeer = {
+      name,
+      tagline,
+      description,
+      first_brewed: firstBrewed,
+      brewers_tips: brewersTips,
+      attenuation_level: attenuationLevel,
+      contributed_by: contributedBy,
+    }
+    axios.post("https://ih-beers-api2.herokuapp.com/beers/new", newBeer);
+    console.log("New product added", newBeer);
 
-    const [newBeer, setNewBeer] = useState({
-      name: "",
-      tagline: "", 
-      description, 
-      first_brewed : "" ,
-      brewers_tips : "",
-      attenuation_level: "", 
-      contributed_by : ""
-    })
-
-      const pushBeer = async () => {
-        const { data } = await axios.post("https://ih-beers-api2.herokuapp.com/beers/new", newBeer);
-        
-      };
-      pushBeer();
-      
-
+    setName("");
+    setTagline("");
+    setDescription("");
+    setFirstBrewed("");
+    setBrewersTips("");
+    setAttenuationLevel(0);
+    setContributedBy("");
   }
+
 
   return (
     <div>
@@ -40,32 +43,68 @@ function NewBeer() {
         <Link to={"/"}>Back to Home</Link>
       </header>
 
-    <form onSumbit={submitHandler}>
-      <label>name : </label>
-      <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/>
+      <form onSubmit={submitHandler}>
+        <label>name : </label>
+        <input
+         
+          type="text"
+          value={name}
+          onChange={(e) => {setName(e.target.value)}}
+        />
 
-      <label>tagline : </label>
-      <input type="text" value={tagline} onChange={(e)=>{setTagline(e.target.value)}}/>
+        <label>tagline : </label>
+        <input
+          name="tagline"
+          type="text"
+          value={tagline}
+          onChange={(e) => {setTagline(e.target.value);}}
+        />
 
-      <label>description : </label>
-      <input type="text" value={description} onChange={(e)=>{setDescription(e.target.value)}}/>
+        <label>description : </label>
+        <input
+          name="description"
+          type="text"
+          value={description}
+          onChange={(e) => {setDescription(e.target.value);}}
+        />
 
-      <label>firstBrewed : </label>
-      <input type="text" value={firstBrewed} onChange={(e)=>{setFirstBrewed(e.target.value)}}/>
+        <label>firstBrewed : </label>
+        <input
+          name="first_brewed"
+          type="text"
+          value={firstBrewed}
+          onChange={(e) => {setFirstBrewed(e.target.value);}}
+        />
 
-      <label>brewersTips : </label>
-      <input type="text" value={brewersTips} onChange={(e)=>{setBrewersTips(e.target.value)}}/>
+        <label>brewersTips : </label>
+        <input
+          name="brewers_tips"
+          type="text"
+          value={brewersTips}
+          onChange={(e) => {setBrewersTips(e.target.value)}}
+        />
 
-      <label>attenuationLevel : </label>
-      <input type="number" value={attenuationLevel} onChange={(e)=>{setAttenuationLevel(e.target.value)}}/>
+        <label>attenuationLevel : </label>
+        <input
+          name="attenuation_level"
+          type="number"
+          value={attenuationLevel}
+          onChange={(e) => {
+            setAttenuationLevel(e.target.value);
+          }}
+        />
 
-      <label>contributedBy : </label>
-      <input type="text" value={contributedBy} onChange={(e)=>{setContributedBy(e.target.value)}}/>
+        <label>contributedBy : </label>
+        <input name="contributed_by"
+          type="text"
+          value={contributedBy}
+          onChange={(e) => {
+            setContributedBy(e.target.value);
+          }}
+        />
 
-
-      <button type="submit">Submit</button>
-    </form>
-
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
