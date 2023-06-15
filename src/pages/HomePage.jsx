8154595f321c5cc75089f835117fa956fc4b3c15
pepealacid <ppalacid@gmail.com>
2023-06-15 12:52:49 +0200

    started

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+const HomePage = () => {
+
+const API_URL = 'https://ih-beers-api2.herokuapp.com/beers'
+
+    return(
+        <div>
+            <div className='home-card'>
+                <Link to={'/beers'}>
+                    <h1>All Beers</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, provident. Libero perspiciatis a vero quis voluptatum dolore unde veniam deserunt itaque ex, corporis fuga eos vitae eum quibusdam, accusantium ipsa.</p>
+                </Link>
+            </div>
+
+            <div className='home-card'>
+                <Link to={'/random-beer'}>
+                    <h1>Random Beer</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, provident. Libero perspiciatis a vero quis voluptatum dolore unde veniam deserunt itaque ex, corporis fuga eos vitae eum quibusdam, accusantium ipsa.</p>
+                </Link>
+            </div>
+
+            <div className='home-card'>
+                <Link to={'/new-beer'}>
+                    <h1>New Beer</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, provident. Libero perspiciatis a vero quis voluptatum dolore unde veniam deserunt itaque ex, corporis fuga eos vitae eum quibusdam, accusantium ipsa.</p>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
