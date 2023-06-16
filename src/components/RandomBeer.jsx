9864fec8d4e3871530e4b8d4ffec9b245b6b3242
RonnySk <ronny_sk@hotmail.com>
2023-06-16 21:34:35 +0200

    CSS RadomBeer Page

--- src/components/RandomBeer.jsx
+++ src/components/RandomBeer.jsx
@@ -1,27 +1,28 @@
 function Beer({ randomBeer }) {
-	return (
-		<div>
-			<h1>{randomBeer.name}</h1>
-			<h2>{randomBeer.tagline}</h2>
-			<img src={randomBeer.image_url} alt={randomBeer.name} />
-			<p>
-				<strong>first brewed:</strong>
-				{randomBeer.first_brewed}
-			</p>
-			<p>
-				<strong>attenuation level:</strong>
-				{randomBeer.attenuation_level}
-			</p>
-			<p>
-				<strong>Description:</strong>
-				{randomBeer.description}
-			</p>
-			<p>
-				<strong>contributed by:</strong>
-				{randomBeer.contributed_by}
-			</p>
-		</div>
-	)
+  console.log("random beer", randomBeer);
+  return (
+    <div className="oneBeer">
+      <img
+        className="beerImg"
+        src={randomBeer.image_url}
+        alt={randomBeer.name}
+      />
+      <div className="oneBeerName">
+        <h1>{randomBeer.name}</h1>
+        <p>{randomBeer.attenuation_level}</p>
+      </div>
+      <div className="oneBeerTagLine">
+        <h2>{randomBeer.tagline}</h2>
+        <p>{randomBeer.first_brewed}</p>
+      </div>
+      <div className="oneBeerdescription">
+        <p>{randomBeer.description}</p>
+      </div>
+      <div className="oneBeerContributedBy">
+        <p>{randomBeer.contributed_by}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Beer
+export default Beer;
