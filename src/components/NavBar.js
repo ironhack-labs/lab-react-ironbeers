31105f2a70578b60bd3f241be84dc97f438d42c5
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-23 17:45:01 +0200

    navbar responsive

---
+++ src/components/NavBar.js
@@ -0,0 +1,40 @@
+import { Container, Navbar, Nav, Form, Button, Switch } from "react-bootstrap";
+import { Link } from "react-router-dom";
+export const NavBar = () => {
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="sm"
+      bg="dark"
+      data-bs-theme="dark"
+      className="NavBar"
+    >
+      <Container>
+        <Navbar.Brand href="/">IronBeer</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to={"/"}>
+              All Beers
+            </Nav.Link>
+            <Nav.Link as={Link} to={"/random"}>
+              Random Beer
+            </Nav.Link>
+            <Nav.Link as={Link} to={"/new"}>
+              New Beer
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
