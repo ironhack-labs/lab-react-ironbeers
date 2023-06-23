817c54a82f577d830a4a03ef4e3f63124bd37a3b
Philippe Miossec <pmiossec@gmail.com>
2023-06-23 16:37:07 +0200

    Iteration 3 | List all the beers

--- src/pages/Beers.js
+++ src/pages/Beers.js
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Beers() {
+
+    const [beers, setBeers] = useState([]);
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_URL}/beers`)
+        .then(response => setBeers(response.data))
+        .catch(console.log);
+    });
+
     return(
-        <Header />
+        <>
+            <Header />
+
+            <div id="beers">
+            {beers.map(b => <article>
+
+                <div className="beer">
+                    <Link to={"/beers/" + b._id}>
+                        <img src={b.image_url} alt={b.name} />
+                    </Link>
+                    <div className="infos">
+                        <p className="name">{b.name}</p>
+                        <p className="tagline">{b.tagline}</p>
+                        <p className="created-by"><b>Created by: </b>{b.contributed_by}</p>
+                    </div>
+                </div>
+                <hr/>
+            </article>)}
+            </div>
+        </>
     )
 }
