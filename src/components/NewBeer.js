6ca6a1c0f54e78ecc406ddc09b080aa4e18025c7
Jani Ojala <jani@stptrade.com>
2023-06-24 23:08:22 +0200

    Iteration 6 | Create a new beer

--- src/components/NewBeer.js
+++ src/components/NewBeer.js
@@ -1,11 +1,147 @@
 import Header from "./Header";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function NewBeer() {
+  const [name, setName] = useState("");
+  const [tagline, setTagline] = useState("");
+  const [description, setDescription] = useState("");
+  const [first_brewed, setFirst_Brewed] = useState("");
+  const [brewers_tips, setBrewers_Tips] = useState("");
+  const [attenuation_level, setAttentuationLevel] = useState("");
+  const [contributed_by, setContributed_By] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBeer = {
+      name: name,
+      tagline: tagline,
+      description: description,
+      first_brewed: first_brewed,
+      brewers_tips: brewers_tips,
+      attenuation_level: attenuation_level,
+      contributed_by: contributed_by,
+    };
+    createBeer(newBeer);
+  };
+
+  const createBeer = (newBeer) => {
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/beers/new`, newBeer)
+
+      .then((response) => {
+        console.log(response.data);
+        navigate("/beers");
+      })
+      .catch((e) => console.log(e));
+  };
+
   return (
-    <div>
+    <section>
       <Header />
-      <h1>Create new Beer!</h1>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="enter name of beer"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+          </label>
+        </p>
+
+        <p>
+          <label>
+            Tagline
+            <input
+              type="text"
+              name="tagline"
+              value={tagline}
+              onChange={(e) => {
+                setTagline(e.target.value);
+              }}
+            />
+          </label>
+        </p>
+
+        <label>
+          Description
+          <input
+            type="text"
+            name="description"
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+        </label>
+
+        <p>
+          <label>
+            First Brewed
+            <input
+              type="text"
+              name="first_brewed"
+              value={first_brewed}
+              onChange={(e) => {
+                setFirst_Brewed(e.target.value);
+              }}
+            />
+          </label>
+        </p>
+
+        <p>
+          <label>
+            Brewer's tips
+            <input
+              type="text"
+              name="brewers_tips"
+              value={brewers_tips}
+              onChange={(e) => {
+                setBrewers_Tips(e.target.value);
+              }}
+            />
+          </label>
+        </p>
+
+        <p>
+          <label>
+            Attentuation Level
+            <input
+              type="number"
+              name="attentuation_level"
+              value={attenuation_level}
+              onChange={(e) => {
+                setAttentuationLevel(e.target.value);
+              }}
+            />
+          </label>
+        </p>
+
+        <p>
+          <label>
+            Contributed by
+            <input
+              type="text"
+              name="contributed_by"
+              value={contributed_by}
+              onChange={(e) => {
+                setContributed_By(e.target.value);
+              }}
+            />
+          </label>
+        </p>
+
+        <button onClick={createBeer}>Create</button>
+      </form>
+    </section>
   );
 }
 
