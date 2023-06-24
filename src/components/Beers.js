2a0cdd498c43f25a1a41dda19ea78c3a14480a06
Jani Ojala <jani@stptrade.com>
2023-06-24 20:53:33 +0200

    Iteration #1 DONE

---
+++ src/components/Beers.js
@@ -0,0 +1,5 @@
+function Beers() {
+  return <h1>This is the Beers page</h1>;
+}
+
+export default Beers;
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>This is the Header page</h1>;
+}
+
+export default Header;
