bc4716759e9797ddaea2720a2c72facb4292bf85
ProgrammingDj <djamelpettes@gmail.com>
2023-06-24 14:38:05 +0200

    Iteration 3, api loaded

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
+import axios from "axios";
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ListBeers from './components/ListBeers';
 import NewBeer from './components/NewBeer';
 import RandomBeer from './components/RandomBeers';
 import SingleBeers from './components/SingleBeers';
+import { useEffect, useState } from "react";
+
 
 function App() {
+
+  const [appArray, setAppArray] = useState();
+
+
+
+  const baseURL= "https://ih-beers-api2.herokuapp.com/beers";
+  
+  useEffect(() => {
+    getBeerAPI();
+  },[]);
+  
+  const getBeerAPI = () => {
+    axios.get(`${baseURL}`)
+    .then(response => {
+      setAppArray(response.data)})
+      .catch(e => console.log(e));
+    };
+  
+
   return (
     <Router>
       <div className='totalframe'>
@@ -21,7 +43,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/beers" element={<ListBeers />} />
+          <Route path="/beers" element={<ListBeers listBeers={appArray}  />} />
           <Route path="/beers/new" element={<NewBeer />} />
           <Route path="/beers/search?q={query}" element={<RandomBeer />} />
           <Route path="/beers/:id" element={<SingleBeers />} />
--- src/components/ListBeers.js
+++ src/components/ListBeers.js
@@ -1,5 +1,28 @@
-function ListBeers() {
+import { Link } from "react-router-dom"
+
+function ListBeers(props) {
+
     <p>This is ListBeers</p>
+
+    if (props.listBeers === undefined || props.listBeers === null) {
+        console.log("This is it:" + props.listBeers)
+        return <p>taking a sipp of the Beers... moment, i will be right there. </p>
+    } else {
+        return props.listBeers.map((beerValue) => {
+            console.log("This is it:" + beerValue)
+            return (
+                <div key={beerValue.id} className="beerbox">
+                   <Link to={`/beers/${beerValue._id}`}>
+                    <img src={beerValue.image_url} alt={beerValue.name} width="33px" height="100px"></img>
+                    {beerValue.name} <br/>
+                    {beerValue.tagline} <br/>
+                    {beerValue.contributed_by} <br/>
+                </Link>
+                </div>
+            )
+        })
+    }
+
 }
 
 export default ListBeers;
