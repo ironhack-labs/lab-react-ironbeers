2eafda2efe741619f4f680dd8270c37f04b2da8e
Fernando Ramos <fndo.ramos@gmail.com>
2023-06-24 19:14:11 +0200

    add iteration 6

--- src/pages/NewBeer.js
+++ src/pages/NewBeer.js
@@ -1,11 +1,121 @@
+import { useState } from "react";
 import Header from "../components/Header";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function NewBeer() {
-    return (
-        <>
-        <Header />
-        </>
-    )
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({});
+
+  const handleOnchange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+    console.log(inputs);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/new`, inputs)
+      .then((message) => {
+        console.log(message);
+        navigate("/beers");
+      })
+      .catch((e) => console.log("Error creating new beer in the API", e));
+  };
+  return (
+    <>
+      <Header />
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={inputs.name || ""}
+            placeholder="Enter beer name"
+            required={true}
+            onChange={handleOnchange}
+          />
+        </label>
+
+        <label>
+          Tagline
+          <input
+            type="text"
+            name="tagline"
+            value={inputs.tagline || ""}
+            placeholder="Enter tagline"
+            required={true}
+            onChange={handleOnchange}
+          />
+        </label>
+
+        <label>
+          Description
+          <input
+            type="text"
+            name="description"
+            value={inputs.description || ""}
+            placeholder="Enter description"
+            onChange={handleOnchange}
+          />
+        </label>
+
+        <label>
+          First Brewed
+          <input
+            type="text"
+            name="first_brewed"
+            value={inputs.first_brewed || ""}
+            placeholder="Enter date"
+            onChange={handleOnchange}
+          />
+        </label>
+
+        <label>
+          Brewers Tips
+          <input
+            type="text"
+            name="brewers_tips"
+            value={inputs.brewers_tips || ""}
+            placeholder="Enter tips"
+            onChange={handleOnchange}
+          />
+        </label>
+
+        <label>
+          Attenuation Level
+          <input
+            type="number"
+            min={10}
+            name="attenuation_level"
+            value={inputs.attenuation_level || ""}
+            placeholder="10"
+            onChange={handleOnchange}
+          />
+        </label>
+
+        <label>
+          Contributed By
+          <input
+            type="text"
+            name="contributed_by"
+            value={inputs.contributed_by || ""}
+            placeholder="Enter contributor"
+            onChange={handleOnchange}
+          />
+        </label>
+
+        <button>Create</button>
+      </form>
+    </>
+  );
 }
 
 export default NewBeer;
--- src/pages/RandomBeer.js
+++ src/pages/RandomBeer.js
@@ -1,9 +1,45 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+
 
 function RandomBeer() {
+
+    const [randomBeer, setRandomBeer] = useState(null)
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_URL}/random`)
+            .then(response => setRandomBeer(response.data))
+            .catch(e => console.log("Error getting random beer from the API", e))
+    }, [])
+
+
     return (
         <>
         <Header />
+        {randomBeer ? (
+        <div>
+          <div>
+            {randomBeer.image_url && (
+              <img src={randomBeer.image_url} alt={randomBeer.name} />
+            )}
+          </div>
+          <div>
+            <h1>{randomBeer.name}</h1>
+            <h2>{randomBeer.tagline}</h2>
+            <h2>{randomBeer.first_brewed}</h2>
+            <h2>{randomBeer.attenuation_level}</h2>
+            <p>{randomBeer.description}</p>
+            <p>Created by: {randomBeer.contributed_by}</p>
+            <Link to={`/`}>Back</Link>
+            <hr />
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
         </>
     )
 }
