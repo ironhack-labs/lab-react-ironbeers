b12849cd96b0c6dc3da4d1ecdf057ae34b8d25bd
Filipe Ferreira <filipeferreira94@gmail.com>
2023-06-25 16:48:17 +0100

    Added loading spinner to beer list and details

--- src/components/BeerDetails.js
+++ src/components/BeerDetails.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
+import Spinner from 'react-bootstrap/Spinner';
+
 import { useParams } from 'react-router-dom';
 
 function BeerDetails() {
 	const { id } = useParams();
-	const [beer, setBeer] = useState([]);
+	const [beer, setBeer] = useState(null);
 
 	useEffect(() => {
 		axios
@@ -19,34 +21,49 @@ function BeerDetails() {
 			});
 	}, []);
 
-	return (
-		<>
-			<div className="d-flex flex-wrap align-items-center justify-content-center">
-				<Card
-					className="m-3 container p-3 align-items-center justify-content-center"
-					style={{ width: '18rem', minHeight: '18rem' }}
-				>
-					<Card.Img className="img-fluid" variant="top" src={beer.image_url} />
-					<Card.Body>
-						<Card.Title className="fw-bold">{beer.name}</Card.Title>
-						<Card.Text>{beer.tagline}</Card.Text>
-						<Card.Text>
-							<span className="fw-bold">First Brewed:</span> {beer.first_brewed}
-						</Card.Text>
-						<Card.Text>
-							<span className="fw-bold">Attenuation Level:</span>{' '}
-							{beer.attenuation_level}
-						</Card.Text>
-						<Card.Text>{beer.description}</Card.Text>
-						<Card.Text>
-							<span className="fw-bold">Created by:</span>{' '}
-							{beer.contributed_by.split('<')[0]}
-						</Card.Text>
-					</Card.Body>
-				</Card>
+	if (!beer) {
+		return (
+			<div className="container-fluid d-flex justify-content-center align-items-center vh-100">
+				<Spinner animation="border" variant="danger" role="status">
+					<span className="visually-hidden">Loading...</span>
+				</Spinner>
 			</div>
-		</>
-	);
+		);
+	} else {
+		return (
+			<>
+				<div className="d-flex flex-wrap align-items-center justify-content-center">
+					<Card
+						className="m-3 container p-3 align-items-center justify-content-center"
+						style={{ width: '18rem', minHeight: '18rem' }}
+					>
+						<Card.Img
+							className="img-fluid"
+							variant="top"
+							src={beer.image_url}
+						/>
+						<Card.Body>
+							<Card.Title className="fw-bold">{beer.name}</Card.Title>
+							<Card.Text>{beer.tagline}</Card.Text>
+							<Card.Text>
+								<span className="fw-bold">First Brewed:</span>{' '}
+								{beer.first_brewed}
+							</Card.Text>
+							<Card.Text>
+								<span className="fw-bold">Attenuation Level:</span>{' '}
+								{beer.attenuation_level}
+							</Card.Text>
+							<Card.Text>{beer.description}</Card.Text>
+							<Card.Text>
+								<span className="fw-bold">Created by:</span>{' '}
+								{beer.contributed_by.split('<')[0]}
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</div>
+			</>
+		);
+	}
 }
 
 export default BeerDetails;
--- src/components/ListBeers.js
+++ src/components/ListBeers.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 import { Card, Button } from 'react-bootstrap';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
 
 function ListBeers() {
-	const [beerList, setBeerList] = useState([]);
+	const [beerList, setBeerList] = useState(null);
 
 	useEffect(() => {
 		axios
@@ -18,52 +19,62 @@ function ListBeers() {
 			});
 	}, []);
 
-	return (
-		<>
-			<h2 className="m-4">
-				Discover a world of flavors and aromas as you explore our carefully
-				curated list of beers. Click on a card for more details!
-			</h2>
-			<div className="d-flex flex-wrap align-items-center justify-content-center">
-				{beerList.map((element, index) => {
-					return (
-						<Link
-							to={`/beer/${element._id}`}
-							className="text-decoration-none"
-							key={'beer ' + index}
-						>
-							<Card
-								className="m-3 container p-3 align-items-center justify-content-center"
-								style={{ width: '18rem', minHeight: '18rem' }}
-							>
-								<div className="d-flex align-items-center row no-gutters">
-									<div className="col-4">
-										<Card.Img
-											className="img-fluid"
-											variant="top"
-											src={element.image_url}
-										/>
-									</div>
-									<div className="col-8 d-flex align-items-center">
-										<Card.Body>
-											<Card.Title className="fw-bold">
-												{element.name}
-											</Card.Title>
-											<Card.Text>{element.tagline}</Card.Text>
-											<Card.Text>
-												<span className="fw-bold">Created by:</span>{' '}
-												{element.contributed_by.split('<')[0]}
-											</Card.Text>
-										</Card.Body>
-									</div>
-								</div>
-							</Card>
-						</Link>
-					);
-				})}
+	if (!beerList) {
+		return (
+			<div className="container-fluid d-flex justify-content-center align-items-center vh-100">
+				<Spinner animation="border" variant="danger" role="status">
+					<span className="visually-hidden">Loading...</span>
+				</Spinner>
 			</div>
-		</>
-	);
+		);
+	} else {
+		return (
+			<>
+				<h2 className="m-4">
+					Discover a world of flavors and aromas as you explore our carefully
+					curated list of beers. Click on a card for more details!
+				</h2>
+				<div className="d-flex flex-wrap align-items-center justify-content-center">
+					{beerList.map((element, index) => {
+						return (
+							<Link
+								to={`/beer/${element._id}`}
+								className="text-decoration-none"
+								key={'beer ' + index}
+							>
+								<Card
+									className="m-3 container p-3 align-items-center justify-content-center"
+									style={{ width: '18rem', minHeight: '18rem' }}
+								>
+									<div className="d-flex align-items-center row no-gutters">
+										<div className="col-4">
+											<Card.Img
+												className="img-fluid"
+												variant="top"
+												src={element.image_url}
+											/>
+										</div>
+										<div className="col-8 d-flex align-items-center">
+											<Card.Body>
+												<Card.Title className="fw-bold">
+													{element.name}
+												</Card.Title>
+												<Card.Text>{element.tagline}</Card.Text>
+												<Card.Text>
+													<span className="fw-bold">Created by:</span>{' '}
+													{element.contributed_by.split('<')[0]}
+												</Card.Text>
+											</Card.Body>
+										</div>
+									</div>
+								</Card>
+							</Link>
+						);
+					})}
+				</div>
+			</>
+		);
+	}
 }
 
 export default ListBeers;
