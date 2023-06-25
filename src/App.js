34c06414494e8035bd7939169f40f0fa95ca5f0b
“Gokce <gokceberndt@gmail.com>
2023-06-25 14:48:01 +0200

    iteration 2 done, images/logo not visible

--- src/App.js
+++ src/App.js
@@ -14,25 +14,15 @@ function App() {
   return (
     <div className="App">
 
-      <div>
-        <div>
-        <img src="./assets/beers.png"/>
+      <header>
+        
+        <Link to= "/">
+        <img src="./logo.svg"/>
+        </Link>
 
-        <Link to= "/beers">All Beers</Link>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-       
-        </div>
-        <div>
-        <img src="./assets/random-beer.png" />
-        <Link to= "/random-beer">Random Beer</Link>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div>
-        <img src="./assets/new-beer.png" />
-        <Link to= "/new-beer">New Beer</Link>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-      </div>
+      </header>
+
+      
 
 
       <Routes>
