8b0bef6a2e86d86191b393f10724e68cd4bcb0c4
Juan Lozano <jalozanolopez@gmail.com>
2023-06-26 01:28:34 +0200

    Iteration 3

--- src/components/BeersList.js
+++ src/components/BeersList.js
@@ -8,15 +8,31 @@ function BeersList() {
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_URL}`)
             .then((response) => {
-                console.log(response.data);
+                // console.log(response);
                 return(setBeerList(response.data))
             })
             .catch(e => console.log(e))
     }, [])
 
+    // console.log(beers);
+
     return(
         <div>
-            jjaaa
+            <h1>Beers List</h1>
+            {beers.map((beerObj) => {
+                console.log(beerObj);
+                return(
+                    <div key={beerObj._id}>
+                        <img src={beerObj.image_url}/>
+                        <h1>{beerObj.name}</h1>
+                        <h3>{beerObj.tagline}</h3>
+                        <p>Created by: {beerObj.contributed_by}</p>
+                        <button>Details</button>
+                        <hr></hr>
+                    </div>
+                )
+            })}
+            
         </div>
     )
 }
