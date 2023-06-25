445a813c57f7b58e732a41b11b7eb60e661f3747
Filipe Ferreira <filipeferreira94@gmail.com>
2023-06-25 11:29:14 +0100

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
+//react import
 import { Route, Routes } from 'react-router-dom';
+
+//style import
 import './App.css';
+
+// page import
 import Home from './components/Home';
 import ListBeers from './components/ListBeers';
 import BeerDetails from './components/BeerDetails';
 import RandomBeer from './components/RandomBeer';
 import NewBeer from './components/NewBeer';
+import NavBar from './components/NavBar';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-			</header>
-
 			<Routes>
-				<Route path="/" element={Home} />
-				<Route path="/beer/list" element={ListBeers} />
-				<Route path="/beer/:id" element={BeerDetails} />
-				<Route path="/beer/random" element={RandomBeer} />
-				<Route path="/beer/new" element={NewBeer} />
+				<Route path="/" element={<Home />} />
+				<Route element={<NavBar />}>
+					<Route path="/beer/list" element={<ListBeers />} />
+					<Route path="/beer/:id" element={<BeerDetails />} />
+					<Route path="/beer/random" element={<RandomBeer />} />
+					<Route path="/beer/new" element={<NewBeer />} />
+				</Route>
 			</Routes>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
