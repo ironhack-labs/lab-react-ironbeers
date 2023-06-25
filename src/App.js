54e45b31c8771994c689ee6bd474bb7ff6c34a29
Juan Lozano <jalozanolopez@gmail.com>
2023-06-26 00:46:28 +0200

    commit for .env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import BeersList from './components/BeersList';
 import Header from './components/Header';
 import Homepage from './components/Homepage';
 import { Routes, Route } from 'react-router-dom';
@@ -10,8 +11,9 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='/' element={<Homepage />} />
-        <Route path='/' element={<Homepage />} />
+        <Route path='/beers' element={<BeersList />} />
+        <Route path='/random-beer' element={<Homepage />} />
+        <Route path='/new-beer' element={<Homepage />} />
       </Routes>
     </div>
   );
---
+++ src/components/BeersList.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function BeersList() {
+
+    const [beers, setBeerList] = useState([])
+
+    useEffect(() => {
+        
+    }, [])
+
+    return(
+        <div>
+            jjaaa
+        </div>
+    )
+}
+
+export default BeersList;
