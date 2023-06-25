eebac3de5efc1b5055a5f79dc801c383aeaedbf6
Juan Lozano <jalozanolopez@gmail.com>
2023-06-26 01:09:55 +0200

    commit env 2

--- src/components/BeersList.js
+++ src/components/BeersList.js
@@ -4,9 +4,14 @@ import { useEffect, useState } from "react";
 function BeersList() {
 
     const [beers, setBeerList] = useState([])
-
+    // console.log(typeof(process.env.REACT_APP_API_URL));
     useEffect(() => {
-        
+        axios.get(`${process.env.REACT_APP_API_URL}`)
+            .then((response) => {
+                console.log(response.data);
+                return(setBeerList(response.data))
+            })
+            .catch(e => console.log(e))
     }, [])
 
     return(
