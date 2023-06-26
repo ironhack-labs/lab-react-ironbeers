3d6833eb452c857a6068f66b224dd376bf0b29e5
singa23 <singabe-xd@hotmail.fr>
2023-06-26 20:43:12 +0200

    iteration1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import React from "react";
 import "./App.css";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>LAB | React IronBeers</h1>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/beers" element={<AllBeersPage/>}/>
+      <Route path="/random-beer" element={<RandomBeerPage/>}/>
+      <Route path="/new-beer" element={<AddBeerPage/>}/>
+      <Route path="/beers/:beerId" element={<BeerDetailsPage/>}/>
+    </Routes>
+    </>
   );
 }
 
