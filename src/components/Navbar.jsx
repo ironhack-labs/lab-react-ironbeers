a86c29bdff398e28f4bdc737a7de439fe56e5f81
singa23 <singabe-xd@hotmail.fr>
2023-06-26 21:47:26 +0200

    iteration 2 et 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,18 @@
-function Navbar() {}
+import React from "react";
+import { Link } from "react-router-dom";
+import Home from "../assets/home-icon.png";
+import "../index.css";
+
+function Navbar() {
+  return (
+    <>
+      <Link to="/">
+        <nav className="navbar">
+          <img src={Home} alt="Home" />
+        </nav>
+      </Link>
+    </>
+  );
+}
 
 export default Navbar;
