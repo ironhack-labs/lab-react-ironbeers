f4ecd7dbb1d305988701c05226fd3e3e8223e3da
Rayane <alirayane14@gmail.com>
2023-06-26 21:08:53 +0200

    iteration 1 et 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import AllBeersPage from "./pages/HomePage";
+import RandomBeerPage from "./pages/HomePage";
+import AddBeerPage from "./pages/HomePage";
+import BeerDetailsPage from "./pages/HomePage";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React IronBeers</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/beers" element={<AllBeersPage />} />
+        <Route path="/random-beer" element={<RandomBeerPage />} />
+        <Route path="/new-beer" element={<AddBeerPage />} />
+        <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,63 @@
-function HomePage() {}
+import { NavLink } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <div className="page">
+        <NavLink to="/beers">
+          <img
+            src="https://cdn.pixabay.com/photo/2013/11/12/01/29/bar-209148_1280.jpg"
+            alt=""
+          />
+          <div>
+            <h2>All Beers</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id
+              vehicula nisi. Vivamus sit amet tempus velit. Vestibulum sed
+              egestas turpis. Pellentesque bibendum libero quis facilisis
+              pulvinar. Fusce augue felis.
+            </p>
+          </div>
+        </NavLink>
+      </div>
+
+      <div className="page">
+        <NavLink to="/random-beer">
+          <img
+            src="https://cdn.pixabay.com/photo/2015/09/24/20/36/beer-barrel-956322_640.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Random Beer</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id
+              vehicula nisi. Vivamus sit amet tempus velit. Vestibulum sed
+              egestas turpis. Pellentesque bibendum libero quis facilisis
+              pulvinar. Fusce augue felis.
+            </p>
+          </div>
+        </NavLink>
+      </div>
+
+      <div className="page">
+        <NavLink to="/new-beer">
+          <img
+            src="https://cdn.pixabay.com/photo/2018/05/31/16/51/beer-3444480_640.jpg"
+            alt="beers"
+          />
+          <div>
+            <h2>New Beer</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id
+              vehicula nisi. Vivamus sit amet tempus velit. Vestibulum sed
+              egestas turpis. Pellentesque bibendum libero quis facilisis
+              pulvinar. Fusce augue felis.
+            </p>
+          </div>
+        </NavLink>
+      </div>
+    </div>
+  );
+}
 
 export default HomePage;
