fe56d14af5ad07056ed083fe72943fb0f0d877f5
singa23 <singabe-xd@hotmail.fr>
2023-06-27 20:42:59 +0200

    iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/beers" element={<AllBeersPage />} /> */}
-        {/* <Route path="/random-beer" element={<RandomBeerPage />} />
+        <Route path="/beers" element={<AllBeersPage />} />
+        <Route path="/random-beer" element={<RandomBeerPage />} />
         <Route path="/new-beer" element={<AddBeerPage />} />
-        <Route path="/beers/:beerId" element={<BeerDetailsPage />} /> */}
+        <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
       </Routes>
     </>
   );
--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,41 @@
-function AllBeersPage() {}
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "../index.css";
+
+function AllBeersPage() {
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-beers-api2.herokuapp.com/beers")
+      .then((response) => setBeers(response.data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  return (
+    <div>
+      {beers.map((beer) => (
+        <Link
+          to={`/beers/${beer._id}`}
+          className="beer-container"
+          key={beer._id}
+        >
+          <img className="beer-image" src={beer.image_url} alt={beer.name} />
+          <div className="beer-details">
+            <h2>{beer.name}</h2>
+            <h3>{beer.tagline}</h3>
+            <p>
+              <strong>Created by: </strong>
+              {beer.contributed_by}
+            </p>
+            <hr />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
 
 export default AllBeersPage;
