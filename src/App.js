f66f8aef4272b634bc88aeed4dd783d47755c8c2
Andres Franco <andresfranla@gmail.com>
2023-08-08 12:11:01 -0600

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
 import Beers from './pages/Beers';
 import HomePage from './pages/HomePage';
+import BeerDetail from './pages/BeerDetail';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { baseUrl } from './utils/constants';
 
 function App() {
+  const [beers,setBeers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        const beersData = await (await axios.get(baseUrl + '/')).data
+        setBeers(beersData)
+    }
+    fetchData()
+}, [])
+
+
   return (
-    <div className="App">
-      <Header/>
-      <HomePage/>
-      <Beers/>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/beers' element={<Beers beers={beers}/>}/>
+        <Route path='/beers/:beerId' element={ <BeerDetail beers={beers} /> }/>
+      </Routes>
     </div>
   );
 }
--- src/pages/Beers.jsx
+++ src/pages/Beers.jsx
@@ -1,35 +1,24 @@
-import { useEffect, useState } from "react";
-import axios from 'axios';
-import { baseUrl } from "../utils/constants";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
-function Beers () {
-    const [beers, setBeers] = useState([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const beersData = await (await axios.get(baseUrl + '/')).data
-            setBeers(beersData)
-        }
-        fetchData()
-    }, [])
+function Beers ({beers}) {
 
     return(
         <div>
+            <Header/>
             <h3> List of beers </h3>
             <br />
             {
                 beers.map(beer => (
-                    <div key={beer._id} className="card row">
-                        <div>
-                      <img src={beer.image_url} alt="beer" style={{width: '100px'}}/>
-                        </div>
-
-                        <div>
-                      <h3>{beer.name}</h3>
-                      <h4>{beer.tagline}</h4>
-                      <p><strong>Created by: </strong> {beer.contributed_by} </p>
-                        </div>
-                    </div>
+                    <Link to={`/beers/${beer._id}`} style={{color: 'black', textDecoration: 'none'}}>
+                        <div className="card row" style={{display: "flex", justifyContent: "space-between"}}>
+                            <img src={beer.image_url} alt="beer" style={{width: '100px'}}/>
+                            <h3>{beer.name}</h3>
+                            <h4>{beer.tagline}</h4>
+                            <p><strong>Created by: </strong> {beer.contributed_by} </p>
+                        </div>                    
+                    </Link>
+                    
                   ))                
             }
         </div>
