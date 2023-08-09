3c3ae04b04e1e16e42471e25383d9f5a00694f4e
churrundo <churrundo@gmail.com>
2023-08-08 21:03:01 -0600

    Build Beers page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Beers from "./pages/Beers";
 import NewBeer from "./pages/NewBeer";
 import RandomBeer from "./pages/RandomBeer";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Beers" element={<Beers />} />
       <Route path="/NewBeer" element={<NewBeer />} />
       <Route path="/RandomBeer" element={<RandomBeer />} />
     </Routes>
--- src/pages/Beers.jsx
+++ src/pages/Beers.jsx
@@ -1,9 +1,36 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import baseURL from "../utils/constants.js";
 
-const Beers = () => (
-  <header>
-    <Link to="/">Home</Link>
-  </header>
-);
+const Beers = () => {
+  const [beers, setBeers] = useState([]);
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      console.log("response data:", response.data);
+      setBeers(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <header>
+        <Link to="/">Home</Link>
+      </header>
+      {beers.map((beer) => {
+        return (
+          <div className="beer-card" key={beer._id}>
+            <img src={beer.image_url} alt={beer.name} className="beer-image" />
+            <h2>{beer.name}</h2>
+            <p className="beer-tagline">{beer.tagline}</p>
+            <p className="beer-contributor">
+              Contributed by: {beer.contributed_by}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Beers;
