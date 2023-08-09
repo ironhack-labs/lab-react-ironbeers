adddda1c58b5f0b2c5916b58565014df053a32c3
churrundo <churrundo@gmail.com>
2023-08-08 20:39:19 -0600

    Build Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const Home = () => (
+<div>
+    <section>
+      <img src="/assets/beers.png" alt="Beers" />
+      <Link to="/Beers" className="home-link">Beers</Link>
+    </section>
+
+    <section>
+      <img src="/assets/new-beer.png" alt="New Beer" />
+      <Link to="/NewBeer" className="home-link">New Beer</Link>
+    </section>
+
+    <section>
+      <img src="/assets/random-beer.png" alt="Random Beers" />
+      <Link to="/RandomBeer" className="home-link">Random Beer</Link>
+    </section>
+  </div>);
+
+
+export default Home;
