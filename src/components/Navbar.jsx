e445329bb1f52ef89c8c18dbb84050bce77eb9a3
Irene Buceta <irenebuzalocen@gmail.com>
2023-08-24 20:22:17 +0200

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,13 @@
-function Navbar() {}
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-dark bg-primary mb-3 d-flex justify-content-center">
+            <NavLink to='/'>
+                <img src="src/assets/home-icon.png" alt="icon" />
+            </NavLink>
+        </nav>
+    )
+}
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,27 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+
+
+function HomePage() {
+
+    return (
+        <div className="container">
+            <Link to="/beers">
+                <img src="/src/assets/beers.png" alt="beers" />
+                <h2>All Beers</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, amet! Quaerat, veritatis minus fugiat molestias vel magnam provident. Eius delectus molestiae perspiciatis sequi exercitationem id totam odio voluptatem unde necessitatibus!</p>
+            </Link>
+            < Link to="/random-beer" >
+                <img src="/src/assets/random-beer.png" alt="radom" />
+                <h2>Random Beer</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, amet! Quaerat, veritatis minus fugiat molestias vel magnam provident. Eius delectus molestiae perspiciatis sequi exercitationem id totam odio voluptatem unde necessitatibus!</p>
+            </Link >
+            < Link to="/new-beer" >
+                <img src="/src/assets/new-beer.png" alt="new" />
+                <h2>New Beer</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, amet! Quaerat, veritatis minus fugiat molestias vel magnam provident. Eius delectus molestiae perspiciatis sequi exercitationem id totam odio voluptatem unde necessitatibus!</p>
+            </Link >
+        </div>
+    )
+}
 
 export default HomePage;
