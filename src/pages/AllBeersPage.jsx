cd703554d0ed2ced4aca06db48644890f45864e7
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-08-25 19:30:41 +0100

    done

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,32 @@
-function AllBeersPage() {}
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function AllBeersPage() {
+    const [ beers, setBeers ] = useState([]);
+    
+    useEffect(() => {
+        axios.get("https://ih-beers-api2.herokuapp.com/beers")
+        .then((response) =>{
+            setBeers(response.data)
+        })
+    }, []);
+
+    return(
+        <div>
+            {beers.map((beer) =>{
+                return(
+                    <div key={beer._id}>
+                        <img src={beer.image_url} alt="" />
+                        <h1>{beer.name}</h1>
+                        <p>{beer.tagline}</p>
+                        <p>{beer.contributed_by}</p>
+                        <Link to={`/beers/${beer._id}`}>Beer Details</Link>
+                    </div>
+                )
+            })}
+        </div>
+    );
+}
 
 export default AllBeersPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,13 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+
+function HomePage() {
+    return(
+        <div>
+            <Link to={"/beers"}>List of Beers</Link>
+            <Link to={"/random-beer"}>Random Beers</Link>
+            <Link to={"/new-beer"}>Add a New Beer</Link>
+        </div>
+    );
+}
 
 export default HomePage;
