cfec4c87c17e11d51d113aa58f1f762c2f372298
Pedro Figueiredo <pedro.figueiredo35@gmail.com>
2023-09-17 13:46:18 +0100

    lab solved

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,3 +1,39 @@
-function BeerDetailsPage() {}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { NavLink, useParams } from "react-router-dom";
+
+function BeerDetailsPage() {
+    const {beerId} = useParams()
+    const [beerDetails, setBeerDetails] = useState(null)
+    useEffect(() => {
+        axios
+         .get(`${import.meta.env.VITE_API_URL}/beers/${beerId}`)
+            .then((response)=>{
+                setBeerDetails(response.data)
+            })
+            .catch((e)=>{
+                console.log('failed to get a beer from API',e)
+            })
+         
+    }, [])
+    const renderedBeer = ()=>{ 
+        return (
+            <div>
+                <h2>Name: <p>{beerDetails.name}</p></h2>
+                <img src={beerDetails.image_url} alt="image" />
+                <h3>{beerDetails.tagline}</h3>
+                <h3>Alcohol level:{beerDetails.attenuation_level}</h3>
+                <h3>Brewers tips: {beerDetails.brewers_tips}</h3>
+                <h4>Contributed by: {beerDetails.contributed_by}</h4>
+            </div>
+
+
+        )
+    }
+return( 
+<div>
+    {beerDetails === null ? <p>Loading...</p> : renderedBeer() }
+</div>)
+}
 
 export default BeerDetailsPage;
