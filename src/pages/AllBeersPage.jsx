48e0ed3cf0ff04c5a2c6a8ed922b0fcb3981a1f6
DivyaRaj258 <divya.r258@gmail.com>
2023-09-17 17:20:05 +0200

    solved lab

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,24 @@
-function AllBeersPage() {}
+import "../App";
+import "../App";
+
+function AllBeersPage() {
+    return (
+    <ul>
+  
+    {/* {beers.map((beer) => (
+      <li key={beer._id}>
+        <img src={beer.image_url} alt={beer.name} style={{ maxWidth: '50px' }} />
+        <h2>{beer.name}</h2>
+        <p>{beer.tagline}</p>
+         <p>{beer.contributed_by}</p>
+
+      </li>
+    ))} */}
+    
+  </ul>
+    );
+}
+
+
 
 export default AllBeersPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,12 @@
-function HomePage() {}
+import AllBeers from './AllBeersPage';
+import AddBeer from './AddBeerPage';
+import RandomBeer from './RandomBeerPage';
+
+function HomePage() {
+return (
+<h2>HOME PAGE 123</h2>
+);
+
+}
 
 export default HomePage;
