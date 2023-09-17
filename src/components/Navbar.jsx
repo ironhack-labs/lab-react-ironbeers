736590aef7dd7210c174e4445bb2f678ec67dd75
Alexis Parsat-Lacoste <alexis@parsat-lacoste.com>
2023-09-17 22:36:44 +0200

    Lab done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,8 @@
-function Navbar() {}
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return <Link to="/">Home</Link>;
+}
 
 export default Navbar;
--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,3 +1,34 @@
-function RandomBeersPage() {}
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default RandomBeersPage;
+function RandomBeerPage() {
+  const [beer, setBeer] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+      .then((response) => {
+        setBeer(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      {beer ? (
+        <div>
+          <h1>{beer.name}</h1>
+          <img src={beer.image_url} alt={beer.name} />
+          <p>{beer.tagline}</p>
+          <p>First Brewed: {beer.first_brewed}</p>
+          <p>Attenuation Level: {beer.attenuation_level}</p>
+          <p>Description: {beer.description}</p>
+          <p>Contributed by: {beer.contributed_by}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default RandomBeerPage;
