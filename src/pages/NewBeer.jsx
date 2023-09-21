5af2c83417df3a863497795ff43fbe99889d72ae
Biljana Kotevska <biljanakotev@gmail.com>
2023-09-21 10:30:15 +0200

    added handleSubmit function and axios call to newBeer page

--- src/pages/NewBeer.jsx
+++ src/pages/NewBeer.jsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import '../pages/NewBeer.css';
+import axios from 'axios';
 
 function NewBeer() {
   const [name, setName] = useState('');
@@ -11,9 +12,26 @@ function NewBeer() {
   const [attenuationLevel, setAttenuationLevel] = useState('');
   const [contributedBy, setContributedBy] = useState('');
 
-  function handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-  }
+
+    // Creating the body for the POST request
+    // using the object property value shorthand
+    // values are coming from the state variable
+    const body = { name, tagline, description, firstBrewed, brewersTips, attenuationLevel, contributedBy };
+    console.log(body);
+
+    axios.post(' https://ih-beers-api2.herokuapp.com/beers/new').then((response) => {
+      console.log(response.data);
+      setName('');
+      setTagLine('');
+      setDescription('');
+      setFirstBrewed('');
+      setBrewersTips('');
+      setAttenuationLevel(0);
+      setContributedBy('');
+    });
+  };
 
   return (
     <div>
@@ -26,47 +44,55 @@ function NewBeer() {
             <label className="d-block" htmlFor="name">
               Name
             </label>
-            <input type="text" />
+            <input type="text" name="name" onChange={(e) => setName(e.target.value)} value={name} />
           </div>
+
           <div className="input-container mb-25">
             <label className="d-block" htmlFor="tagline">
               Tagline
             </label>
-            <input type="text" />
+            <input type="text" name="tagline" onChange={(e) => setTagLine(e.target.value)} value={tagline} />
           </div>
+
           <div className="input-container mb-25">
-            <label className="d-block" htmlFor="Description">
+            <label className="d-block" htmlFor="description">
               Description
             </label>
-            <textarea type="text" rows="10" cols="50" />
+            <textarea type="text" rows="10" cols="50" name="description" onChange={(e) => setDescription(e.target.value)} value={description} />
           </div>
+
           <div className="input-container mb-25">
-            <label className="d-block" htmlFor="First Brewed">
+            <label className="d-block" htmlFor="first-brewed">
               First Brewed
             </label>
-            <input type="text" />
+            <input type="text" name="first-brewed" onChange={(e) => setFirstBrewed(e.target.value)} value={firstBrewed} />
           </div>
+
           <div className="input-container mb-25">
-            <label className="d-block" htmlFor="Brewers Tips">
+            <label className="d-block" htmlFor="brewers-tips">
               Brewers Tips
             </label>
-            <input type="text" />
+            <input type="text" name="brewers-tips" onChange={(e) => setBrewersTips(e.target.value)} value={brewersTips} />
           </div>
+
           <div className="input-container mb-25">
-            <label className="d-block" htmlFor="Attenuation Level">
+            <label className="d-block" htmlFor="attenuation-level">
               Attenuation Level
             </label>
-            <input type="number" />
+            <input type="number" name="attenuation-level" onChange={(e) => setAttenuationLevel(e.target.value)} value={attenuationLevel} />
           </div>
+
           <div className="input-container mb-25">
-            <label className="d-block" htmlFor="contributed by">
+            <label className="d-block" htmlFor="contributed-by">
               Contributed by
             </label>
-            <input type="text" />
+            <input type="text" name="contributed-by" onChange={(e) => setContributedBy(e.target.value)} value={contributedBy} />
           </div>
         </div>
+        <div>
+          <button type="submit">Add New</button>
+        </div>
       </form>
-      <button>Add New</button>
     </div>
   );
 }
