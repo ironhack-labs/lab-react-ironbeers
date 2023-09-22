f720c2553249920cc9c0db3285452cd853807b47
Alice <johndoe@example.com>
2023-09-22 17:01:30 +0200

    first done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import AllBeersPage from "./pages/AllBeersPage"
+import BeerDetailsPage from "./pages/BeerDetailsPage"
+import RandomBeerPage from "./pages/RandomBeerPage"
+import AddBeerPage from "./pages/AddBeerPage"
+import axios from "axios";
 
 function App() {
+
+  const [beersAll, setBeersAll] = useState([]);
+  const [fetching, setFetching] = useState(true);
+
+  useEffect(() => {
+    axios.get("https://ih-beers-api2.herokuapp.com/beers").then((response) => {
+      setBeersAll(response.data);
+      setFetching(false);
+    });
+  }, []);
+
+
   return (
     <div className="App">
+      <Navbar />
       <h1>LAB | React IronBeers</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/beers" element={<AllBeersPage />} />
+        <Route path="/random-beer" element={<RandomBeerPage />} />
+        <Route path="/new-beer" element={<AddBeerPage />} />
+        <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
