42c1c43933c1ca3c1da5b290fc94610d046f3971
Alice <johndoe@example.com>
2023-09-22 17:49:54 +0200

    half done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,20 @@
-function Navbar() {}
+import { NavLink } from "react-router-dom";
+import homeIcon from "../assets/home-icon.png"
+
+function Navbar() {
+  return (
+   
+
+<nav className="navbar navbar-dark bg-primary mb-3">
+<NavLink to="/">
+  <div className="navbar">
+    <img src={homeIcon} alt="Home" />
+  </div>
+</NavLink>
+</nav>
+   
+   
+  );
+}
 
 export default Navbar;
--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,51 @@
-function AllBeersPage() {}
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers";
+
+function AllBeersPage() {
+  const [allBeers, setAllBeers] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(API_URL)
+      .then((res) => setAllBeers(res.data))
+      .catch((error) => setError(error));
+  }, []);
+
+  if (!allBeers) return <p>Page is loading... ❤</p>;
+
+  return (
+    <div>
+      <div
+        className="container"
+        style={{ maxHeight: "90vh", overflow: "scroll" }}
+      >
+        <div className="beer-list">
+          {allBeers &&
+            allBeers.map((beer) => {
+              return (
+                <Link
+                  key={beer._id}
+                  className="list-group-item list-group-item-action"
+                  to={`/beers/${beer._id}`}>  
+                  <div className="beer-card"> 
+                 <img src={beer.img_url} alt="beer-picture"/>
+                 <h3>{beer.name}</h3>
+                 <h4>{beer.tagline}</h4>
+                 <p>
+                    <b>Contributed by:{beer.contributed_by}</b>
+                  </p>
+                 </div>
+              </Link>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default AllBeersPage;
