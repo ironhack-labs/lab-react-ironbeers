0ca350768c562b97a66f20b037227c8ef598fe5f
Xavier Ortega <xaviersendra@hotmail.com>
2023-09-24 20:04:18 +0200

    Style

--- src/pages/Beers.jsx
+++ src/pages/Beers.jsx
@@ -3,6 +3,51 @@ import Header from '../components/Header'
 import { useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+
+const BeerListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 20px;
+`;
+
+const BeerCard = styled.div`
+  width: 300px;
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  text-align: center;
+  background-color: #f7f7f7;
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+
+  a {
+    text-decoration: none;
+    color: #333;
+  }
+
+  img {
+    width: 100px;
+    height: auto;
+    margin-bottom: 10px;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  h3 {
+    font-size: 1rem;
+    color: #777;
+  }
+
+  p {
+    font-size: 0.9rem;
+    color: #555;
+  }
+`;
 
 
 function Beers() {
@@ -20,9 +65,11 @@ function Beers() {
     return (
         <>
             <Header></Header>
+            <BeerListContainer>
             {beers.map((oneBeer) => {
                 return (
-                    <div key={oneBeer._id}>
+                    <BeerCard key={oneBeer._id}>
+
                     <Link to={oneBeer._id}>
                         <ul>
                             <img src={oneBeer.image_url} alt="" style={{ width: 60 }} />
@@ -32,9 +79,10 @@ function Beers() {
                         </ul>
                     </Link>
 
-                    </div>
+                    </BeerCard>
                 )
             })}
+            </BeerListContainer>
         </>
     )
 }
--- src/pages/RandomBeer.jsx
+++ src/pages/RandomBeer.jsx
@@ -1,40 +1,73 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const RandomBeerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
+
+const BeerImage = styled.img`
+  width: 200px;
+  height: auto;
+  margin-bottom: 20px;
+`;
+
+const BeerTitle = styled.h2`
+  font-size: 2rem;
+  margin: 10px 0;
+`;
+
+const BeerSubtitle = styled.h3`
+  font-size: 1.5rem;
+  color: #777;
+  margin: 10px 0;
+`;
+
+const BeerDescription = styled.p`
+  font-size: 1rem;
+  color: #555;
+  margin: 10px 0;
+`;
 
 function RandomBeer() {
-  const [beer, setBeer] = useState(null)
-
-
+  const [beer, setBeer] = useState(null);
 
   useEffect(() => {
-    axios.get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/random`)
       .then((response) => {
-        setBeer(response.data)
+        setBeer(response.data);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }, [])
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <Header></Header>
-      <div>
+      <RandomBeerContainer>
         {!beer && <h2>...Loading</h2>}
 
         {beer && (
           <>
-            <img src={beer.image_url} alt={beer.name} />
-            <h2>{beer.name}  {beer.attenuation_level}</h2>
-            <h3>{beer.tagline}</h3>
-            <h5>{beer.first_brewed}</h5>
-            <h4>{beer.description}</h4>
-            <h5>{beer.contributed_by}</h5>
+            <BeerImage src={beer.image_url} alt={beer.name} />
+            <BeerTitle>
+              {beer.name} {beer.attenuation_level}
+            </BeerTitle>
+            <BeerSubtitle>{beer.tagline}</BeerSubtitle>
+            <BeerSubtitle>First Brewed: {beer.first_brewed}</BeerSubtitle>
+            <BeerDescription>{beer.description}</BeerDescription>
+            <BeerSubtitle>Contributed by: {beer.contributed_by}</BeerSubtitle>
           </>
         )}
-      </div>
+      </RandomBeerContainer>
     </>
-  )
+  );
 }
 
-export default RandomBeer
+export default RandomBeer;
