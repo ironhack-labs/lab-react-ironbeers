8b7ba8e49b0f46fede99f453a7cdf294652b0b0f
Jorlyjoseph <141339657+Jorlyjoseph@users.noreply.github.com>
2023-09-24 18:27:17 +0200

    iteration 7

--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,5 +1,62 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function RandomBeersPage() {
-  return <div>Random beers page</div>;
+  const [beerDetails, setBeerdetails] = useState({});
+
+  const apiUrl = `https://ih-beers-api2.herokuapp.com/beers/random`;
+
+  useEffect(() => {
+    const fetchBeerDetails = async () => {
+      try {
+        const response = await axios.get(apiUrl);
+        const {
+          image_url,
+          name,
+          tagline,
+          first_brewed,
+          attenuation_level,
+          description,
+          contributed_by,
+        } = response.data;
+
+        setBeerdetails({
+          image_url,
+          name,
+          tagline,
+          first_brewed,
+          attenuation_level,
+          description,
+          contributed_by,
+        });
+      } catch (error) {
+        console.error("Error fetching beers:", error);
+      }
+    };
+    fetchBeerDetails();
+  }, []);
+  console.log(beerDetails);
+  return (
+    <div className="details">
+      <div className="image-container">
+        <img src={beerDetails.image_url} />
+      </div>
+
+      <div className="name-container">
+        <h2>{beerDetails.name}</h2>
+        <p>{beerDetails.attenuation_level}</p>
+      </div>
+
+      <div className="tagline-container">
+        <p>{beerDetails.tagline}</p>
+        <p>{beerDetails.first_brewed}</p>
+      </div>
+
+      <p>{beerDetails.description}</p>
+
+      <p>{beerDetails.contributed_by}</p>
+    </div>
+  );
 }
 
 export default RandomBeersPage;
