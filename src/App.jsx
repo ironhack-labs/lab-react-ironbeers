3a9799fe550d4ded7a6528450a1d38ac2ce7391e
Daniel Rodrigues <danielbrodrigues25@gmail.com>
2023-11-07 23:16:37 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import "./App.css";
+import {Routes, Route} from 'react-router-dom'
+import HomePage from "./pages/HomePage";
+import NavBar from "./pages/NavBar";
+import AllBeersPage from "./pages/AllBeersPage";
+import AddBeerPage from "./pages/AddBeerPage";
+import BeerDetailsPage from "./pages/BeerDetailsPage";
+import RandomBeersPage from "./pages/RandomBeerPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>LAB | React IronBeers</h1>
+    <div>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/beers" element={<AllBeersPage/>}/>
+        <Route path="/random-beer" element={<RandomBeersPage/>}/>
+        <Route path="/new-beer" element={<AddBeerPage/>}/>
+        <Route path="/beers/:beerId" element={<BeerDetailsPage/>}/>
+      </Routes>
     </div>
   );
 }
--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,3 +1,36 @@
-function BeerDetailsPage() {}
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from 'react-router-dom';
+
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers";
+
+function BeerDetailsPage() {
+  const [beer, setBeer] = useState(null);
+  const { beerId } = useParams(); // This is how you access the beerId from the URL.
+
+  useEffect(() => {
+    axios
+    .get(`${API_URL}/${beerId}`)
+    .then((response) => {
+        console.log(response.data); // To help visualize the data structure.
+        setBeer(response.data);
+      })
+    .catch((error) => console.log(error));
+  }, [beerId]); // The effect depends on beerId and will rerun if it changes.
+
+  if (!beer) return <p>Loading...</p>; // Handling the loading state.
+
+  return (
+    <div>
+      <img src={beer.image_url} alt={beer.name} style={{ height: '200px' }} />
+      <h1>{beer.name}</h1>
+      <h2>{beer.tagline}</h2>
+      <p><strong>First Brewed:</strong> {beer.first_brewed}</p>
+      <p><strong>Attenuation Level:</strong> {beer.attenuation_level}</p>
+      <p><strong>Description:</strong> {beer.description}</p>
+      <p><strong>Contributed by:</strong> {beer.contributed_by}</p>
+    </div>
+  );
+}
 
 export default BeerDetailsPage;
