91f6543806c9d1b3d9f813c01b4ae3b15e0eff84
FaresQaddoura29 <faresqaddoura@gmail.com>
2023-11-07 17:05:57 +0000

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,12 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+import homeIcon from '/src/assets/home-icon.png';
+export default function Navbar(){
+    return (
+        <nav>
+            <Link to="/">
+                <img src={homeIcon} />
+            </Link>
+        </nav>
+    )
+}
 
-export default Navbar;
