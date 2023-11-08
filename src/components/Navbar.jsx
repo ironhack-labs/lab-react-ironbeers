642c61b56f4d0886c322b170aadb7397d8357d44
Miguel Mestre <mestremn28@hotmail.com>
2023-11-08 00:11:00 +0000

    5week2day

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,12 @@
-function Navbar() {}
+import {Link} from 'react-router-dom';
+import React from 'react';
+
+function Navbar() {
+   return( <div className='navBar'>
+             
+            <Link to="/"><img src="https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"/></Link>
+        
+    </div>)
+}
 
 export default Navbar;
