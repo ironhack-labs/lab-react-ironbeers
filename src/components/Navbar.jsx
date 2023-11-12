f8b85b4df2067dfaed8ee0dd73d95bc41fd42547
GasparBonari <gasparbonari@gmail.com>
2023-11-12 17:03:16 +0100

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,15 @@
-function Navbar() {}
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-link">
+        Home
+      </Link>
+    </nav>
+  );
+};
 
 export default Navbar;
