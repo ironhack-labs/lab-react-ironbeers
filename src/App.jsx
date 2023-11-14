19a4029c4a1f48e09d446a12490e8a4c75670289
Júlia Sendra <juliasendra98@gmail.com>
2023-11-14 17:34:49 +0100

    Iteration 1 | Setup React Router and Create Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import AllBeersPage from "./pages/AllBeersPage";
+import RandomBeerPage from "./pages/RandomBeerPage";
+import BeerDetailPage from "./pages/BeerDetailsPage";
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React IronBeers</h1>
+
+      <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/beers" element={<AllBeersPage />} />
+      <Route path="/random-beer" element={<RandomBeerPage} />
+      <Route path="/new-beer" element={<AddBeerPage} />
+      <Route path="/beers/:beerId" element={<BeerDetailPage} />
+    
+    </Routes>
     </div>
+
   );
 }
 
