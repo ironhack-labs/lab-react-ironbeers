a1c1432f4ac453ac55f4ce70120f33744ffd54ce
mayakilbertus <maya.kilbertus@yahoo.de>
2023-11-14 17:54:19 +0100

    Iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import AllBeersPage from "./pages/AllBeersPage";
 import RandomBeerPage from "./pages/RandomBeerPage";
 import AddBeerPage from "./pages/AddBeerPage";
 import BeerDetailsPage from "./pages/BeerDetailsPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/beers" element={<AllBeersPage />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,15 @@
-function Navbar() {}
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <NavLink to={"/"}>
+          <img id="home-icon" src="./src/assets/home-icon.png" />
+        </NavLink>
+      </nav>
+    </>
+  );
+}
 
 export default Navbar;
