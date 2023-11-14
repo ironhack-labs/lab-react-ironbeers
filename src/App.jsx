8400dbe892ca8a01e941b2e37e68410b9a36e5b8
Júlia Sendra <juliasendra98@gmail.com>
2023-11-14 17:58:50 +0100

    Iteration 1 corrected

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import AllBeersPage from "./pages/AllBeersPage";
 import RandomBeerPage from "./pages/RandomBeerPage";
+import AddBeerPage from "./pages/AddBeerPage";
 import BeerDetailPage from "./pages/BeerDetailsPage";
 import Navbar from "./components/Navbar";
 
@@ -17,9 +18,9 @@ function App() {
       <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/beers" element={<AllBeersPage />} />
-      <Route path="/random-beer" element={<RandomBeerPage} />
-      <Route path="/new-beer" element={<AddBeerPage} />
-      <Route path="/beers/:beerId" element={<BeerDetailPage} />
+      <Route path="/random-beer" element={<RandomBeerPage />} />
+      <Route path="/new-beer" element={<AddBeerPage />} />
+      <Route path="/beers/:beerId" element={<BeerDetailPage />} />
     
     </Routes>
     </div>
