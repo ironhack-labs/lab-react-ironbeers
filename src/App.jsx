c37d50e8da1cb76c9bb4f1e5006a8c38d3c0e69d
Júlia Sendra <juliasendra98@gmail.com>
2023-11-14 17:52:20 +0100

    Iteration 3 | Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import HomePage from "./pages/HomePage";
 import AllBeersPage from "./pages/AllBeersPage";
 import RandomBeerPage from "./pages/RandomBeerPage";
 import BeerDetailPage from "./pages/BeerDetailsPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React IronBeers</h1>
+      <h1>IronBeers</h1>
+
+      <Navbar> </Navbar>
 
       <Routes>
       <Route path="/" element={<HomePage />} />
