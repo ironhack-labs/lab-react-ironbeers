530a8ebb2ea9e0e01f7a7b4ecc9449af854073b3
“elisejonkers” <elisejonkers18@hotmail.com>
2023-11-14 17:25:56 +0100

    set up HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,22 @@
-function HomePage() {}
+import { Link, NavLink } from "react-router-dom";
+
+function HomePage() {
+    return (
+      <div className="HomePage">
+        <NavLink to="/beers">
+          <img src="src/assets/beers.png" />
+          <h1>All Beers</h1>
+        </NavLink>
+        <NavLink to="/random-beer">
+          <img src="src/assets/random-beer.png" />
+          <h1>Random Beer</h1>
+        </NavLink>
+        <NavLink to="/new-beer">
+          <img src="src/assets/new-beer.png" />
+          <h1>New Beer</h1>
+        </NavLink>
+      </div>
+    );
+}
 
 export default HomePage;
