f62b8e81fdd1f9dbef47f9690c7afbc947917f02
Cristina RP <cristinarp95@gmail.com>
2023-11-14 21:48:45 +0100

    Solve Iteration 6
    Display a random beer and its details

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -4,38 +4,43 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-const apiUrl = "https://ih-beers-api2.herokuapp.com/beers"
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers"
 
 function BeerDetailsPage(props) {
 
     const [beerDetails, setBeerDetails] = useState('');
-    // const [fetching, setFetching] = useState(true);
+    const [fetching, setFetching] = useState(true);
 
     const {beerId} = useParams();
 
     useEffect(() => {
-        axios.get(`${apiUrl}/${beerId}`)
+        axios.get(`${API_URL}/${beerId}`)
             .then((response) => {
                 setBeerDetails(response.data);
+                setFetching(false);
             })
-            .catch((e) => {
-                console.log(e);
+            .catch((error) => {
+                console.log('Error' + error);
             })
     }, [beerId])
 
     return (
         <div className="beer-details">
-            <img src={beerDetails.image_url} />
-            <div className="text-beer-details">
-                <h1>{beerDetails.name}</h1>
-                <p>{beerDetails.tagline}</p>
-                <p>{beerDetails.attenuation_level}</p>
-                <p>{beerDetails.first_brewed}</p>
-                <p>{beerDetails.description}</p>
-                <p>{beerDetails.contributed_by}</p>    
-            </div>
+            {fetching ? <p>Loading...</p> : (
+                <div>
+                    <img src={beerDetails.image_url} alt={`${beerDetails.name} bottle`} />
+                    <div className="text-beer-details">
+                        <h1>{beerDetails.name}</h1>
+                        <p>{beerDetails.tagline}</p>
+                        <p>{beerDetails.attenuation_level}</p>
+                        <p>{beerDetails.first_brewed}</p>
+                        <p>{beerDetails.description}</p>
+                        <p>{beerDetails.contributed_by}</p>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
 export default BeerDetailsPage;
--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,8 +1,39 @@
-function RandomBeersPage() {
+import axios from "axios";
+
+import { useEffect, useState } from "react";
+
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers/random"
+
+function RandomBeersPage(props) {
+
+    const [randomBeer, setRandomBeer] = useState()
+    const [fetching, setFetching] = useState(true);
+
+    useEffect(() => {
+        axios.get(API_URL)
+            .then((response) => {
+                setRandomBeer(response.data)
+                setFetching(false)
+            })
+            .catch((error) => console.log('Error' + error))
+    }, [])
+
     return (
-        <div>
-            <h1>RANDOM BEER</h1>
-        </div>
+        <div className="beer-details">
+            {fetching ? <p>Loading ...</p> :
+                <div>
+                    <img src={randomBeer.image_url} alt={`${randomBeer.name} bottle`}  />
+                    <div className="text-beer-details">
+                        <h1>{randomBeer.name}</h1>
+                        <h2>{randomBeer.tagline}</h2>
+                        <p>{randomBeer.first_brewed}</p>
+                        <h2>{randomBeer.attenuation_level}</h2>
+                        <p>{randomBeer.description}</p>
+                        <p>{randomBeer.contributed_by}</p>
+                    </div>
+                </div>
+            }
+        </div >
     )
 }
 
