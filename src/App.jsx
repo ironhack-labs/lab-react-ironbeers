4eda2bd616cfcc8ee69f572316f2386b4a1ccc26
yvzbly <yavuzbalay90@gmail.com>
2023-11-14 20:41:25 +0100

    Done until 7th iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import BeerDetailsPage from "./pages/BeerDetailsPage";
+import AllBeersPage from "./pages/AllBeersPage";
+import RandomBeerPage from "./pages/RandomBeerPage";
+import AddBeerPage from "./pages/AddBeerPage";
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React IronBeers</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/beers" element={<AllBeersPage />} />
+        <Route path="/random-beer" element={<RandomBeerPage />} />
+        <Route path="/new-beer" element={<AddBeerPage />} />
+        <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,13 @@
-function Navbar() {}
+import React from "react";
+import { Link } from "react-router-dom";
+function Navbar() {
+    return(
+        <nav>
+            <Link to="/">🏠Home</Link>
+        </nav>
+    )
+
+
+}
 
 export default Navbar;
