d0e2aa09c218ea007ef17c143ed2974088839028
mayakilbertus <maya.kilbertus@yahoo.de>
2023-11-14 18:17:12 +0100

    Iteration 4

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,46 @@
-function AllBeersPage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function AllBeersPage() {
+  const [beersList, setBeersList] = useState(null);
+
+  const fetchData = () => {
+    axios
+      .get("https://ih-beers-api2.herokuapp.com/beers")
+      .then((response) => {
+        setBeersList(response.data);
+        console.log("Data fetching from API successful");
+        console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <>
+      {beersList === null ? (
+        <p>Loading....</p>
+      ) : (
+        beersList.map((beer, index) => {
+          return (
+            <>
+              <section className="beer-card" key={index}>
+                <img className="beer-logo" src={beer.image_url}></img>
+                <div className="beer-info">
+                  <h2>{beer.name}</h2>
+                  <p>{beer.tagline}</p>
+                  <p>Created by: {beer.contributed_by}</p>
+                </div>
+              </section>
+              <hr />
+            </>
+          );
+        })
+      )}
+    </>
+  );
+}
 
 export default AllBeersPage;
