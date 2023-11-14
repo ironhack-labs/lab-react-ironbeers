5de7a382893d3612d23f85ed63fb82a4158fdb80
Júlia Sendra <juliasendra98@gmail.com>
2023-11-14 21:18:23 +0100

    Iteration 5 | Display a single beer

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -39,9 +39,6 @@ function AllBeersPage() {
                     </div>
                 );
             })}
-
-
-
         </div>
     )
 }
--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,3 +1,44 @@
-function BeerDetailsPage() {}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers";
+
+function BeerDetailsPage() {
+    const [beer, setBeer] = useState({});
+    const { beerId } = useParams();
+
+    const getTheBeer = () => {
+        axios.get(`${API_URL}/${beerId}`)
+            .then((response) => {
+                setBeer(response.data);
+            })
+            .catch((error) => {
+                console.log("Error getting beer from the API...");
+                console.log(error);
+            })
+    };
+
+    useEffect(() => {
+        getTheBeer();
+    }, [beerId]);
+
+    return (
+        <div className="BeerDetail">
+             <div className="BeerCard">
+                <img className="beerImg" alt="`${beer.name}`" src={beer.image_url} />
+                    <div className="beerDetails">
+                    <h1>{beer.name}</h1>
+                    <p>{beer.tagline}</p>
+                    <p>First Brewed: {beer.first_brewed}</p>
+                    <p>Attenuation Level: {beer.attenuation_level}</p>
+                    <p>Description: {beer.description}</p>
+                    <p>Contributed By: {beer.contributed_by}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default BeerDetailsPage;
