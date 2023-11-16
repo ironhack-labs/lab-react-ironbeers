6841005ea56b8dcafe7932503d32877a491dd579
Zohra <zohra.boukhatem1@gmail.com>
2023-11-16 10:58:49 +0100

    added form to add beer page (without submit functionality)

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,7 +1,57 @@
 import Navbar from "../components/Navbar";
 
 function AddBeerPage() {
-    return <Navbar/>
+  return (
+    <>
+      <Navbar />
+      <form>
+        <label>
+            Name:
+          <input name="name" type="text"/>
+          <br/>
+        </label>
+
+        <label>
+        Tagline:
+        <input name="tagline" type="text"/>
+        <br/>
+
+        </label>
+
+        <label>
+        Description:
+        <textarea name="description" type="text"/>
+        <br/>
+        </label>
+
+        <label>
+        First Brewed:
+          <input name="first_brewed" type="text"/>
+          <br/>
+        </label>
+
+        <label>
+        Brewer's Tips:
+        <input name="brewers_tips" type="text"/>
+        <br/>
+        </label>
+
+        <label>
+        Attenuation Level:
+        <input name="attenuation_level" type="number"/>
+        <br/>
+        </label>
+
+        <label>
+        Contributed By:
+        <input name="contributed_by" type="text"/>
+        <br/>
+        </label>
+        
+        <button type="submit">Add Beer</button>
+      </form>
+    </>
+  );
 }
 
 export default AddBeerPage;
