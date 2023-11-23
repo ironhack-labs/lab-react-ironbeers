fb85eadc19612e6d682a787a3efd384a4672d05e
Mohamed khamkham <mohamkham1999@gmail.com>
2023-11-23 23:45:26 +0100

    no esta terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import "./App.css";
+import "./App.css"
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import AllBeersPage from "./pages/AllBeersPage"
+import AddBeerPage from "./pages/AddBeerPage"
+import RandomBeerPage from "./pages/RandomBeerPage"
+import BeerDetailsPage from "./pages/BeerDetailsPage"
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React IronBeers</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/beers" element={<AllBeersPage />} />
+        <Route path="/random-beer" element={<RandomBeerPage />} />
+        <Route path="/new-beer" element={<AddBeerPage />} />
+        <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,20 @@
-function Navbar() {}
+import { Link } from 'react-router-dom';
+import icono from './../assets/home-icon.png'
+
+
+function Navbar() {
+    return (
+        <>
+            <div className="Navbar">
+                <Link to={'/'}><img src={icono} alt="" /></Link>
+            </div>
+        </>
+    )
+
+}
+
 
 export default Navbar;
+
+
+
