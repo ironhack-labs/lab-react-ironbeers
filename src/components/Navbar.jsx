0bb9a302d1094a71b59c8efc54a2e1d95bc50449
Chiara Manni <chiaramiren@gmail.com>
2023-11-23 19:50:52 +0100

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,14 @@
-function Navbar() {}
+import { NavLink } from "react-router-dom"
+import home from "./../assets/home-icon.png"
 
-export default Navbar;
+function Navbarr() {
+
+    return (
+
+        <nav className="Navbar" >
+            <NavLink to="/"><img src={home} alt="home icon" /></NavLink>
+        </nav>
+    )
+}
+
+export default Navbarr
--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,46 @@
-function AllBeersPage() {}
+import axios from "axios"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const baseURL = "https://ih-beers-api2.herokuapp.com/beers"
+
+function AllBeersPage() {
+
+    const [beers, setBeers] = useState([])
+
+    useEffect(() => {
+        axios
+            .get(baseURL)
+            .then((response) => setBeers(response.data))
+            .catch(err => console.log("THIS IS AN ERROR", err))
+    }, [])
+
+    return (
+
+        <div className="beersPage">
+
+            <h1>BEERS!!</h1>
+
+            {
+                beers.map(elm => {
+                    return (
+                        <div className="beersList">
+                            <img src={elm.image_url} alt={elm.name} className="beersimg" />
+                            <div className="beerDetails">
+                                <Link to={`/${elm._id}`}><h3>{elm.name}</h3></Link>
+                                <p><b>Tagline:</b> {elm.tagline}</p>
+                                <p><b>Contributed by:</b> {elm.contributed_by}</p>
+                            </div>
+                        </div>
+
+                    )
+                })
+            }
+
+        </div>
+
+
+    )
+}
 
 export default AllBeersPage;
