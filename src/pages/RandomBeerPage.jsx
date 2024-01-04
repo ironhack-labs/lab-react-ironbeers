ea6704497d0f804ab98136b539afa84ca936ea30
Zohra <zohra.boukhatem1@gmail.com>
2024-01-04 11:18:18 +0100

    add random beer functionality

--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -4,14 +4,17 @@ import {useEffect, useState} from "react"
 
 function RandomBeersPage() {
 
-    const [randomBeer, setRandomBeer] = useState([])
+    const [randomBeer, setRandomBeer] = useState({})
 
 useEffect(() => {
     axios.get("https://ih-beers-api2.herokuapp.com/beers")
     .then((response) => {
+        console.log(response)
         let max = response.data.length-1
         const id = Math.floor(Math.random() * max)
-        setRandomBeer(id)
+        setRandomBeer(response.data[id])
+        console.log(randomBeer)
+        console.log(randomBeer.name)
     })
     .catch(error => error)
 }, [])
