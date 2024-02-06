5a5233484ed1a77bc624d9dfa8763edc31d973c2
samanta-scavassa <samanta.camargoterra@gmail.com>
2024-02-06 17:39:41 +0100

    solve react iron brewers lab

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,27 +1,31 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "../components/Search";
-import beersJSON from "./../assets/beers.json";
+import axios from "axios";
 
-
+const beersURL = "https://ih-beers-api2.herokuapp.com/beers";
 
 function AllBeersPage() {
   // Mock initial state, to be replaced by data from the API. Once you retrieve the list of beers from the Beers API store it in this state variable.
-  const [beers, setBeers] = useState(beersJSON);
-
-
+  const [beers, setBeers] = useState("");
 
   // TASKS:
   // 1. Set up an effect hook to make a request to the Beers API and get a list with all the beers.
   // 2. Use axios to make a HTTP request.
   // 3. Use the response data from the Beers API to update the state variable.
-
-
+  useEffect(() => {
+    axios
+      .get(beersURL)
+      .then((response) => {
+        setBeers(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   // The logic and the structure for the page showing the list of beers. You can leave this as it is for now.
   return (
     <>
-      <Search />
+      <Search setBeers={setBeers} />
 
       <div className="d-inline-flex flex-wrap justify-content-center align-items-center w-100 p-4">
         {beers &&
@@ -29,14 +33,19 @@ function AllBeersPage() {
             return (
               <div key={i}>
                 <Link to={"/beers/" + beer._id}>
-                  <div className="card m-2 p-2 text-center" style={{ width: "24rem", height: "18rem" }}>
+                  <div
+                    className="card m-2 p-2 text-center"
+                    style={{ width: "24rem", height: "18rem" }}
+                  >
                     <div className="card-body">
                       <img
                         src={beer.image_url}
                         style={{ height: "6rem" }}
                         alt={"image of" + beer.name}
                       />
-                      <h5 className="card-title text-truncate mt-2">{beer.name}</h5>
+                      <h5 className="card-title text-truncate mt-2">
+                        {beer.name}
+                      </h5>
                       <h6 className="card-subtitle mb-3 text-muted">
                         <em>{beer.tagline}</em>
                       </h6>
