61dc97fb94d7689a705b50a010b1de5670681286
Nadine Rangosch <nrangosch@gmail.com>
2024-02-09 18:53:17 +0100

    added styling for random beer page

--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -17,18 +17,20 @@ function RandomBeersPage() {
         <div>
             {!beerDetails && <div className="loading"><p>...loading</p></div>}
             {beerDetails && (
-                <div className="beer-wrapper">
-                            <div className="img-wrapper">
-                        <img src={beerDetails.image_url} alt={beerDetails.name} height={200} />
+                <div className="beer-details-wrapper">
+                            <div className="img-details-wrapper">
+                        <img src={beerDetails.image_url} alt={beerDetails.name} height={300} />
 
                             </div>
-                        <div className="beer-info">
-                        <h2>{beerDetails.name}</h2>
-                        <p>{beerDetails.tagline}</p>
-                        <p>{beerDetails.first_brewed}</p>
-                        <p>{beerDetails.attenuation_level}</p>
-                        <p>{beerDetails.description}</p>
-                        <p>{beerDetails.contributed_by}</p>
+                        <div className="beer-details-info">
+                        <h2 className="beer-name">{beerDetails.name}</h2>
+                        <span className="top-right-info">
+                        <p className="beer-level">{beerDetails.attenuation_level}</p>
+                        <p className="beer-brewed">{beerDetails.first_brewed}</p>
+                        </span>
+                        <span className="beer-tagline">{beerDetails.tagline}</span>
+                        <p className="beer-desc">{beerDetails.description}</p>
+                        <p className="beer-contributed">{beerDetails.contributed_by}</p>
 
                         </div>
                         </div>
