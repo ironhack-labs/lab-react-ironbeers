c05aa60d38d58bceb4272dbe8e4840cb617e7767
Stefan Kummerlöw <gummiz@gmx.net>
2024-02-13 16:01:22 +0100

    show all beers, add new beer

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function AddBeerPage() {
   // State variables to store the values of the form inputs. You can leave these as they are.
@@ -21,20 +23,49 @@ function AddBeerPage() {
   const handleAttenuationLevel = (e) => setAttenuationLevel(e.target.value);
   const handleContributedBy = (e) => setContributedBy(e.target.value);
 
-
-
   // TASK:
   // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
 
 
+  const baseUrl = "https://ih-beers-api2.herokuapp.com/beers/";
+  const targetUrl = baseUrl + "/new";
+
+
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const newEntry = {
+      name: name,
+      tagline: tagline,
+      description: description,
+      image_url: imageUrl,
+      first_brewed: firstBrewed,
+      brewers_tips: brewersTips,
+      attenuation_level: attenuationLevel,
+      contributed_by: contributedBy
+
+    };
+
+    axios
+      .post(targetUrl, newEntry)
+      .then((result) => {
+        // console.log(result);
+        navigate("/beers")
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   // Structure and the content of the page showing the form for adding a new beer. You can leave this as it is.
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
+        <form onSubmit={submitHandler}>
           <label>Name</label>
           <input
             className="form-control mb-4"
--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "../components/Search";
 import beersJSON from "./../assets/beers.json";
-
+import axios from "axios"
 
 
 function AllBeersPage() {
   // Mock initial state, to be replaced by data from the API. Once you retrieve the list of beers from the Beers API store it in this state variable.
   const [beers, setBeers] = useState(beersJSON);
 
+  const baseUrl = "https://ih-beers-api2.herokuapp.com/beers"
 
 
   // TASKS:
@@ -16,7 +17,14 @@ function AllBeersPage() {
   // 2. Use axios to make a HTTP request.
   // 3. Use the response data from the Beers API to update the state variable.
 
-
+  useEffect(() => {
+    axios.get(baseUrl)
+    .then((result) => {
+      setBeers(result.data);
+    }).catch((err) => {
+      console.log(err);
+    });
+  },[]) 
 
   // The logic and the structure for the page showing the list of beers. You can leave this as it is for now.
   return (
