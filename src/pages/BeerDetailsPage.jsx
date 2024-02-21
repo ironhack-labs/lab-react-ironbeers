d47e38ed40670759ae538aac3f3dc0326aaf3277
Abranucu <abrahamcastiellobarbas@gmail.com>
2024-02-21 18:13:16 +0100

    faltan bonus

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import beersJSON from "./../assets/beers.json";
+import axios from "axios";
 
 
 function BeerDetailsPage() {
@@ -17,6 +18,16 @@ function BeerDetailsPage() {
   // 2. Set up an effect hook to make a request for the beer info from the Beers API.
   // 3. Use axios to make a HTTP request.
   // 4. Use the response data from the Beers API to update the state variable.
+  const params = useParams()
+  console.log(params.beerId)
+  useEffect(() => {
+    axios.get(`https://ih-beers-api2.herokuapp.com/beers/${params.beerId}`)
+    .then((res) => {
+      console.log(res.data)
+      setBeer(res.data)
+    })
+    .catch((err) => console.log(err))
+  }, [])
 
 
 
