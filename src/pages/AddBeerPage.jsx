49738bdb612ea930eaf67da4323fde40c07d4701
PaulinaWW <159482754+PaulinaWW@users.noreply.github.com>
2024-03-12 23:57:30 +0100

    Update AddBeerPage.jsx

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -28,13 +28,26 @@ function AddBeerPage() {
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
 
+const handleForSubmit = (newBeer) => {
+    newBeer.preventDefault()
 
+    axios
+      .post(`${api_base_url}/new-beer`, newBeer)
+      .then(() => navigate("/beers"))
+      .catch(err => console.log(err))
+  }
+
+  const handleInputChange = elm => {
+    const { value, name } = elm.target
+    setNewBeer({ ...newBeer, [name]: value })
+  }
 
   // Structure and the content of the page showing the form for adding a new beer. You can leave this as it is.
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
         <form>
+        <form onSubmit={handleForSubmit}>
           <label>Name</label>
           <input
             className="form-control mb-4"
@@ -43,6 +56,8 @@ function AddBeerPage() {
             placeholder="Beer Name"
             value={name}
             onChange={handleName}
+            value={newBeer.name}
+            onChange={handleInputChange}
           />
           <label>Tagline</label>
           <input
@@ -52,81 +67,82 @@ function AddBeerPage() {
             placeholder="Beer Tagline"
             value={tagline}
             onChange={handleTagline}
+            value={newBeer.tagline}
+            onChange={handleInputChange}
           />
 
           <label className="form-label">Description</label>
-          <textarea
-            className="form-control mb-4"
-            type="text"
+@@ -61,8 +65,8 @@ function AddBeerPage() {
             name="description"
             placeholder="Description"
             rows="3"
             value={description}
             onChange={handleDescription}
+            value={newBeer.description}
+            onChange={handleInputChange}
           ></textarea>
 
           <label>Image</label>
-          <input
-            className="form-control mb-4"
+@@ -71,8 +75,8 @@ function AddBeerPage() {
             type="text"
             name="imageUrl"
             placeholder="Image URL"
             value={imageUrl}
             onChange={handleImageUrl}
+            value={newBeer.imageUrl}
+            onChange={handleInputChange}
           />
 
           <label>First Brewed</label>
-          <input
-            className="form-control mb-4"
+@@ -81,8 +85,8 @@ function AddBeerPage() {
             type="text"
             name="firstBrewed"
             placeholder="Date - MM/YYYY"
             value={firstBrewed}
             onChange={handleFirstBrewed}
+            value={newBeer.firstBrewed}
+            onChange={handleInputChange}
           />
 
           <label>Brewer Tips</label>
-          <input
-            className="form-control mb-4"
+@@ -91,8 +95,8 @@ function AddBeerPage() {
             type="text"
             name="brewersTips"
             placeholder="..."
             value={brewersTips}
             onChange={handleBrewersTips}
+            value={newBeer.brewersTips}
+            onChange={handleInputChange}
           />
 
           <label>Attenuation Level</label>
-          <div className="input-group mb-2">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1">
-                %
-              </span>
-            </div>
-            <input
+@@ -106,8 +110,8 @@ function AddBeerPage() {
               className="form-control mb-4"
               type="number"
               name="attenuationLevel"
               value={attenuationLevel}
               onChange={handleAttenuationLevel}
+              value={newBeer.attenuationLevel}
+              onChange={handleInputChange}
               min={0}
               max={100}
             />
-          </div>
-
-          <label>Contributed By</label>
-          <input
-            className="form-control mb-4"
+@@ -119,14 +123,14 @@ function AddBeerPage() {
             type="text"
             name="contributedBy"
             placeholder="Contributed by"
             value={contributedBy}
             onChange={handleContributedBy}
+            value={newBeer.contributedBy}
+            onChange={handleInputChange}
           />
           <button className="btn btn-primary btn-round">Add Beer</button>
         </form>
       </div>
     </>
   );
+  )
 }
 
 export default AddBeerPage;
+export default AddBeerPage
