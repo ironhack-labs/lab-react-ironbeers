935088dc355574dd06212c979e0ffa0463643fc9
unknown <unidrelyn@gmail.com>
2024-03-13 22:25:33 +0100

    iteration 6

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,14 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+import LogoHome from "../assets/home-icon.png";
+
+function Navbar() {
+	return (
+		<div className="containerNavbar">
+			<Link to="/">
+				<img src={LogoHome} alt="LogoHome" />
+			</Link>
+		</div>
+	);
+}
 
 export default Navbar;
--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,3 +1,30 @@
-function RandomBeersPage() {}
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import { CardBeer } from "../components/CardBeer";
+
+function RandomBeersPage() {
+	const [beer, setBeer] = useState([]);
+	useEffect(() => {
+		const getBeer = async () => {
+			try {
+				const res = await axios.get(
+					`https://ih-beers-api2.herokuapp.com/beers/random`
+				);
+				console.log(res.data);
+				setBeer(res.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getBeer();
+	}, []);
+
+	if (!beer) {
+		return <div>loading...</div>;
+	}
+
+	return <CardBeer beer={beer} />;
+}
 
 export default RandomBeersPage;
