4f0ec15d3abb160b93d305fb8a9f2b554108472e
Aliakbar Torbati <alitorbati1368@gmail.com>
2024-03-13 17:37:37 +0100

    results

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,37 @@ import AllBeersPage from "./pages/AllBeersPage";
 import RandomBeerPage from "./pages/RandomBeerPage";
 import AddBeerPage from "./pages/AddBeerPage";
 import BeerDetailsPage from "./pages/BeerDetailsPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [beers, setBeers] = useState(null);
+
+  useEffect(() => {
+    const getBeers= async () => {
+      try{
+        // const response= await axios("https://ih-beers-api2.herokuapp.com/beers");
+        const response= await axios("https://api.escuelajs.co/api/v1/products");
+
+        console.log("here is the response.data", response.data);
+        setBeers(response.data);
+      }catch(error){
+        console.log(error);
+      } 
+
+    }
+    getBeers();
+
+  }, []); 
+
   return (
     <div className="App">
     <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/beers" element={<AllBeersPage />} />
+        <Route path="/beers" element={<AllBeersPage beers={beers} setBeers={setBeers}/>} />
         <Route path="/random-beer" element={<RandomBeerPage />} />
-        <Route path="/new-beer" element={<AddBeerPage />} />
+        <Route path="/new-beer" element={<AddBeerPage beers={beers} setBeers={setBeers}/>} />
         <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
       </Routes>
     </div>
--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function AddBeerPage() {
+
+function AddBeerPage({beers}, {setBeers}) {
+  const nav = useNavigate();
   // State variables to store the values of the form inputs. You can leave these as they are.
   const [name, setName] = useState("");
   const [tagline, setTagline] = useState("");
@@ -20,21 +23,54 @@ function AddBeerPage() {
   const handleBrewersTips = (e) => setBrewersTips(e.target.value);
   const handleAttenuationLevel = (e) => setAttenuationLevel(e.target.value);
   const handleContributedBy = (e) => setContributedBy(e.target.value);
-
-
-
   // TASK:
   // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
+  const handleCreateNewBeer = async (event) => {
+    event.preventDefault();
+
+    const newBeer = {
+      name,
+      tagline,
+      description,
+      imageUrl,
+      firstBrewed,
+      brewersTips,
+      attenuationLevel,
+      contributedBy,
+    };
+    try {
+      const response = await fetch(
+        "https://ih-beers-api2.herokuapp.com/beers/new",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newBeer),
+        }
 
 
+
+
+      );
+      const parsed = await response.json();
+      console.log("New Beer:", parsed);
+      setBeers([parsed, ...beers]);
+      nav("/");
+    } catch {
+      (err) => {
+        console.log(err);
+      };
+    }
+  };
 
   // Structure and the content of the page showing the form for adding a new beer. You can leave this as it is.
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
+        <form onSubmit={handleCreateNewBeer}>
           <label>Name</label>
           <input
             className="form-control mb-4"
