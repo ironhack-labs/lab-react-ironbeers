4ad756fdf0d6e0f165238faf4e7f275aa754a000
Arnau Ramos <arnauramos21@gmail.com>
2024-03-21 21:19:11 +0100

    done iteartion 8

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -3,26 +3,51 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const beersAPI = "https://ih-beers-api2.herokuapp.com/beers";
+
 function AllBeersPage() {
   const [beers, setBeers] = useState([]);
+  const [beersFromAPI, setBeersFromAPI] = useState([]);
+  const [searchBeer, setSearchBeer] = useState("");
 
-  axios
-  .get(beersAPI)
-  .then((response) => {
-    setBeers(response.data);
-  });
+  useEffect(() => {
+    axios.get(beersAPI).then((response) => {
+      setBeers(response.data);
+      setBeersFromAPI(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    filterBeers();
+  }, [searchBeer]);
+
+  const filterBeers = () => {
+    const filteredBeers = beersFromAPI.filter((beer) => {
+      return beer.name.toLowerCase().includes(searchBeer.toLowerCase());
+    });
+    setBeers(filteredBeers);
+  };
+
+  const handleChange = (e) => {
+    const newBeer = e.target.value.toLowerCase();
+    setSearchBeer(newBeer);
+  };
 
   return (
     <div>
       <h1>All Beers</h1>
+      <input
+        onChange={handleChange}
+        value={searchBeer}
+        placeholder="Search Beer"
+      />
+      <br />
       <ul>
         {beers.map((beer) => (
           <li key={beer._id}>
-          <img src={beer.image_url}/>
+            <img src={beer.image_url} />
             <Link to={`/beers/${beer._id}`}>{beer.name}</Link>
             <p>{beer.tagline}</p>
             <p>Created by: {beer.contributed_by}</p>
-            
           </li>
         ))}
       </ul>
