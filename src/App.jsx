56b3d86f8ace19f4f7feaaa59138124629fe8740
Sowjanya Kambhampati <sowjanyaakella87@gmail.com>
2024-03-21 20:25:48 +0100

    iteration 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import AllBeersPage from "./pages/AddBeerPage";
 import BeerDetailsPage from "./pages/BeerDetailsPage";
 import HomePage from "./pages/HomePage";
 import RandomBeerPage from "./pages/RandomBeerPage"
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React IronBeers</h1>
+      <Navbar/>
       <Routes>
         <Route path='/new-beer' element={<AddBeerPage/>}/>
         <Route path='/beers' element={<AllBeersPage/>}/>
